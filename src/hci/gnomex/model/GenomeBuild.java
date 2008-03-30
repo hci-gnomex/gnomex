0d639c57d7f072305436812b484c0d0b508431ce
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2008-03-30 20:58:12 +0000

    Add genome build dictionary for solexa requests.

---
+++ src/hci/gnomex/model/GenomeBuild.java
@@ -0,0 +1,65 @@
+package hci.gnomex.model;
+
+import hci.dictionary.model.DictionaryEntry;
+import java.io.Serializable;
+
+
+
+public class GenomeBuild extends DictionaryEntry implements Serializable {
+  private Integer  idGenomeBuild;
+  private String   genomeBuildName;
+  private Integer  idOrganism;
+  private String   isActive;
+  
+  public String getDisplay() {
+    String display = this.getNonNullString(getGenomeBuildName());
+    return display;
+  }
+
+  public String getValue() {
+    return getIdGenomeBuild().toString();
+  }
+
+
+  
+  public String getIsActive() {
+    return isActive;
+  }
+
+  
+  public void setIsActive(String isActive) {
+    this.isActive = isActive;
+  }
+
+
+  
+  public String getGenomeBuildName() {
+    return genomeBuildName;
+  }
+
+  
+  public void setGenomeBuildName(String genomeBuildName) {
+    this.genomeBuildName = genomeBuildName;
+  }
+
+  
+  public Integer getIdGenomeBuild() {
+    return idGenomeBuild;
+  }
+
+  
+  public void setIdGenomeBuild(Integer idGenomeBuild) {
+    this.idGenomeBuild = idGenomeBuild;
+  }
+
+  
+  public Integer getIdOrganism() {
+    return idOrganism;
+  }
+
+  
+  public void setIdOrganism(Integer idOrganism) {
+    this.idOrganism = idOrganism;
+  }
+
+}
