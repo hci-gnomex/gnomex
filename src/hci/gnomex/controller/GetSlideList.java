e8eb9ab608985f6098de56d55645bfc09203afa3
HCI\jholmberg <HCI\jholmberg@572511d9-adea-4dea-8d56-86744b86a2dd>
2008-04-08 16:07:19 +0000

    new slid list command

---
+++ src/hci/gnomex/controller/GetSlideList.java
@@ -0,0 +1,109 @@
+package hci.gnomex.controller;
+
+import hci.gnomex.model.SlideDesign;
+import hci.gnomex.model.SlideProduct;
+import hci.gnomex.model.SlideProductFilter;
+import hci.gnomex.utility.HibernateSession;
+import hci.framework.control.Command;
+import hci.framework.control.RollBackCommandException;
+import hci.framework.model.DetailObject;
+import hci.framework.utilities.XMLReflectException;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
+
+
+public class GetSlideList extends GNomExCommand implements Serializable {
+  
+  private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetSlideList.class);
+  
+  private SlideProductFilter filter;
+  
+  public void validate() {
+  }
+  
+  public void loadCommand(HttpServletRequest request, HttpSession session) {
+
+    filter = new SlideProductFilter();
+    HashMap errors = this.loadDetailObject(request, filter);
+    this.addInvalidFields(errors);
+  }
+
+  public Command execute() throws RollBackCommandException {
+    
+    try {
+      
+   
+    Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
+    
+    StringBuffer buf = filter.getQuery(this.getSecAdvisor());
+    log.info("Query for GetSlideProductList: " + buf.toString());
+    List slideDesigns = (List)sess.createQuery(buf.toString()).list();
+    
+    Document doc = new Document(new Element("SlideProductList"));
+    Element spNode = null;
+    for(Iterator i = slideDesigns.iterator(); i.hasNext();) {
+      SlideProduct sp = (SlideProduct)i.next();
+      Hibernate.initialize(sp.getSlideDesigns());
+      spNode = new Element("SlideProduct");
+      spNode.setAttribute("name",sp.getName());
+      spNode.setAttribute("id", sp.getIdSlideProduct().toString());
+      spNode.setAttribute("isSlideSet",sp.getIsSlideSet());
+      if (sp.getIsSlideSet().equals("Y")) {
+        Iterator sdIter = sp.getSlideDesigns().iterator();
+        while (sdIter.hasNext()) {
+          SlideDesign sd = (SlideDesign) sdIter.next();
+          Element sdNode = new Element("SlideDesign");
+          sdNode.setAttribute("name", sd.getName());
+          sdNode.setAttribute("id", sd.getIdSlideDesign().toString());
+          spNode.addContent(sdNode);
+        }
+      }
+      doc.getRootElement().addContent(spNode);
+    }
+    
+    XMLOutputter out = new org.jdom.output.XMLOutputter();
+    this.xmlResult = out.outputString(doc);
+    
+    setResponsePage(this.SUCCESS_JSP);
+    }catch (NamingException e){
+      log.error("An exception has occurred in GetSlideProductList ", e);
+      e.printStackTrace();
+      throw new RollBackCommandException(e.getMessage());
+    }catch (SQLException e) {
+      log.error("An exception has occurred in GetSlideProductList ", e);
+      e.printStackTrace();
+      throw new RollBackCommandException(e.getMessage());
+    } catch (XMLReflectException e){
+      log.error("An exception has occurred in GetSlideProductList ", e);
+      e.printStackTrace();
+      throw new RollBackCommandException(e.getMessage());
+    } catch (Exception e){
+      log.error("An exception has occurred in GetSlideProductList ", e);
+      e.printStackTrace();
+      throw new RollBackCommandException(e.getMessage());
+    } finally {
+      try {
+        this.getSecAdvisor().closeReadOnlyHibernateSession();        
+      } catch(Exception e) {
+        
+      }
+    }
+    
+    return this;
+  }
+
+}
