a795b058d4a530b05fdf12152cec61ee4c625456
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2008-09-03 15:41:29 +0000

    To fix upload problem (missing session in upload servlet for FireFox, Safari using Flex upload API), encode session in URL for upload servlet.  Also, use non-secure (http: rather than https:) when making http request; otherwise, existing session is not accessible to upload servlet.

---
+++ trunk/src/hci/gnomex/controller/UploadAnalysisURLServlet.java
@@ -0,0 +1,50 @@
+package hci.gnomex.controller;
+
+import hci.gnomex.model.Analysis;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UploadAnalysisURLServlet extends HttpServlet {
+  
+  private Integer idAnalysis = null;
+  private String  directoryName = "";
+  
+  private Analysis analysis;
+  private String   fileName;
+
+  protected void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
+    try {
+      
+      boolean isLocalHost = req.getServerName().equalsIgnoreCase("localhost") || req.getServerName().equals("127.0.0.1");
+      
+      //
+      // COMMENTED OUT CODE: 
+      //    String baseURL =  "http"+ (isLocalHost ? "://" : "s://") + req.getServerName() + req.getContextPath();
+      //
+      // To fix upload problem (missing session in upload servlet for FireFox, Safari), encode session in URL
+      // for upload servlet.  Also, use non-secure (http: rather than https:) when making http request; 
+      // otherwise, existing session is not accessible to upload servlet.
+      //
+      //
+      
+      String baseURL =  "http"+  "://"  + req.getServerName() + req.getContextPath();
+      String URL = baseURL + "/" + "UploadAnalysisFileServlet.gx";
+      // Encode session id in URL so that session maintains for upload servlet when called from
+      // Flex upload component inside FireFox, Safari
+      URL += ";jsessionid=" + req.getRequestedSessionId();
+      
+      
+      res.setContentType("application/xml");
+      res.getOutputStream().println("<UploadAnalysisURL url='" + URL + "'/>");
+      
+    } catch (Exception e) {
+      System.out.println("An error has occured in UploadAnalysisURLServlet - " + e.toString());
+    }
+  }
+}
