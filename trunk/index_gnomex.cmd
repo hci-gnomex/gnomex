816cdb7785706551d7cbdbf414f1cc49da94bf8a
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2010-02-03 17:48:05 +0000

    Added sqlserver 2009 jdbc driver; use relative path for orion home.

--- trunk/index_gnomex.cmd
+++ trunk/index_gnomex.cmd
@@ -1,13 +1,13 @@
 @echo off
 
-set ORION_HOME=C:\orion
-set HIBERNATE_HOME=C:\orion\applications\hcienv
+set ORION_HOME=..\..\
+set HIBERNATE_HOME=%ORION_HOME%\applications\hciEnv
 set LIB=%HIBERNATE_HOME%\lib
 set GNOMEX_HOME=c:\orion\applications\gnomex
 set GNOMEX_JAR=%GNOMEX_HOME%\gnomex.jar
 set LUCENE_JAR=%GNOMEX_HOME%\lib\lucene-core-2.2.0.jar
 
-set JDBC_DRIVER=%ORION_HOME%\lib\mysql-connector-java-5.1.7-bin.jar;%ORION_HOME%\lib\msutil.jar;%ORION_HOME%\lib\msbase.jar;%ORION_HOME%\lib\mssqlserver.jar
+set JDBC_DRIVER=%ORION_HOME%\lib\mysql-connector-java-5.1.7-bin.jar;%ORION_HOME%\lib\msutil.jar;%ORION_HOME%\lib\msbase.jar;%ORION_HOME%\lib\mssqlserver.jar;%ORION_HOME%\lib\sqljdbc4.jar
 set CP=.;%GNOMEX_JAR%;%GNOMEX_HOME%\lib;%LUCENE_JAR%;%JDBC_DRIVER%;%HIBERNATE_HOME%;%LIB%\hibernate3.jar;%ORION_HOME%\lib;%ORION_HOME%\lib\hci_utils.jar;%ORION_HOME%\lib\hci_framework.jar;%LIB%\Hibernate3Utils.jar;%ORION_HOME%\lib\dom4j-1.6.1.jar;%ORION_HOME%\lib\log4j-1.2.11.jar;%ORION_HOME%\lib\commons-logging-1.0.4.jar;%ORION_HOME%\lib\commons-collections-2.1.1.jar;%ORION_HOME%\lib\jta.jar;%ORION_HOME%\lib\jdom.jar;
 for %%i in (%LIB%\*.jar) do call append_classpath.cmd %%i
 
