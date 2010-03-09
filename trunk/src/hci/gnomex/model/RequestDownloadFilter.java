a8692866925a0530d5e1b8ffd3e9037a16bdfdaa
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2010-03-09 23:43:13 +0000

    Fix bug that was filtering out Agilent 1 color experiments when microarray checkbox was checked on Experiments view.

--- trunk/src/hci/gnomex/model/RequestDownloadFilter.java
+++ trunk/src/hci/gnomex/model/RequestDownloadFilter.java
@@ -363,6 +363,9 @@ public class RequestDownloadFilter extends DetailObject {
       queryBuf.append(RequestCategory.OTHER_MICROARRAY_REQUEST_CATEGORY);
       queryBuf.append("', ");
       queryBuf.append("'");
+      queryBuf.append(RequestCategory.AGILIENT_1_COLOR_MICROARRAY_REQUEST_CATEGORY);
+      queryBuf.append("', ");
+      queryBuf.append("'");
       queryBuf.append(RequestCategory.AGILIENT_MICROARRAY_REQUEST_CATEGORY);
       queryBuf.append("') ");
 
