e0ae68551821ee3e5bc68807dd7effb61799adfb
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2010-07-28 19:35:54 +0000

    - Reformat Analysis edit window to leave more room for description.  Moved analysis group to its own tab. - Move filter widgets and refresh link to top panel in Analysis view. - Add ability to filter by multiple labs in Analysis view and ability to filter by analysis created in last week, month, 3 months, or year.

--- trunk/src/hci/gnomex/model/AnalysisGroupFilter.java
+++ trunk/src/hci/gnomex/model/AnalysisGroupFilter.java
@@ -1,6 +1,8 @@
 package hci.gnomex.model;
 
 
+import java.util.Calendar;
+
 import hci.framework.model.DetailObject;
 import hci.gnomex.security.SecurityAdvisor;
 
@@ -12,6 +14,12 @@ public class AnalysisGroupFilter extends DetailObject {
   private String                searchPublicProjects;
   private Integer               idRequest;
   private Integer               idAnalysis;
+  private String                labKeys;
+  private String                searchText;
+  private String                lastWeek = "N";
+  private String                lastMonth = "N";
+  private String                lastThreeMonths = "N";
+  private String                lastYear = "N";
 
 
   private StringBuffer          queryBuf;
@@ -93,12 +101,92 @@ public class AnalysisGroupFilter extends DetailObject {
       queryBuf.append(" ag.idLab =");
       queryBuf.append(idLab);
     } 
+    // Search by multiple labs
+    if (labKeys != null && !labKeys.equals("")) {
+      String[] tokens = labKeys.split(":");
+      if (tokens.length > 0) {
+        this.addWhereOrAnd();
+        queryBuf.append(" ag.idLab  in (");
+        for(int x = 0; x < tokens.length; x++) {
+          if (x > 0) {
+            queryBuf.append(", ");
+          }
+          queryBuf.append(tokens[x]);
+        }
+        queryBuf.append(")");
+      }
+      
+    }
     // Search by idAnalysis 
     if (idAnalysis != null){
       this.addWhereOrAnd();
       queryBuf.append(" a.idAnalysis =");
       queryBuf.append(idAnalysis);
     }
+    
+    // Search by text
+    if (searchText != null && !searchText.equals("")) {
+      this.addWhereOrAnd();
+      queryBuf.append("(");
+      queryBuf.append(" a.name like '%" + searchText + "%'");
+      queryBuf.append(" OR ");
+      queryBuf.append(" a.description like '%" + searchText + "%'");
+      queryBuf.append(" OR ");
+      queryBuf.append(" ag.name like '%" + searchText + "%'");
+      queryBuf.append(" OR ");
+      queryBuf.append(" ag.description like '%" + searchText + "%'");
+      queryBuf.append(")");
+    }
+    
+    // Search for analysis craete in last week
+    if (lastWeek.equals("Y")) {
+
+      Calendar cal = Calendar.getInstance();
+      cal.add(Calendar.DAY_OF_YEAR, -7);
+      java.sql.Date lastWeek = new java.sql.Date(cal.getTimeInMillis());
+      
+      this.addWhereOrAnd();
+      queryBuf.append(" a.createDate >= '");
+      queryBuf.append(this.formatDate(lastWeek));
+      queryBuf.append("'");
+    }
+    // Search for analysis created in last month
+    if (lastMonth.equals("Y")) {
+
+      Calendar cal = Calendar.getInstance();
+      cal.add(Calendar.MONTH, -1);
+      java.sql.Date lastMonth = new java.sql.Date(cal.getTimeInMillis());
+      
+      this.addWhereOrAnd();
+      queryBuf.append(" a.createDate >= '");
+      queryBuf.append(this.formatDate(lastMonth));
+      queryBuf.append("'");
+    }
+    // Search for analysis created in last 3 months
+    if (lastThreeMonths.equals("Y")) {
+
+      Calendar cal = Calendar.getInstance();
+      cal.add(Calendar.MONTH, -3);
+      java.sql.Date last3Month = new java.sql.Date(cal.getTimeInMillis());
+      
+      this.addWhereOrAnd();
+      queryBuf.append(" a.createDate >= '");
+      queryBuf.append(this.formatDate(last3Month));
+      queryBuf.append("'");
+    }
+    // Search for analysis created in last year
+    if (lastYear.equals("Y")) {
+
+      Calendar cal = Calendar.getInstance();
+      cal.add(Calendar.YEAR, -1);
+      java.sql.Date lastYear = new java.sql.Date(cal.getTimeInMillis());
+      
+      this.addWhereOrAnd();
+      queryBuf.append(" a.createDate >= '");
+      queryBuf.append(this.formatDate(lastYear));
+      queryBuf.append("'");
+    }    
+
     
   }
   private void addExperimentItemCriteria() {
@@ -192,6 +280,66 @@ public class AnalysisGroupFilter extends DetailObject {
   
   public void setIdAnalysis(Integer idAnalysis) {
     this.idAnalysis = idAnalysis;
+  }
+
+  
+  public String getLabKeys() {
+    return labKeys;
+  }
+
+  
+  public void setLabKeys(String labKeys) {
+    this.labKeys = labKeys;
+  }
+
+  
+  public String getSearchText() {
+    return searchText;
+  }
+
+  
+  public void setSearchText(String searchText) {
+    this.searchText = searchText;
+  }
+
+  
+  public String getLastWeek() {
+    return lastWeek;
+  }
+
+  
+  public void setLastWeek(String lastWeek) {
+    this.lastWeek = lastWeek;
+  }
+
+  
+  public String getLastMonth() {
+    return lastMonth;
+  }
+
+  
+  public void setLastMonth(String lastMonth) {
+    this.lastMonth = lastMonth;
+  }
+
+  
+  public String getLastThreeMonths() {
+    return lastThreeMonths;
+  }
+
+  
+  public void setLastThreeMonths(String lastThreeMonths) {
+    this.lastThreeMonths = lastThreeMonths;
+  }
+
+  
+  public String getLastYear() {
+    return lastYear;
+  }
+
+  
+  public void setLastYear(String lastYear) {
+    this.lastYear = lastYear;
   }
 
     
