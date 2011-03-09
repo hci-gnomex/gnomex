2bcd409644d7a679f0f82fdf0e122c511a09d61a
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-03-09 21:47:31 +0000

    Bump up to rev 5.1

---
+++ trunk/sql/gnomex_db_upload_to_5.1.sql
@@ -0,0 +1,7 @@
+INSERT INTO `gnomex`.`Property` (`idProperty`,`propertyName`,`propertyValue`,`propertyDescription`, `forServerOnly`) VALUES
+ (48, 'fdt_supported', 'N', 'Flag indicating if FDT is supported.', 'N'),
+ (49, 'http_port', '80', 'The port number that non-secure (http) runs on.', 'Y'),
+ (50, 'fdt_directory', '', 'The fdt staging directory (file path) that is accessible from the fdt server.', 'N'),
+ (51, 'fdt_directory_gnomex', '', 'The fdt staging directory (file path) that is accessible from gnomex application server.', 'N'),
+ (52, 'fdt_client_codebase', '', 'The URL that the fdtClient.jar is served from.', 'N'),
+ (53, 'fdt_server_name', '', 'The server name that the fdt server is running on', 'N');
