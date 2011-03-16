408ad5614a946b8d0a9a4c13edbc701e5f3a85c5
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-03-16 19:38:51 +0000

    - Reworked FDT interface so that daemon is responsible for moving uploaded files to target experiment/analysis directory. - Show uploaded experiment files (in upload staging dir) on downloads tree. - Allow files to be downloaded from root experiment directory.

---
+++ trunk/scripts/gnomex_fdt_daemon.sh
@@ -0,0 +1,1 @@
+java -cp /usr/orion/applications/gnomex/gnomex.jar hci.gnomex.daemon.FDTUploadDaemon  >> /temp/gnomex_fdt_daemon.log $* &
--- trunk/src/hci/gnomex/controller/FastDataTransferUploadStart.java
+++ trunk/src/hci/gnomex/controller/FastDataTransferUploadStart.java
@@ -2,10 +2,16 @@ package hci.gnomex.controller;
 
 import hci.framework.control.Command;
 import hci.framework.control.RollBackCommandException;
+import hci.gnomex.constants.Constants;
+import hci.gnomex.model.Analysis;
+import hci.gnomex.model.Request;
+import hci.gnomex.security.SecurityAdvisor;
+import hci.gnomex.utility.DictionaryHelper;
 import hci.gnomex.utility.PropertyHelper;
 
 import java.io.File;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,42 +25,102 @@ public class FastDataTransferUploadStart extends GNomExCommand implements Serial
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(FastDataTransferUploadStart.class);
 
   private String serverName;
+  private Integer idAnalysis;
+  private Integer idRequest;
+  private String targetDir;
 
   public void validate() {
   }
 
   public void loadCommand(HttpServletRequest request, HttpSession session) {
     serverName = request.getServerName();     
+    
+    if (request.getParameter("idRequest") != null && !request.getParameter("idRequest").equals("")) {
+      idRequest = Integer.valueOf(request.getParameter("idRequest"));
+    }
+    if (request.getParameter("idAnalysis") != null && !request.getParameter("idAnalysis").equals("")) {
+      idAnalysis = Integer.valueOf(request.getParameter("idAnalysis"));
+    }
+    
+    if (idAnalysis == null && idRequest == null) {
+      this.addInvalidField("missing id", "idRequest or idAnalysis must be provided");
+    }
   }
 
   public Command execute() throws RollBackCommandException {
 
     try {
       Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
+      SecurityAdvisor secAdvisor = this.getSecAdvisor();
+      DictionaryHelper dh = DictionaryHelper.getInstance(sess);
+      SimpleDateFormat yearFormatter = new SimpleDateFormat("yyyy");
+      String createYear = "";
 
-      UUID uuid = UUID.randomUUID();
-      String uuidStr = uuid.toString();
-
-      // Directories must be created one level at a time so that permissions will be set properly in Linux			
-      String softlinks_dir = PropertyHelper.getInstance(sess).getFDTDirectoryForGNomEx(serverName)+uuidStr;				
-
-      File dir = new File(softlinks_dir);
-      boolean isDirCreated = dir.mkdir();  
-      if (!isDirCreated) {
-        this.addInvalidField("Error.", "Unable to create " + softlinks_dir + " directory.");		
-      }	        
-
-      // change ownership to fdt
-      Process process = Runtime.getRuntime().exec( new String[] { "chown", "-R", "fdt:fdt", softlinks_dir } );          
-      process.waitFor();
-      process.destroy();        
-      // only fdt user (and root) can read and write to this directory
-      process = Runtime.getRuntime().exec( new String[] { "chmod", "700", softlinks_dir } );          
-      process.waitFor();
-      process.destroy();        
+      if (idAnalysis != null) {
+        Analysis analysis = (Analysis)sess.get(Analysis.class, idAnalysis);
+        if (!secAdvisor.canUpdate(analysis)) {
+          this.addInvalidField("insufficient permissions", "insufficient permissions to upload analysis files");
+        }
+        if (this.isValid()) {
+          createYear = yearFormatter.format(analysis.getCreateDate());
+          String baseDir = dh.getAnalysisWriteDirectory(serverName);
+          targetDir = baseDir + File.separator + createYear + File.separator + analysis.getNumber();
+        }
+      } else if (idRequest != null) {
+        Request experiment = (Request)sess.get(Request.class, idRequest);
+        if (!secAdvisor.canUpdate(experiment)) {
+          this.addInvalidField("insufficient permissions", "insufficient permissions to upload experiment files");
+        }
+        if (this.isValid()) {
+          createYear = yearFormatter.format(experiment.getCreateDate());
+          String baseDir = dh.getMicroarrayDirectoryForWriting(serverName);
+          targetDir = baseDir + File.separator + createYear + File.separator + Request.getBaseRequestNumber(experiment.getNumber()) + File.separator + Constants.UPLOAD_STAGING_DIR;
+        }
+      }
+      if (this.isValid()) {
+        if (!new File(targetDir).exists()) {
+          File dir = new File(targetDir);
+          boolean success = dir.mkdirs();
+          if (!success) {
+            this.addInvalidField("mkdir", "Unable to make target dir");
+          }
+        }
+      }
       
+      if (this.isValid()) {
+        UUID uuid = UUID.randomUUID();
+        String uuidStr = uuid.toString();
 
-      this.xmlResult = "<FDTUploadUuid uuid='" + uuidStr + "'/>";
+        // Directories must be created one level at a time so that permissions will be set properly in Linux      
+        String softlinks_dir = PropertyHelper.getInstance(sess).getFDTDirectoryForGNomEx(serverName)+ File.separator + uuidStr;       
+
+        File dir = new File(softlinks_dir);
+        boolean isDirCreated = dir.mkdir();  
+        if (!isDirCreated) {
+          this.addInvalidField("Error.", "Unable to create " + softlinks_dir + " directory.");    
+        }         
+
+        // change ownership to fdt
+        Process process = Runtime.getRuntime().exec( new String[] { "chown", "-R", "fdt:fdt", softlinks_dir } );          
+        process.waitFor();
+        process.destroy();        
+        
+        // only fdt user (and root) can read and write to this directory
+        process = Runtime.getRuntime().exec( new String[] { "chmod", "700", softlinks_dir } );          
+        process.waitFor();
+        process.destroy();        
+        
+        // start daemon
+        process = Runtime.getRuntime().exec( new String[] { "gnomex_fdt_daemon", "-sourceDir", softlinks_dir, "-targetDir", targetDir } );          
+        process.waitFor();
+        process.destroy();        
+        
+
+        this.xmlResult = "<FDTUploadUuid uuid='" + uuidStr + "'/>";
+        
+      }
+      
+      
 
       if (isValid()) {
         setResponsePage(this.SUCCESS_JSP);
