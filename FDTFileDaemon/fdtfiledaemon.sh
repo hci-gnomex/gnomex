8850457449a50da8c84dc69b28c393d41fcd2134
HCI\rcundick <HCI\rcundick@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-04-06 20:06:36 +0000

    Updated FDTFileDaemon class path

--- FDTFileDaemon/fdtfiledaemon.sh
+++ FDTFileDaemon/fdtfiledaemon.sh
@@ -1,1 +1,1 @@
-java -cp /usr/orion/fdtfiledaemon/fdtfiledaemon.jar hci.gnomex.app.FDTFileDaemon -softlinksDirPath /mnt/fdt >> /temp/fdtfiledaemon.log $* &
+java -cp /usr/orion/fdtfiledaemon/fdtfiledaemon.jar hci.gnomex.daemon.FDTFileDaemon -softlinksDirPath /mnt/fdt >> /temp/fdtfiledaemon.log $* &
