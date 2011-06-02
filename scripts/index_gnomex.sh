0e62062f19f2fac1473adc2e3ad975e91fb503b0
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-06-02 15:48:50 +0000

    Add new SQL Server 2008 JDBC driver to classpath for batch job that creates lucene indices.

--- scripts/index_gnomex.sh
+++ scripts/index_gnomex.sh
@@ -11,7 +11,7 @@ fi
 LIB=$HIBERNATE_HOME/lib
 GNOMEX_JAR=$GNOMEX_HOME/gnomex.jar
 LUCENE_JAR=$GNOMEX_HOME/lib/lucene-core-2.2.0.jar
-JDBC_DRIVER=$ORION_HOME/lib/mysql-connector-java-5.1.7-bin.jar:$ORION_HOME/lib/msutil.jar:$ORION_HOME/lib/msbase.jar:$ORION_HOME/lib/mssqlserver.jar
+JDBC_DRIVER=$ORION_HOME/lib/mysql-connector-java-5.1.7-bin.jar:$ORION_HOME/lib/sqljdbc4.jar:$ORION_HOME/lib/msutil.jar:$ORION_HOME/lib/msbase.jar:$ORION_HOME/lib/mssqlserver.jar
 CLASSPATH=".:$GNOMEX_JAR:$GNOMEX_HOME/lib:$LUCENE_JAR:$JDBC_DRIVER:$HIBERNATE_HOME:$LIB/hibernate3.jar:$ORION_HOME/lib:$ORION_HOME/lib/hci_utils.jar:$ORION_HOME/lib/hci_framework.jar:$LIB/Hibernate3Utils.jar:$ORION_HOME/lib/dom4j-1.6.1.jar:$ORION_HOME/lib/log4j-1.2.11.jar:$ORION_HOME/lib/commons-logging-1.0.4.jar:$ORION_HOME/lib/commons-collections-2.1.1.jar:$ORION_HOME/lib/jta.jar:$ORION_HOME/lib/jdom.jar"
 for JAR in $LIB/*.jar
 do
