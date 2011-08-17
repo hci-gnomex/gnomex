6a04d4f9adc5eb955435e1300e3434f08e164f19
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-08-17 03:06:32 +0000

    Add missing q30Gb column to upgrade script.

--- sql/gnomex_db_upgrade_to_5.2.sql
+++ sql/gnomex_db_upgrade_to_5.2.sql
@@ -1,6 +1,7 @@
 -- Add FlowCellChannel columns for read1ClustersPassedFilterM, read2ClustersPassedFilterM, q30Gb
 alter table gnomex.FlowCellChannel add column read1ClustersPassedFilterM INT(10) NULL;
 alter table gnomex.FlowCellChannel add column read2ClustersPassedFilterM INT(10) NULL;
+alter table gnomex.FlowCellChannel add column q30Gb INT(10) NULL;
 
 
  
