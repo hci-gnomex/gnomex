46d17c0d7f50b37049813bed4340471b6e14ae7a
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-08-26 22:23:11 +0000

    Fix sql error.

--- sql/gnomex_db_upgrade_to_5.2.1.sql
+++ sql/gnomex_db_upgrade_to_5.2.1.sql
@@ -1,6 +1,6 @@
 INSERT INTO `gnomex`.`Property` (`propertyName`,`propertyValue`,`propertyDescription`, `forServerOnly`) 
   values ('usage_user_visibility', 'none', 'Permission level for non-admin gnomex users to view lab usage data. [none, masked, full]', 'N');
 
-alter table gnomex.FlowCellChannel alter column q30Gb q30Gb DECIMAL(4,1) null;
+alter table FlowCellChannel change column q30Gb q30Gb DECIMAL(4,1);
 
-alter table gnomex.Lab change column excludeUsage char(1) null;
+alter table Lab add column excludeUsage char(1) null;
