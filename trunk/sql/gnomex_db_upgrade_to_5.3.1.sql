f5d435aed5f92f129ea76712bae3e72c623fde12
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-10-11 02:17:40 +0000

    Fix sql syntax.

--- trunk/sql/gnomex_db_upgrade_to_5.3.1.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.3.1.sql
@@ -1,2 +1,5 @@
-alter table gnomex.FlowCellChannel change column idFlowCell INT(10) NULL;
-alter table gnomex.SequenceLane change column idFlowCellChannel INT(10) NULL;
+alter table gnomex.FlowCellChannel change column idFlowCell idFlowCell INT(10) NULL;
+alter table gnomex.SequenceLane change column idFlowCellChannel idFlowCellChannel INT(10) NULL;
+
+alter table gnomex.SequenceLane add column readCount INT(10) null;
+alter table gnomex.SequenceLane add column pipelineVersion varchar (10) null;
