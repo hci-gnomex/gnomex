82b3cbbc01eb124d68fc49ad0b5927f20318d2d9
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2011-10-18 19:01:37 +0000

    Added IlluminaExomeCapturePerCapPlugin to account for per capture pricing on exome captures.

---
+++ trunk/src/hci/gnomex/billing/IlluminaExomeCapturePerCapPlugin.java
@@ -0,0 +1,140 @@
+package hci.gnomex.billing;
+
+import hci.gnomex.model.BillingItem;
+import hci.gnomex.model.BillingPeriod;
+import hci.gnomex.model.BillingStatus;
+import hci.gnomex.model.BioanalyzerChipType;
+import hci.gnomex.model.Hybridization;
+import hci.gnomex.model.LabeledSample;
+import hci.gnomex.model.Price;
+import hci.gnomex.model.PriceCategory;
+import hci.gnomex.model.PriceCriteria;
+import hci.gnomex.model.Request;
+import hci.gnomex.model.Sample;
+import hci.gnomex.model.SequenceLane;
+import hci.gnomex.utility.DictionaryHelper;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hibernate.Session;
+
+
+public class IlluminaExomeCapturePerCapPlugin implements BillingPlugin {
+  // Capture is for up to six samples
+  static final int SAMPLES_PER_CAPTURE = 6;
+
+  public List constructBillingItems(Session sess, String amendState, BillingPeriod billingPeriod, PriceCategory priceCategory, Request request, 
+      Set<Sample> samples, Set<LabeledSample> labeledSamples, Set<Hybridization> hybs, Set<SequenceLane> lanes) {
+
+    List billingItems = new ArrayList<BillingItem>();
+    
+    if (samples == null || samples.size() == 0) {
+      return billingItems;
+    }
+
+    
+    // Generate the billing item.  Find the price using the
+    // criteria of the illumina application.
+    int numberOfSamples = 0;
+    
+    // Show the sample numbers in the notes
+    String notes = "";
+    for(Iterator i = samples.iterator(); i.hasNext();) {
+      Sample s = (Sample)i.next();
+      
+      if (notes.length() > 0) {
+        notes += ",";
+      }
+      notes += s.getNumber();
+      
+      // Only charge for lib prep if core is performing it.
+      if (s.getSeqPrepByCore() != null && s.getSeqPrepByCore().equals("Y")) {
+        numberOfSamples++;
+      } 
+      
+    }
+    
+    // If we don't have any samples that were prepped by core
+    // just bypass creating billing item.
+    if (numberOfSamples == 0) {
+      return billingItems;
+    }
+    
+    // Calculate number of captures (6 samples per capture)
+    int qty = 0;
+    if (numberOfSamples < SAMPLES_PER_CAPTURE) {
+      qty = 1;
+    } else {
+      int mod = numberOfSamples % SAMPLES_PER_CAPTURE;
+      if (mod == 0) {
+        qty = numberOfSamples / SAMPLES_PER_CAPTURE;
+      } else {
+        qty = numberOfSamples / SAMPLES_PER_CAPTURE;
+        qty++;
+      } 
+    }
+    
+    
+
+    // Find the price
+    Price price = null;
+    for(Iterator i1 = priceCategory.getPrices().iterator(); i1.hasNext();) {
+      Price p = (Price)i1.next();
+      if (p.getIsActive() != null && p.getIsActive().equals("Y")) {
+        for(Iterator i2 = p.getPriceCriterias().iterator(); i2.hasNext();) {
+          PriceCriteria criteria = (PriceCriteria)i2.next();
+          if (criteria.getFilter1().equals(request.getCodeApplication())) {          
+            price = p;
+            break;            
+          }
+        }
+      }
+    }
+
+    // Instantiate a BillingItem for the matched price
+    if (price != null) {
+      BigDecimal theUnitPrice = price.getUnitPrice();
+      if (request.getLab() != null && request.getLab().getIsExternalPricing() != null && request.getLab().getIsExternalPricing().equalsIgnoreCase("Y")) {
+        theUnitPrice = price.getUnitPriceExternal();
+      }
+
+      BillingItem billingItem = new BillingItem();
+      billingItem.setCategory(priceCategory.getName());
+      billingItem.setCodeBillingChargeKind(priceCategory.getCodeBillingChargeKind());
+      billingItem.setIdBillingPeriod(billingPeriod.getIdBillingPeriod());
+      billingItem.setDescription(price.getName());
+      billingItem.setQty(Integer.valueOf(qty));
+      billingItem.setUnitPrice(theUnitPrice);
+      billingItem.setPercentagePrice(new BigDecimal(1));
+      if (qty > 0 && theUnitPrice != null) {
+        billingItem.setTotalPrice(theUnitPrice.multiply(new BigDecimal(qty)));          
+      }
+      billingItem.setCodeBillingStatus(BillingStatus.PENDING);
+      billingItem.setIdRequest(request.getIdRequest());
+      billingItem.setIdBillingAccount(request.getIdBillingAccount());
+      billingItem.setIdLab(request.getIdLab());
+      billingItem.setIdPrice(price.getIdPrice());
+      billingItem.setIdPriceCategory(price.getIdPriceCategory());
+
+      // Hold off on saving the notes.  Need to reserve note field
+      // for complete date, etc at this time.
+      //billingItem.setNotes(notes);
+
+
+      billingItems.add(billingItem);
+
+    }
+    
+    
+    return billingItems;
+  }
+
+  
+
+}
