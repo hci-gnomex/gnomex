6f73cbb1ef09b9736af33cb3fb29677e1466fb95
HCI\u0395021 <HCI\u0395021@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-01-31 02:34:16 +0000

    An experiment queue that allows a user to see where there experiment is at in the queue

--- src/hci/gnomex/controller/ExperimentQueue.java
+++ src/hci/gnomex/controller/ExperimentQueue.java
@@ -36,40 +36,40 @@ import java.util.Set;
 
 
 public class ExperimentQueue extends HttpServlet {
-  
+
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ExperimentQueue.class);
-  
+
   private Document doc;
-  
+
   ArrayList<String> listItems;
 
   public void init() throws ServletException { 
     listItems = new ArrayList<String>();
   }
-  
+
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     doPost(request, response);
   }
-  
+
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     try{
-      
+
       String xmlResult;
-      
+
       Session sess = HibernateGuestSession.currentGuestSession(request.getUserPrincipal().getName());
-      
+
       HttpSession session = request.getSession();
-      
+
       Element root = new Element("HTML");
       doc = new Document(root);
-      
+
       Element head = new Element("HEAD");
       root.addContent(head);
-      
+
       Element body = new Element("BODY");
       body.setAttribute("style", "background-color:whitesmoke");
       root.addContent(body);
-      
+
       Element mainHead = new Element("H1");
       Element hr = new Element("HR");
       Element a = new Element("A");
@@ -78,20 +78,21 @@ public class ExperimentQueue extends HttpServlet {
       mainHead.addContent(a);
       mainHead.addContent(hr);
       body.addContent(mainHead);
-      
+
       Element descriptionHeader = new Element("H4");
       descriptionHeader.addContent("Estimated Sequencing Time");
       body.addContent(descriptionHeader);
-      
+
       Element description = new Element("P");
       Element hr1 = new Element("HR");
       description.addContent("In order to keep Illumina sequencing costs down, samples are run in batches. These batches are dictated by a flowcell. An Illumina flowcell contains eight lanes and each lane must be filled with samples prior to starting a sequencing run. Samples are run on a flowcell according to the date in which the samples were received. Genome Analzyer flowcells often require one of the eight lanes to be a genomic DNA control. Between one and twelve samples can be placed in each lane if multiplexing of samples is implemented. All samples on the same flowcell will experience the same type of run (single end read or paired end read) and the same number of sequencing cycles (36, 50, 76 or 101). These factors create challenges in providing firm guidelines in the estimated time frame from drop off of sample to delivery of data. We attempt to have a five week turnaround for samples that are run on single end read sequencing flowcells (although we have been set back a bit while going through the recent HiSeq training). Paired end sequencing runs only represent approximately 20% of the flowcells that are run at this core facility. Therefore, the length of time from submission of a sample to the completion of sequencing can be somewhat longer for paired end libraries. A sequencing run can last take from three to ten days. Pipeline analysis of the sequence data can take an additional one to three days. ");
       description.addContent(hr1);
       body.addContent(description);
-      
+
       StringBuffer query = new StringBuffer("SELECT sp from NumberSequencingCyclesAllowed sp");
-      List nsca = sess.createQuery(query.toString()).list();
       
+      List nsca = sess.createQuery(query.toString()).list();
+
       for(Object temp : nsca)
       {
         NumberSequencingCyclesAllowed cycle = (NumberSequencingCyclesAllowed)temp;
@@ -99,22 +100,21 @@ public class ExperimentQueue extends HttpServlet {
         //Start for loop here  for(Dictionary Entry : numbersequencingcyclesAllowed){
         StringBuffer query1 = new StringBuffer("select seqLane.number, flowCell.lastCycleDate, " +
             "user.lastName, user.firstName from Request as r, FlowCellChannel as flowCell " +
-            "join r.appUser as user " +
-            "join r.sequenceLanes as seqLane " +
+            "left join r.appUser as user " +
+            "left join r.sequenceLanes as seqLane " +
             "where r.completedDate is NULL and " +
-            "seqLane.idRequest = r.idRequest and " +
             "flowCell.pipelineFailed = 'N' and flowCell.lastCycleFailed = 'N' and " +
-            "seqLane.idSeqRunType = " + cycle.getIdSeqRunType() + " and seqLane.idNumberSequencingCycles = " + cycle.getIdNumberSequencingCycles() + 
-            " and seqLane.idFlowCellChannel = flowCell.idFlowCellChannel " +
-            "Order by flowCell.lastCycleDate asc");
+            "seqLane.idSeqRunType = " + cycle.getIdSeqRunType() + " and seqLane.idNumberSequencingCycles = " + cycle.getIdNumberSequencingCycles() +
+            "and seqLane.idFlowCellChannel = flowCell.idFlowCellChannel " +
+        "Order by flowCell.lastCycleDate asc");
 
         List<Object[]> sqlResults = sess.createQuery(query1.toString()).list();
-        
+
         if (sqlResults.size() == 0)
           continue;
         listItems.add(cycle.getName());
       }
-      
+
       orderList(listItems);
 
       Element hrefTable = new Element("TABLE");
@@ -129,72 +129,90 @@ public class ExperimentQueue extends HttpServlet {
       contentNav.addContent(makeList(listItems));
       hrefTable.addContent(contentNav);
       body.addContent(hrefTable);
-      
+
       for(String temp : listItems)
       {
-        
+
         StringBuffer query1 = new StringBuffer("SELECT n from NumberSequencingCyclesAllowed n" +
-        		" where n.name = " + "'" + temp + "'");
+            " where n.name = " + "'" + temp + "'");
         List results = sess.createQuery(query1.toString()).list();
-        
+
         NumberSequencingCyclesAllowed cycle = (NumberSequencingCyclesAllowed)results.get(0);
-        
 
-      //Start for loop here  for(Dictionary Entry : numbersequencingcyclesAllowed){
-      StringBuffer query2 = new StringBuffer("select seqLane.number, flowCell.lastCycleDate, " +
-      		"user.lastName, user.firstName from Request as r, FlowCellChannel as flowCell " +
-          "join r.appUser as user " +
-      		"join r.sequenceLanes as seqLane " +
-      		"where r.completedDate is NULL and " +
-      		"seqLane.idRequest = r.idRequest and " +
-      		"flowCell.pipelineFailed = 'N' and " +
-      		"seqLane.idSeqRunType = " + cycle.getIdSeqRunType() + " and seqLane.idNumberSequencingCycles = " + cycle.getIdNumberSequencingCycles() + 
-      		" and seqLane.idFlowCellChannel = flowCell.idFlowCellChannel " + 
-      		"Order by flowCell.lastCycleDate asc");
 
-      List<Object[]> sqlResults = sess.createQuery(query2.toString()).list();
-      
-      if (sqlResults.size() == 0)
-        continue;
-      
-      Element heading = new Element("H2");
-      Element bold = new Element("B");
-      Element anchor = new Element("A");
-      Element hr4 = new Element("HR");
-      anchor.setAttribute("name", cycle.getName());
-      heading.addContent(anchor);
-      heading.addContent(cycle.getName());
-      heading.addContent(bold);
-      heading.addContent(hr4);
-      body.addContent(heading);
-      
-      body.addContent(CreateTable(sqlResults));
-      
-      Element anchor1 = new Element("A");
-      anchor1.addContent("Back to Top");
-      anchor1.setAttribute("href", "#Illumina");
-      body.addContent(anchor1);
-      
+        //Start for loop here  for(Dictionary Entry : numbersequencingcyclesAllowed){
+        StringBuffer query2 = new StringBuffer("select seqLane.number, " +
+            "flowCell.lastCycleDate, " +
+            "user.lastName, user.firstName, seqLane.idFlowCellChannel, flowCell.pipelineDate, " +
+            "samp.qualDate, samp.seqPrepByCore, samp.seqPrepDate, flowCell.clustersPerTile, flowCell.pipelineFailed, flowCell.firstCycleDate, flowCell.pipelineDate " +
+            "from Request as r " +
+            "left join r.appUser as user " +
+            "left join r.sequenceLanes as seqLane " +
+            "left join seqLane.flowCellChannel as flowCell "+
+            "left join seqLane.sample as samp "+
+            "where (flowCell.pipelineDate is NULL or " +
+            "r.completedDate is NULL) and " +
+            "seqLane.idSeqRunType = " + cycle.getIdSeqRunType() + " and seqLane.idNumberSequencingCycles = " + cycle.getIdNumberSequencingCycles() +
+            "and flowCell.lastCycleFailed = 'N' and flowCell.firstCycleFailed = 'N' and flowCell.pipelineFailed = 'N' "+
+        "Order by flowCell.lastCycleDate asc");
+
+//        StringBuffer status = new StringBuffer("select samp.qualDate, samp.seqPrepByCore, samp.seqPrepDate, seqLane.idFlowCellChannel, "+
+//            "flow.pipelineDate, seqLane.number, flow.clustersPerTile "+
+//            "from Sample as samp, SequenceLane as seqLane, FlowCellChannel as flow, Request as r "+
+//            "where samp.idSample = seqLane.idSample and " +
+//            "seqLane.idFlowCellChannel = flow.idFlowCellChannel and "+
+//            "samp.idRequest = r.idRequest and "+
+//            "r.completedDate is NULL or "+
+//            "flowCell.pipelineDate is NULL) and "+
+//            "flow.lastCycleFailed = 'N' and "+
+//            "flow.pipelineFailed = 'N' and "+
+//        "flow.firstCycleFailed = 'N'");
+
+        List<Object[]> sqlResults = sess.createQuery(query2.toString()).list();
+
+        //statusResults = sess.createQuery(status.toString()).list();
+
+        if (sqlResults.size() == 0)
+          continue;
+
+        Element heading = new Element("H2");
+        Element bold = new Element("B");
+        Element anchor = new Element("A");
+        Element hr4 = new Element("HR");
+        anchor.setAttribute("name", cycle.getName());
+        heading.addContent(anchor);
+        heading.addContent(cycle.getName());
+        heading.addContent(bold);
+        heading.addContent(hr4);
+        body.addContent(heading);
+
+        body.addContent(CreateTable(sqlResults));
+
+        Element anchor1 = new Element("A");
+        anchor1.addContent("Back to Top");
+        anchor1.setAttribute("href", "#Illumina");
+        body.addContent(anchor1);
+
       }
 
       //end loop here.  Will repeat for all entries in nsca dictionary in order
       //to make a table for each type of sequencing experiment
-      
-      
+
+
       XMLOutputter out = new org.jdom.output.XMLOutputter();
       out.setOmitEncoding(true);
       xmlResult = out.outputString(doc);
       xmlResult = xmlResult.replaceAll("&amp;", "&");
       xmlResult = xmlResult.replaceAll("�",     "&micro");
       log.debug(xmlResult);
-      
+
       response.setContentType("text/html");
       PrintWriter outWriter = response.getWriter();
       outWriter.println(xmlResult);
       outWriter.close();
-      
+
     }
-    
+
     catch (Exception e) {
       log.error("An exception has occurred in ExperimentQueue ", e);
       e.printStackTrace();
@@ -209,35 +227,35 @@ public class ExperimentQueue extends HttpServlet {
       } catch (Exception e) {
       }
     }
-  
+
   }
 
 
   private void orderList(ArrayList<String> listItems) {
 
     String temp = "";
-    
+
     String[] tempi;
     String[] tempj;
-    
+
     for(int i = 1; i < listItems.size(); i++){
       temp = listItems.get(i);
       tempi = listItems.get(i).split(" ");
       int j = i;
       tempj = listItems.get(j - 1).split(" ");
-      
+
       while(j > 0 && Integer.parseInt(tempj[0]) > Integer.parseInt(tempi[0])){
         listItems.set(j, listItems.get(j-1));
         j--;
         if(j-1 > 0)
           tempj = listItems.get(j-1).split(" ");
-          
+
       }
-      
+
       listItems.set(j, temp);
     }
-    
-    
+
+
   }
 
   private Element makeList(ArrayList<String> s){
@@ -253,73 +271,99 @@ public class ExperimentQueue extends HttpServlet {
     }
 
     return orderedList;
-    
+
   }
   private Element CreateTable(List<Object[]> sqlResults) {
-    
+
     Element table = new Element("TABLE");
     table.setAttribute("border", "2");
     table.setAttribute("cellpadding", "2");
     table.addContent(makeHeaderRow());
-    
+
 
     for(int i = 0; i < sqlResults.size(); i++)
     {
-      Object[] temp = sqlResults.get(i); 
-      if(temp[1] == null)
-        continue;
+      Object[] temp = sqlResults.get(i);
+
       String submitterInitials = (((String) temp[2]).substring(0, 1)) + (((String) temp[3]).substring(0, 1));
-      
+
       String sampleId = (String)temp[0];
-      
+
+      String status = getStatus(temp);
+
       Date dateReceived = (Date)temp[1];
- 
-      table.addContent(makeRow(sampleId, dateReceived, submitterInitials));
+
+      table.addContent(makeRow(sampleId, dateReceived, submitterInitials, status));
     }
-      return table;
+    return table;
   }
 
- 
-  
-  private Element makeRow(String sampleId, Date dateReceived, String initials)
+  private String getStatus(Object[] temp){
+      if(temp[6] == null){// if sample quality date is null
+        return "Sample Quality";
+      }
+      else if(temp[7] == null){ //if seqPrepByCore is null
+        return "Library Prep Stage";
+      }
+      else if(temp[11] == null){ // if first cycle date is null
+        return "Cluster Generation";
+      }
+      else if(temp[12] == null){ // if data pipeline date is null
+        return "Sequence Run";
+      }
+      else
+        return "Data Pipeline Stage";
+  }
+
+  private Element makeRow(String sampleId, Date dateReceived, String initials, String status)
   {
     Element row = new Element("TR");
     Element cell = new Element("TD");
     cell.addContent(sampleId);
     row.addContent(cell);
-    
+
     cell = new Element("TD");
     cell.addContent(dateReceived.toString());
     row.addContent(cell);
-    
+
     cell = new Element("TD");
     cell.addContent(initials);
     row.addContent(cell);
-    
+
+    cell = new Element("TD");
+    cell.addContent(status);
+    row.addContent(cell);
+
+
     return row;
 
   }
-  
+
   private Element makeHeaderRow()
   {
     Element headerRow = new Element("TR");
-    
+
     Element sampleId = new Element("TH");
     sampleId.setAttribute("width", "100");
     sampleId.addContent("Sample ID");
-    
+
     Element dateReceived = new Element("TH");
     dateReceived.setAttribute("width", "100");
     dateReceived.addContent("Date Received");
-    
+
     Element initials = new Element("TH");
     initials.setAttribute("width", "100");
     initials.addContent("Initials");
-    
+
+    Element status = new Element("TH");
+    status.setAttribute("width", "100");
+    status.addContent("Current Stage");
+
     headerRow.addContent(sampleId);
     headerRow.addContent(dateReceived);
     headerRow.addContent(initials);
-    
+    headerRow.addContent(status);
+
     return headerRow;
   }
 
@@ -329,6 +373,6 @@ public class ExperimentQueue extends HttpServlet {
 }
 
 
-  
+
 
 
