ca3ebab5cb0c32497bda9a8b3ce2a6377062a150
HCI\u0763721 <HCI\u0763721@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-02-07 18:21:55 +0000

    Add new splitType column

--- sql/gnomex_db_upgrade_to_5.3.6.0.sql
+++ sql/gnomex_db_upgrade_to_5.3.6.0.sql
@@ -2,3 +2,10 @@ use gnomex;
 
 --Rename TotalPrice to InvoicePrice.
 alter table BillingItem change totalPrice invoicePrice decimal(8,2) not null;
+
+-- Add splitType
+alter table BillingItem add column  `splitType` char(1) NULL;
+
+-- Initialize split type
+update BillingItem set splitType='%' where splitType is null
+
