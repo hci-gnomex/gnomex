257f2e869c44f7d7544b486bd286c9b35d4b2016
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-02-08 03:33:32 +0000

    Started introducing capillary sequencing experiment to submit request window.

---
+++ src/hci/gnomex/billing/CapSeqPlugin.java
@@ -0,0 +1,116 @@
+package hci.gnomex.billing;
+
+import hci.gnomex.constants.Constants;
+import hci.gnomex.model.Application;
+import hci.gnomex.model.BillingItem;
+import hci.gnomex.model.BillingPeriod;
+import hci.gnomex.model.BillingStatus;
+import hci.gnomex.model.BioanalyzerChipType;
+import hci.gnomex.model.Hybridization;
+import hci.gnomex.model.LabeledSample;
+import hci.gnomex.model.Price;
+import hci.gnomex.model.PriceCategory;
+import hci.gnomex.model.PriceCriteria;
+import hci.gnomex.model.Request;
+import hci.gnomex.model.RequestCategory;
+import hci.gnomex.model.Sample;
+import hci.gnomex.model.SequenceLane;
+import hci.gnomex.utility.DictionaryHelper;
+import hci.gnomex.utility.RequestParser;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hibernate.Session;
+
+
+public class CapSeqPlugin implements BillingPlugin {
+
+  public List constructBillingItems(Session sess, String amendState, BillingPeriod billingPeriod, PriceCategory priceCategory, Request request, 
+      Set<Sample> samples, Set<LabeledSample> labeledSamples, Set<Hybridization> hybs, Set<SequenceLane> lanes) {
+    
+    
+    List billingItems = new ArrayList<BillingItem>();
+    
+    if (samples == null || samples.size() == 0) {
+      return billingItems;
+    }
+    
+    
+
+    // Count up number of samples for each codeBioanalyzerChipType
+    int qty = 0;
+    for(Iterator i = samples.iterator(); i.hasNext();) {
+      Sample sample = (Sample)i.next();
+      qty++;
+    }
+    
+      
+    // Find the price for capillary sequencing
+    Price price = null;
+    for(Iterator i1 = priceCategory.getPrices().iterator(); i1.hasNext();) {
+      Price p = (Price)i1.next();
+      if (p.getIsActive() != null && p.getIsActive().equals("Y")) {
+        /*
+        for(Iterator i2 = p.getPriceCriterias().iterator(); i2.hasNext();) {
+          PriceCriteria criteria = (PriceCriteria)i2.next();
+          if (criteria.getFilter1().equals(filter1)) {
+            if (filter2.equals("null")) {
+              if (criteria.getFilter2() == null || criteria.getFilter2().equals("")) {
+                price = p;
+                break;              
+                
+              }
+            } else {
+              if (criteria.getFilter2() != null && criteria.getFilter2().equals(filter2)) {
+                price = p;
+                break;
+              }
+            }
+          } 
+        }
+        */
+        price = p;
+      }
+    }
+    
+    // Instantiate a BillingItem for the matched billing price
+    if (price != null) {
+      BigDecimal theUnitPrice = price.getUnitPrice();
+      if (request.getLab() != null && request.getLab().getIsExternalPricing() != null && request.getLab().getIsExternalPricing().equalsIgnoreCase("Y")) {
+        theUnitPrice = price.getUnitPriceExternal();
+      }
+      
+      BillingItem billingItem = new BillingItem();
+      billingItem.setCodeBillingChargeKind(priceCategory.getCodeBillingChargeKind());
+      billingItem.setIdBillingPeriod(billingPeriod.getIdBillingPeriod());
+      billingItem.setDescription(price.getName());
+      billingItem.setQty(qty);
+      billingItem.setUnitPrice(theUnitPrice);
+      billingItem.setPercentagePrice(new BigDecimal(1));        
+      if (qty > 0 && theUnitPrice != null) {      
+        billingItem.setInvoicePrice(theUnitPrice.multiply(new BigDecimal(qty)));
+      }
+      billingItem.setCodeBillingStatus(BillingStatus.PENDING);
+      billingItem.setIdRequest(request.getIdRequest());
+      billingItem.setIdLab(request.getIdLab());
+      billingItem.setIdBillingAccount(request.getIdBillingAccount());        
+      billingItem.setIdPrice(price.getIdPrice());
+      billingItem.setIdPriceCategory(priceCategory.getIdPriceCategory());
+      billingItem.setCategory(priceCategory.getName());
+    
+      
+      billingItems.add(billingItem);
+      
+    }
+    
+    
+    return billingItems;
+  }
+  
+}
