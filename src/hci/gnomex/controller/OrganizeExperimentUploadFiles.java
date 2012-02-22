53b69e0c789c758fed79b4c022a334abb7cb85f2
HCI\u0351456 <HCI\u0351456@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-02-22 16:50:49 +0000

    Cleaned up some code that I had previously just commented out

--- src/hci/gnomex/controller/OrganizeExperimentUploadFiles.java
+++ src/hci/gnomex/controller/OrganizeExperimentUploadFiles.java
@@ -30,12 +30,12 @@ import org.jdom.output.XMLOutputter;
 
 
 public class OrganizeExperimentUploadFiles extends GNomExCommand implements Serializable {
-  
- 
-  
+
+
+
   // the static field for logging in Log4J
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(OrganizeExperimentUploadFiles.class);
-  
+
   private Integer                      idRequest;
   private String                       filesXMLString;
   private Document                     filesDoc;
@@ -43,25 +43,25 @@ public class OrganizeExperimentUploadFiles extends GNomExCommand implements Seri
   private Document                     filesToRemoveDoc;
   private FileDescriptorUploadParser   parser;
   private FileDescriptorUploadParser   filesToRemoveParser;
-  
+
   private String                       serverName;
-  
+
   private DictionaryHelper             dictionaryHelper = null;
-  
+
   public void validate() {
   }
-  
+
   public void loadCommand(HttpServletRequest request, HttpSession session) {
-    
+
     if (request.getParameter("idRequest") != null && !request.getParameter("idRequest").equals("")) {
       idRequest = new Integer(request.getParameter("idRequest"));
     } else {
       this.addInvalidField("idRequest", "idRequest is required");
     }
-    
+
     if (request.getParameter("filesXMLString") != null && !request.getParameter("filesXMLString").equals("")) {
       filesXMLString = request.getParameter("filesXMLString");
-      
+
       StringReader reader = new StringReader(filesXMLString);
       try {
         SAXBuilder sax = new SAXBuilder();
@@ -72,10 +72,10 @@ public class OrganizeExperimentUploadFiles extends GNomExCommand implements Seri
         this.addInvalidField( "FilesLXMLString", "Invalid files xml");
       }
     }
-    
+
     if (request.getParameter("filesToRemoveXMLString") != null && !request.getParameter("filesToRemoveXMLString").equals("")) {
       filesToRemoveXMLString = "<FilesToRemove>" + request.getParameter("filesToRemoveXMLString") +  "</FilesToRemove>";
-      
+
       StringReader reader = new StringReader(filesToRemoveXMLString);
       try {
         SAXBuilder sax = new SAXBuilder();
@@ -88,141 +88,122 @@ public class OrganizeExperimentUploadFiles extends GNomExCommand implements Seri
     }
 
     serverName = request.getServerName();
-    
+
   }
 
   public Command execute() throws RollBackCommandException {
-    
+
     Session sess = null;
     if (filesXMLString != null) {
       try {
         sess = this.getSecAdvisor().getHibernateSession(this.getUsername());
-        
+
         Request request = (Request)sess.load(Request.class, idRequest);
         String baseDir = PropertyDictionaryHelper.getInstance(sess).getMicroarrayDirectoryForWriting(serverName);
         baseDir += "/" + request.getCreateYear() + "/" + Request.getBaseRequestNumber(request.getNumber());
-        
-        
+
+
         if (this.getSecAdvisor().canUpdate(request)) {
-          
-//          if (request.getIsExternal() != null && request.getIsExternal().equals("Y")) {
-          if (request.getIsExternal()!= null || request.getIsExternal()==null) {
-            parser.parse();
-            
-            // Add new directories to the file system
-            for (Iterator i = parser.getNewDirectoryNames().iterator(); i.hasNext();) {
-              String directoryName = (String)i.next();
-              File dir = new File(baseDir + "/" + directoryName);
-              if (!dir.exists()) {
-                boolean success = dir.mkdirs();
-                if (!success) { 
-                  // File was not successfully deleted
-                  throw new Exception("Unable to create directory " + directoryName);
-                }
-                
-              }
-            }
-            
-            
-            // Move files to designated folder
-            for(Iterator i = parser.getFileNameMap().keySet().iterator(); i.hasNext();) {
-              String directoryName = (String)i.next();
-              List fileNames = (List)parser.getFileNameMap().get(directoryName);
-              
-              for(Iterator i1 = fileNames.iterator(); i1.hasNext();) {
-                String fileName = (String)i1.next();
-                
-                File sourceFile = new File(fileName);
-                String targetDirName = baseDir + "/" + directoryName;
-                File targetDir = new File(targetDirName);
-                
-                if (!targetDir.exists()) {
-                  boolean success = targetDir.mkdirs();
-                  if (!success) {
-                    throw new Exception("Unable to create directory " + targetDir.getCanonicalPath());                    
-                  }
-                }
 
-                // Don't try to move if the file is in the same directory
-                /*String td = targetDirName.replaceAll("\\\\", "_");
-                td = td.replaceAll("/", "_");
-                td = td.replaceAll("__", "_");
-                String spath = sourceFile.getAbsolutePath().replaceAll("\\\\", "_");
-                spath = spath.replaceAll("/", "_");
-                spath = spath.replaceAll("__", "_");
-                
-                if (spath.startsWith(td)) {
-                  continue;
-                }*/
-                
-                String td = targetDir.getAbsolutePath();
-                String sd = sourceFile.getAbsolutePath();
-                sd = sd.substring(0,sd.lastIndexOf(File.separator));
-                
-                if ( td.equals(sd)) {
-                  continue;
-                }
-                
-                File destFile = new File(targetDir, sourceFile.getName());
-                boolean success = sourceFile.renameTo(destFile);
-                /*if (!success) {
-                  // File was not successfully moved
-                  throw new Exception("Unable to move file " + fileName + " to " + targetDirName);
-                }*/
-                if (!success) {
-                  if ( destFile.exists() ) {
-                    if ( sourceFile.exists() ) {
-                      if ( !sourceFile.delete() ) {
-                        throw new Exception("Unable to move file " + fileName + " to " + targetDirName);
-                      }
-                    }
-                  } else {
-                    throw new Exception("Unable to move file " + fileName + " to " + targetDirName);
-                  }
-                }
-                
-              }
-              
-            }
-            
-            // Remove files from file system
-            if (filesToRemoveParser != null) {
-              for (Iterator i = filesToRemoveParser.parseFilesToRemove().iterator(); i.hasNext();) {
-                String fileName = (String)i.next();
-                
-                // Remove references of file in TransferLog
-                String queryBuf = "SELECT tl from TransferLog tl where tl.idRequest = " + idRequest + " AND tl.fileName like '%" + new File(fileName).getName() + "'";
-                List transferLogs = sess.createQuery(queryBuf).list();
-                // Go ahead and delete the transfer log if there is just one row.
-                // If there are multiple transfer log rows for this filename, just
-                // bypass deleting the transfer log since it is not possible
-                // to tell which entry should be deleted.
-                if (transferLogs.size() == 1) {
-                  TransferLog transferLog = (TransferLog)transferLogs.get(0);
-                  sess.delete(transferLog);
-                } 
+          parser.parse();
 
-                boolean success = new File(fileName).delete();
-                if (!success) { 
-                  // File was not successfully deleted
-                  throw new Exception("Unable to delete file " + fileName);
-                }
-                
-
+          // Add new directories to the file system
+          for (Iterator i = parser.getNewDirectoryNames().iterator(); i.hasNext();) {
+            String directoryName = (String)i.next();
+            File dir = new File(baseDir + "/" + directoryName);
+            if (!dir.exists()) {
+              boolean success = dir.mkdirs();
+              if (!success) { 
+                // File was not successfully deleted
+                throw new Exception("Unable to create directory " + directoryName);
               }
-              sess.flush();
+
             }
-            
-            
-            
-            XMLOutputter out = new org.jdom.output.XMLOutputter();
-            this.xmlResult = "<SUCCESS/>";
-            setResponsePage(this.SUCCESS_JSP);          
-            
-          } else {
-            this.addInvalidField("Not external experiment", "This command is only valid for external experiments");
-            setResponsePage(this.ERROR_JSP);           
           }
+
+
+          // Move files to designated folder
+          for(Iterator i = parser.getFileNameMap().keySet().iterator(); i.hasNext();) {
+            String directoryName = (String)i.next();
+            List fileNames = (List)parser.getFileNameMap().get(directoryName);
+
+            for(Iterator i1 = fileNames.iterator(); i1.hasNext();) {
+              String fileName = (String)i1.next();
+
+              File sourceFile = new File(fileName);
+              String targetDirName = baseDir + "/" + directoryName;
+              File targetDir = new File(targetDirName);
+
+              if (!targetDir.exists()) {
+                boolean success = targetDir.mkdirs();
+                if (!success) {
+                  throw new Exception("Unable to create directory " + targetDir.getCanonicalPath());                    
+                }
+              }
+
+              // Don't try to move if the file is in the same directory
+
+              String td = targetDir.getAbsolutePath();
+              String sd = sourceFile.getAbsolutePath();
+              sd = sd.substring(0,sd.lastIndexOf(File.separator));
+
+              if ( td.equals(sd)) {
+                continue;
+              }
+
+              File destFile = new File(targetDir, sourceFile.getName());
+              boolean success = sourceFile.renameTo(destFile);
+
+
+              if (!success) {
+                if ( destFile.exists() ) {
+                  if ( sourceFile.exists() ) {
+                    if ( !sourceFile.delete() ) {
+                      throw new Exception("Unable to move file " + fileName + " to " + targetDirName);
+                    }
+                  }
+                } else {
+                  throw new Exception("Unable to move file " + fileName + " to " + targetDirName);
+                }
+              }
+
+            }
+
+          }
+
+          // Remove files from file system
+          if (filesToRemoveParser != null) {
+            for (Iterator i = filesToRemoveParser.parseFilesToRemove().iterator(); i.hasNext();) {
+              String fileName = (String)i.next();
+
+              // Remove references of file in TransferLog
+              String queryBuf = "SELECT tl from TransferLog tl where tl.idRequest = " + idRequest + " AND tl.fileName like '%" + new File(fileName).getName() + "'";
+              List transferLogs = sess.createQuery(queryBuf).list();
+              // Go ahead and delete the transfer log if there is just one row.
+              // If there are multiple transfer log rows for this filename, just
+              // bypass deleting the transfer log since it is not possible
+              // to tell which entry should be deleted.
+              if (transferLogs.size() == 1) {
+                TransferLog transferLog = (TransferLog)transferLogs.get(0);
+                sess.delete(transferLog);
+              } 
+
+              boolean success = new File(fileName).delete();
+              if (!success) { 
+                // File was not successfully deleted
+                throw new Exception("Unable to delete file " + fileName);
+              }
+
+
+            }
+            sess.flush();
+          }
+          
+          
+          XMLOutputter out = new org.jdom.output.XMLOutputter();
+          this.xmlResult = "<SUCCESS/>";
+          setResponsePage(this.SUCCESS_JSP);          
+
         } else {
           this.addInvalidField("Insufficient permissions", "Insufficient permission to organize uploaded files");
           setResponsePage(this.ERROR_JSP);
@@ -233,26 +214,26 @@ public class OrganizeExperimentUploadFiles extends GNomExCommand implements Seri
         log.error("An exception has occurred in OrganizeExperimentUploadFiles ", e);
         e.printStackTrace();
         throw new RollBackCommandException(e.getMessage());
-          
+
       }finally {
         try {
           if (sess != null) {
             this.getSecAdvisor().closeHibernateSession();
           }
         } catch(Exception e) {
-          
+
         }
       }
-      
+
     } else {
       this.xmlResult = "<SUCCESS/>";
       setResponsePage(this.SUCCESS_JSP);
     }
-    
+
     return this;
   }
-  
 
-  
+
+
 
 }
