41af340f1305ecc01b945bc1a9e7ba20d4ab0717
HCI\u0763721 <HCI\u0763721@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-03-06 19:01:17 +0000

    Fix constraint definition.

--- trunk/sql/gnomex_db_upgrade_to_5.3.9.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.3.9.0.sql
@@ -3,6 +3,6 @@
 alter table PropertyEntry add column idAnalysis int(10) null;
 alter table PropertyEntry add   
 CONSTRAINT `FK_PropertyEntry_Analysis` FOREIGN KEY `FK_PropertyEntry_Analysis` (`idAnalysis`)
-    REFERENCES DataTrack (`idAnalysis`)
+    REFERENCES Analysis (`idAnalysis`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
