e9f507b554b7fc0740a7f559e2bab5812b7fe033
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-04-06 21:10:45 +0000

    make column not-nullable.

--- trunk/sql/gnomex_db_upgrade_to_5.4.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.4.0.0.sql
@@ -9,6 +9,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 alter table gnomex.RequestCategory drop foreign key FK_RequestCategory_SamplePrepMethod;
 alter table gnomex.RequestCategory drop column idSamplePrepMethod;
 
+-- Allow nulls for idGenomeBuild, sortOrder on Segment
+alter table gnomex.Segment change column idGenomeBuild idGenomeBuild INT(10) NULL;
+alter table gnomex.Segment change column sortOrder sortOrder INT(10) unsigned NULL;
+
  
 
 
