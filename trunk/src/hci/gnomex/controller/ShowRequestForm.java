dc62fcee4661c68b4b53ec434d36105550f8f769
HCI\u0395021 <HCI\u0395021@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-04-19 16:00:35 +0000

    Added code that will put the capture lib design in the header if the experiment has a cap lib design

--- trunk/src/hci/gnomex/controller/ShowRequestForm.java
+++ trunk/src/hci/gnomex/controller/ShowRequestForm.java
@@ -50,31 +50,31 @@ import org.jdom.output.XMLOutputter;
 
 
 public class ShowRequestForm extends GNomExCommand implements Serializable {
-  
+
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetRequest.class);
-  
+
   public String SUCCESS_JSP = "/getHTML.jsp";
-  
+
   private Integer          idRequest;
   private Request          request;
-  
+
   private String           amendState = "";
-  
+
   private String           appURL = "";
-  
+
   private AppUser          appUser;
   private BillingAccount   billingAccount;
   private Project          project;
   private Lab              lab;
-  
-  private DictionaryHelper dictionaryHelper;
-  
-  
 
-  
+  private DictionaryHelper dictionaryHelper;
+
+
+
+
   public void validate() {
   }
-  
+
   public void loadCommand(HttpServletRequest request, HttpSession session) {
 
     if (request.getParameter("idRequest") != null) {
@@ -93,20 +93,20 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
   }
 
   public Command execute() throws RollBackCommandException {
-    
+
     try {
-      
-   
+
+
       Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
-      
-     
+
+
       dictionaryHelper = DictionaryHelper.getInstance(sess);
-    
+
       request = (Request)sess.get(Request.class, idRequest);
       if (request == null) {
         this.addInvalidField("no request", "Request not found");
       }
-      
+
 
       if (this.isValid()) {
         if (this.getSecAdvisor().canRead(request)) { 
@@ -116,17 +116,17 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
           if (request.getIdBillingAccount() != null) {
             billingAccount = (BillingAccount)sess.get(BillingAccount.class, request.getIdBillingAccount());
           }
-          
+
           RequestHTMLFormatter formatter = new RequestHTMLFormatter(this.getSecAdvisor(), request, appUser, billingAccount, dictionaryHelper);
-          
+
           if (this.getSecAdvisor().canRead(request)) {
-            
+
             Element root = new Element("HTML");
             Document doc = new Document(root);
-            
+
             Element head = new Element("HEAD");
             root.addContent(head);
-            
+
             Element link = new Element("link");
             link.setAttribute("rel", "stylesheet");
             link.setAttribute("type", "text/css");
@@ -142,47 +142,47 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
             linkPrint.setAttribute("title", "print");
             head.addContent(linkPrint);
 
-            
+
             Element linkPrintInstr = new Element("link");
             linkPrintInstr.setAttribute("rel", "stylesheet");
             linkPrintInstr.setAttribute("type", "text/css");
             linkPrintInstr.setAttribute("href", Constants.REQUEST_FORM_PRINT_INSTRUCTIONS_CSS);
             linkPrintInstr.setAttribute("title", "printAll");
             head.addContent(linkPrintInstr);            
-            
+
             // We need the </script> ending element and standard
             // XML won't do this.  So we work around the problem
             // by just injecting the <SCRIPT> tag as string
             // the document produces a string.
             head.addContent("JAVASCRIPT_GOES_HERE");
-            	
-            
+
+
             Element title = new Element("TITLE");
             title.addContent(dictionaryHelper.getRequestCategory(request.getCodeRequestCategory()) + " Request " + request.getNumber());
             head.addContent(title);
-            
+
             Element body = new Element("BODY");
             root.addContent(body);
 
             Element outerDiv = new Element("DIV");
             outerDiv.setAttribute("id", "container");
             body.addContent(outerDiv);
-            
+
             Element maindiv = new Element("DIV");
             maindiv.setAttribute("id", "containerForm");
             outerDiv.addContent(maindiv);
 
-            
+
             // Print links
             Element printColRight = new Element("DIV");
             printColRight.setAttribute("id", "printLinkColRight");
             maindiv.addContent(printColRight);
-            
+
             Element printLink = new Element("A");
             printLink.setAttribute("HREF", "javascript:setPrintStyleSheet('print');window.print()");
             printLink.addContent("Print");
             printColRight.addContent(printLink);
-            
+
 
             Element printColLeft = new Element("DIV");
             printColLeft.setAttribute("id", "printLinkColLeft");
@@ -192,27 +192,27 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
             printWithInstructionsLink.addContent("Print all (with instructions)");
             printColLeft.addContent(printWithInstructionsLink);
 
-            
-            
+
+
             Element ftr = new Element("DIV");
             ftr.setAttribute("id", "footer");            
             maindiv.addContent(ftr);
-            
-           
+
+
 
             Element h2 = new Element("H2");
             h2.addContent(formatter.makeRequestCategoryImage(null));
             h2.addContent(request.getNumber() + "&nbsp;&nbsp;&nbsp;");
             h2.addContent(dictionaryHelper.getRequestCategory(request.getCodeRequestCategory()) + (request.getIsExternal() != null && request.getIsExternal().equals("Y") ? "" :  " Request"));
             maindiv.addContent(h2);
-            
+
             if (request.getIsExternal() != null && request.getIsExternal().equals("Y")) {
               Element he = new Element("H4");
               he.addContent("External Experiment");
               maindiv.addContent(he);              
             }
-            
-      
+
+
 
             if (request.getCodeApplication() != null && !request.getCodeApplication().equals("")) {
               Element hApp = new Element("H4");
@@ -220,32 +220,39 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
               maindiv.addContent(hApp);
 
               if (RequestCategory.isIlluminaRequestCategory(request.getCodeRequestCategory())) {
-                
+
                 for(Iterator i = request.getSeqLibTreatments().iterator(); i.hasNext();) {
                   SeqLibTreatment t = (SeqLibTreatment)i.next();
                   Element hTreatment = new Element("H4");
                   hTreatment.addContent(t.getSeqLibTreatment());
                   maindiv.addContent(hTreatment);                  
-                
+
                 }
               }
 
             }
             
-            
+            if(request.getCaptureLibDesignId() != null && !request.getCaptureLibDesignId().equals("")){
+              Element capLibDesign = new Element("H5");
+              capLibDesign.addContent("Capture Lib Design: " + request.getCaptureLibDesignId());
+              capLibDesign.setAttribute("style", "text-align:center; color:black");
+              maindiv.addContent(capLibDesign);
+            }
+
+
             maindiv.addContent(new Element("BR"));
             maindiv.addContent(formatter.makeRequestTable());
-            
+
             maindiv.addContent(new Element ("BR"));
-            
+
             Element experimentNote = new Element("H5");
             experimentNote.addContent("Experiment Description");
             maindiv.addContent(experimentNote);
-            
+
             Element description = new Element("H6");
             description.addContent(request.getDescription());
             maindiv.addContent(description);
-          
+
             Element sequenceNote = new Element("H5");
             sequenceNote.addContent("Notes for Core facility");
             maindiv.addContent(sequenceNote);
@@ -261,7 +268,7 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
             Element analysisInstruction = new Element("H6");
             analysisInstruction.addContent(request.getAnalysisInstructions());
             maindiv.addContent(analysisInstruction);
-            
+
             if(request.getAvgInsertSizeFrom() != null && !request.getAvgInsertSizeFrom().equals(""))
             {
               Element avgInsertSize = new Element("H5");
@@ -274,15 +281,15 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
             }
 
             formatter.addSampleTable(maindiv, request.getSamples());
-            
+
             if (!request.getHybridizations().isEmpty()) {
 
               formatter.makePageBreak(maindiv);
-              
+
               TreeSet labeledSamples = new TreeSet(new LabeledSampleNumberComparator());
               labeledSamples.addAll(request.getLabeledSamples());
               formatter.makeLabeledSampleTable(maindiv, labeledSamples);
-              
+
               formatter.makePageBreak(maindiv);
 
               maindiv.addContent(formatter.makeHybTable(request.getHybridizations()));          
@@ -290,12 +297,12 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
 
             if (!request.getSequenceLanes().isEmpty()) {
               formatter.makePageBreak(maindiv);
-              
+
               formatter.addSequenceLaneTable(maindiv, request.getSequenceLanes(), amendState);          
             }
-            
-            
-                       
+
+
+
             // Append the submission instructions to the printable form
             // for non-guest users.
             String instructions = "";
@@ -303,7 +310,7 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
               if (!this.getSecAdvisor().isGuest()) {
                 instructions = this.getInstructions();
                 if (instructions != null) {
-                  
+
 
                   Element instructDiv = new Element("DIV");
                   instructDiv.setAttribute("id", "containerInstruction");
@@ -311,7 +318,7 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
 
                   instructDiv.addContent(new Element("BR"));
                   formatter.makePageBreak(instructDiv);
-                  
+
                   instructDiv.addContent("SUBMISSION_INSTRUCTIONS_GO_HERE");
                   // Convert degree and micro symbols to html escape codes
                   instructions = instructions.replaceAll("\\xB0", "&#176;");
@@ -319,7 +326,7 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
                 }              
               }              
             }
-          
+
             XMLOutputter out = new org.jdom.output.XMLOutputter();
             out.setOmitEncoding(true);
             this.xmlResult = out.outputString(doc);
@@ -331,41 +338,41 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
               this.xmlResult = this.xmlResult.replaceAll("SUBMISSION_INSTRUCTIONS_GO_HERE", instructions);              
             }
 
-            
+
             // Injust the <script> for java script handling of alternate style sheets
             this.xmlResult = this.xmlResult.replaceAll("JAVASCRIPT_GOES_HERE", "<script type=\"text/javascript\" src=\"switchPrintStyleSheet.js\"></script>");
 
           } else {
             this.addInvalidField("Insufficient Permission", "Insufficient permission to access this request");      
           }
-          
+
         } else {
           this.addInvalidField("Insufficient permissions", "Insufficient permission to read request.");
         }
-        
+
       }
-    
+
       if (isValid()) {
         setResponsePage(this.SUCCESS_JSP);
       } else {
         setResponsePage(this.ERROR_JSP);
       }
-    
+
     }catch (UnknownPermissionException e){
       log.error("An exception has occurred in ShowRequestForm ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-        
+
     }catch (NamingException e){
       log.error("An exception has occurred in ShowRequestForm ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-        
+
     }catch (SQLException e) {
       log.error("An exception has occurred in ShowRequestForm ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-      
+
     } catch (Exception e) {
       log.error("An exception has occurred in ShowRequestForm ", e);
       e.printStackTrace();
@@ -374,14 +381,14 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
       try {
         this.getSecAdvisor().closeReadOnlyHibernateSession();    
       } catch(Exception e) {
-        
+
       }
     }
-    
+
     return this;
   }
-  
-  
+
+
 
 
   /**
@@ -395,10 +402,10 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
   public HttpServletResponse setResponseState(HttpServletResponse response) {
     return response;
   } 
- 
+
   private String getInstructions() throws Exception {
     XMLOutputter out = new org.jdom.output.XMLOutputter();
-    
+
     SAXBuilder parser = new SAXBuilder();
     parser.setValidation(false);
     String instructionURL = this.getSubmissionInstructionsUrl();
@@ -412,40 +419,40 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
       try {
         URL url = new URL(instructionURL);
         instructionDoc = parser.build(url.openStream());  
-     } catch (Exception e) {
-       log.warn("Unable to parse submission instructions " + instructionURL, e);
-     }
+      } catch (Exception e) {
+        log.warn("Unable to parse submission instructions " + instructionURL, e);
+      }
     } else {
       // We load the doc directly from the app root
       instructionDoc = parser.build(new FileInputStream(GNomExFrontController.getWebContextPath() + instructionURL));
     }
-    
+
     if (instructionDoc != null) {                  
       Document tempDoc = new Document(new Element("div"));
       for(Iterator i = instructionDoc.getRootElement().getChildren("body").iterator(); i.hasNext();) {
         Element body = (Element)i.next();
         for(Iterator i1 = body.getChildren().iterator(); i1.hasNext();) {
-            Element node = (Element)i1.next();
-            tempDoc.getRootElement().addContent((Element)node.clone());
+          Element node = (Element)i1.next();
+          tempDoc.getRootElement().addContent((Element)node.clone());
         }
       }
-      
+
       return out.outputString(tempDoc.getRootElement());
     } else {
       return null;
     }
-    
+
   }
 
   @SuppressWarnings("unchecked")
   private String getSubmissionInstructionsUrl() {
     TreeMap<Integer, List<SubmissionInstruction>> matchMap = new TreeMap<Integer, List<SubmissionInstruction>>();
-    
+
     for(Iterator i = dictionaryHelper.getSubmissionInstructionMap().keySet().iterator(); i.hasNext();) {
       int matchCount = 0;
       Integer idSubmissionInstruction = (Integer)i.next();
       SubmissionInstruction si = (SubmissionInstruction)dictionaryHelper.getSubmissionInstructionMap().get(idSubmissionInstruction);
-      
+
       if (si.getCodeRequestCategory().equals(this.request.getCodeRequestCategory())) {
         matchCount++;
       } else {
@@ -453,7 +460,7 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
         // doesn't match
         continue;
       }
-      
+
       if (si.getCodeApplication() != null) {
         if (this.request.getCodeApplication() != null && 
             si.getCodeApplication().equals(this.request.getCodeApplication())) {
@@ -464,7 +471,7 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
           continue;
         }
       }
-          
+
       if (si.getCodeBioanalyzerChipType() != null) {
         if (this.request.getCodeBioanalyzerChipType() != null && 
             si.getCodeBioanalyzerChipType().equals(this.request.getCodeBioanalyzerChipType())) {
@@ -495,7 +502,7 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
       instructions.add(si);
       matchMap.put(key, instructions);
     }
-    
+
     // Find the submission instructions with the highest match count.
     // If multiple submission instructions have the same match
     // count, return null because we don't have an exact match.
@@ -513,6 +520,6 @@ public class ShowRequestForm extends GNomExCommand implements Serializable {
       log.warn("No matching Submission Instructions for request " + request.getNumber());
       return null;
     }
-    
+
   }
 }
