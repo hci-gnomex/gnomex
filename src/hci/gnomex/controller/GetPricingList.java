2f319445c9fde6046c5ba4e4d64e94f236ebddf4
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-06-25 20:41:19 +0000

    Filter price list for core facility admins.

--- src/hci/gnomex/controller/GetPricingList.java
+++ src/hci/gnomex/controller/GetPricingList.java
@@ -30,18 +30,18 @@ import org.jdom.Element;
 
 
 public class GetPricingList extends GNomExCommand implements Serializable {
-  
+
   private String showInactive = "N";
   private String showPrices = "Y";
   private String showPriceCriteria = "N";
-  
+
   // the static field for logging in Log4J
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetPricingList.class);
 
-  
+
   public void validate() {
   }
-  
+
   public void loadCommand(HttpServletRequest request, HttpSession session) {
 
     if (request.getParameter("showInactive") != null) {
@@ -61,115 +61,124 @@ public class GetPricingList extends GNomExCommand implements Serializable {
     } else {
       setResponsePage(this.ERROR_JSP);
     }
-    
+
   }
 
   public Command execute() throws RollBackCommandException {
-    
+
     try {
 
       Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
-    
-        if (this.getSecAdvisor().hasPermission(SecurityAdvisor.CAN_MANAGE_BILLING)) {
- 
-          DictionaryManager dictionaryManager = DictionaryManager.getDictionaryManager(ManageDictionaries.DICTIONARY_NAMES_XML, sess, this, true);
-                  
-          Document doc = new Document(new Element("Pricing"));
-          
-          List priceSheets = sess.createQuery("SELECT p from PriceSheet p order by p.name").list();
-          
-          for(Iterator i = priceSheets.iterator(); i.hasNext();) {
-            PriceSheet priceSheet = (PriceSheet)i.next();
-            
-            if (showInactive.equals("N")) {
-                if (priceSheet.getIsActive() != null && priceSheet.getIsActive().equals("N")) {
-                    continue;
-                  }
+
+      if (this.getSecAdvisor().hasPermission(SecurityAdvisor.CAN_MANAGE_BILLING)) {
+
+        DictionaryManager dictionaryManager = DictionaryManager.getDictionaryManager(ManageDictionaries.DICTIONARY_NAMES_XML, sess, this, true);
+
+        Document doc = new Document(new Element("Pricing"));
+
+        StringBuffer buf = new StringBuffer();
+        buf.append("SELECT p from PriceSheet p ");
+        if (!this.getSecAdvisor().hasPermission(SecurityAdvisor.CAN_ADMINISTER_ALL_CORE_FACILITIES)) {
+          buf.append("JOIN p.requestCategories rc ");
+          buf.append("WHERE ");
+          this.getSecAdvisor().appendCoreFacilityCriteria(buf, "rc");
+          buf.append(" ");
+        }
+        buf.append("order by p.name");
+        List priceSheets = sess.createQuery(buf.toString()).list();
+
+        for(Iterator i = priceSheets.iterator(); i.hasNext();) {
+          PriceSheet priceSheet = (PriceSheet)i.next();
+
+          if (showInactive.equals("N")) {
+            if (priceSheet.getIsActive() != null && priceSheet.getIsActive().equals("N")) {
+              continue;
             }
-            
-            priceSheet.excludeMethodFromXML("getPriceCategories");
-            priceSheet.excludeMethodFromXML("getRequestCategories");
-            
-            Element priceSheetNode = priceSheet.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
-            doc.getRootElement().addContent(priceSheetNode);
-            
+          }
 
-            Set categories = priceSheet.getPriceCategories();
-            if (categories != null) {
-              for(Iterator i1 = categories.iterator(); i1.hasNext();) {
-                PriceSheetPriceCategory x = (PriceSheetPriceCategory)i1.next();
-                PriceCategory priceCat = x.getPriceCategory();
+          priceSheet.excludeMethodFromXML("getPriceCategories");
+          priceSheet.excludeMethodFromXML("getRequestCategories");
 
-                if (showInactive.equals("N")) {
-					if (priceCat.getIsActive() != null && priceCat.getIsActive().equals("N")) {
-						continue;
-					}
+          Element priceSheetNode = priceSheet.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
+          doc.getRootElement().addContent(priceSheetNode);
+
+
+          Set categories = priceSheet.getPriceCategories();
+          if (categories != null) {
+            for(Iterator i1 = categories.iterator(); i1.hasNext();) {
+              PriceSheetPriceCategory x = (PriceSheetPriceCategory)i1.next();
+              PriceCategory priceCat = x.getPriceCategory();
+
+              if (showInactive.equals("N")) {
+                if (priceCat.getIsActive() != null && priceCat.getIsActive().equals("N")) {
+                  continue;
                 }
+              }
 
-                priceCat.excludeMethodFromXML("getPrices");
-                
-                Element categoryNode = priceCat.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
-                priceSheetNode.addContent(categoryNode);
-                
-                Set prices = priceCat.getPrices();
-                
-                if (prices != null && showPrices.equals("Y")) {
-                  for(Iterator i2 = prices.iterator(); i2.hasNext();) {
-                    Price price = (Price)i2.next();
-                    
-                    price.excludeMethodFromXML("getPriceCriterias");
-                    
-                    // Exclude inactive prices unless requested otherwise.
-                    if (showInactive.equals("N")) {
-                      if (price.getIsActive() != null && price.getIsActive().equals("N")) {
-                        continue;
-                      }                                           
-                    }
+              priceCat.excludeMethodFromXML("getPrices");
 
-                    Element priceNode = price.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
-                    priceNode.setAttribute("category", categoryNode.getAttributeValue("description"));
-                    priceNode.setAttribute("codeBillingChargeKind", categoryNode.getAttributeValue("codeBillingChargeKind"));
-                    categoryNode.addContent(priceNode);                                              
-                      
-                    if (showPriceCriteria.equals("Y")) {
-                      for(Iterator i3 = price.getPriceCriterias().iterator(); i3.hasNext();) {
-                        PriceCriteria priceCriteria = (PriceCriteria)i3.next();
-                        
-                        Element priceCriteriaNode = priceCriteria.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
+              Element categoryNode = priceCat.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
+              priceSheetNode.addContent(categoryNode);
 
-                        String label = "";
-                        if (priceCat.getDictionaryClassNameFilter1() != null && !priceCat.getDictionaryClassNameFilter1().equals("")) {
-                          label = dictionaryManager.getDisplayByValue(priceCat.getDictionaryClassNameFilter1(), priceCriteria.getFilter1());                        
-                        }
-                        if (priceCat.getDictionaryClassNameFilter1() != null && !priceCat.getDictionaryClassNameFilter1().equals("")) {
-                          label += " " + dictionaryManager.getDisplayByValue(priceCat.getDictionaryClassNameFilter2(), priceCriteria.getFilter2());
-                        }
-                        priceCriteriaNode.setAttribute("display", label);
-                        priceNode.addContent(priceCriteriaNode);
-                      }
-                      
-                    }
-                    
+              Set prices = priceCat.getPrices();
+
+              if (prices != null && showPrices.equals("Y")) {
+                for(Iterator i2 = prices.iterator(); i2.hasNext();) {
+                  Price price = (Price)i2.next();
+
+                  price.excludeMethodFromXML("getPriceCriterias");
+
+                  // Exclude inactive prices unless requested otherwise.
+                  if (showInactive.equals("N")) {
+                    if (price.getIsActive() != null && price.getIsActive().equals("N")) {
+                      continue;
+                    }                                           
                   }
+
+                  Element priceNode = price.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
+                  priceNode.setAttribute("category", categoryNode.getAttributeValue("description"));
+                  priceNode.setAttribute("codeBillingChargeKind", categoryNode.getAttributeValue("codeBillingChargeKind"));
+                  categoryNode.addContent(priceNode);                                              
+
+                  if (showPriceCriteria.equals("Y")) {
+                    for(Iterator i3 = price.getPriceCriterias().iterator(); i3.hasNext();) {
+                      PriceCriteria priceCriteria = (PriceCriteria)i3.next();
+
+                      Element priceCriteriaNode = priceCriteria.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL).getRootElement();
+
+                      String label = "";
+                      if (priceCat.getDictionaryClassNameFilter1() != null && !priceCat.getDictionaryClassNameFilter1().equals("")) {
+                        label = dictionaryManager.getDisplayByValue(priceCat.getDictionaryClassNameFilter1(), priceCriteria.getFilter1());                        
+                      }
+                      if (priceCat.getDictionaryClassNameFilter1() != null && !priceCat.getDictionaryClassNameFilter1().equals("")) {
+                        label += " " + dictionaryManager.getDisplayByValue(priceCat.getDictionaryClassNameFilter2(), priceCriteria.getFilter2());
+                      }
+                      priceCriteriaNode.setAttribute("display", label);
+                      priceNode.addContent(priceCriteriaNode);
+                    }
+
+                  }
+
                 }
               }
             }
-            
           }
-          
-          org.jdom.output.XMLOutputter out = new org.jdom.output.XMLOutputter();
-          this.xmlResult = out.outputString(doc);
-      
-          setResponsePage(this.SUCCESS_JSP);
-      
-        } else {
+
+        }
+
+        org.jdom.output.XMLOutputter out = new org.jdom.output.XMLOutputter();
+        this.xmlResult = out.outputString(doc);
+
+        setResponsePage(this.SUCCESS_JSP);
+
+      } else {
         this.addInvalidField("insufficient permission", "Insufficient permission to access pricing");
       }
     }catch (NamingException e){
       log.error("An exception has occurred in GetPricingList ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-        
+
     }catch (SQLException e) {
       log.error("An exception has occurred in GetPricingList ", e);
       e.printStackTrace();
@@ -186,7 +195,7 @@ public class GetPricingList extends GNomExCommand implements Serializable {
       try {
         this.getSecAdvisor().closeReadOnlyHibernateSession();        
       } catch(Exception e) {
-        
+
       }
     }
 
@@ -195,7 +204,7 @@ public class GetPricingList extends GNomExCommand implements Serializable {
     } else {
       setResponsePage(this.ERROR_JSP);
     }
-    
+
     return this;
   }
 
