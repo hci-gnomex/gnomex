df18248edbc20faeae6f63087cc8302e02f36aa4
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-06-29 03:52:29 +0000

    Fix upgrade script based on testing.

--- trunk/sql/gnomex_db_upgrade_to_5.4.5.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.4.5.0.sql
@@ -1,22 +1,22 @@
 use gnomex;
 
-
 -- Set CoreFacility name DEFAULT to Microarray and Genomic Analysis   
 update CoreFacility set facilityName = 'Microarray and Genomic Analysis' where facilityName = 'DEFAULT';
 
 
 -- Default all of the request categories to the Microarray and Genomic Analysis core facility
-update RequestCategory rc 
-set rc.idCoreFacility = cf.idCoreFacility
-  join CoreFacility cf on cf.idCoreFacility = rc.idCoreFacility
-  where facilityName = 'Microarray and Genomic Analysis'
-  and codeRequestCategory not in ('CAPSEQ', 'MITSEQ', 'FRAGANAL', 'CHERRYPICK');
+update RequestCategory set idCoreFacility = 1;
 
 -- Set the idCoreFacility on Request based on the request category's core facility
 update Request r 
-set r.idCoreFacility = cf.idCoreFacility
-  join RequestCategory r on r.codeRequestCategory = rc.codeRequestCategory
-  join CoreFacility cf on rc.idCoreFacility = cf.idCoreFacility;
+join RequestCategory rc on r.codeRequestCategory = rc.codeRequestCategory
+  join CoreFacility cf on rc.idCoreFacility = cf.idCoreFacility
+set r.idCoreFacility = cf.idCoreFacility;
+
+-- Set the idCoreFacility on the Billing items based on the request  
+update BillingItem bi 
+join Request r on r.idRequest = bi.idRequest  
+set bi.idCoreFacility = r.idCoreFacility;
 
   
 -- New table Primer
@@ -24,7 +24,7 @@ CREATE TABLE gnomex.Primer(
 	idPrimer int(10) auto_increment NOT NULL,
 	name varchar(50) NOT NULL,
 	description varchar(200) NULL,
-	sequencing varchar(2000) null,
+	sequence varchar(2000) null,
 	isActive char(1) NULL,
  PRIMARY KEY (idPrimer) 
  ) ENGINE = INNODB;
@@ -64,10 +64,6 @@ alter table Sample add column isControl char(1) NULL;
 -- new isControl for PlateWell
 alter table gnomex.PlateWell add column isControl char(1) null;
 
--- new purchaseOrderForm and orderFormFileType for BillingAccount
-alter table BillingAccount add column purchaseOrderForm LONGBLOB NULL;
-alter table BillingAccount add column orderFormFileType VARCHAR(10) NULL;
-
 
 -- PriceCategory description is null
 alter table gnomex.PriceCategory change column description description varchar(5000) null;
@@ -76,9 +72,10 @@ alter table gnomex.PriceCategory change column description description varchar(5
 alter table Chromatogram add column qualifiedFilePath varchar(500) null;
 
 
--- Plate label is varchar(50), not varchar(10)
-alter table gnomex.Plate change column label label varchar(50) null;
 
 -- Drop fileName column from Chromatogram -- we don't need it since file name
 -- is comprised of qualifiedFilePath and displayName
 alter table Chromatogram drop column fileName;
+
+alter table BillingAccount add column  `purchaseOrderForm` LONGBLOB NULL;
+alter table BillingAccount add column   `orderFormFileType` VARCHAR(10) NULL;
