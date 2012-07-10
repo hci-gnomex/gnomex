117afb8370e921c5b683410e4bb4ff9f74674c8d
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-07-10 04:45:40 +0000

    Place chromatograms for controls in instrument run directory.

--- trunk/src/hci/gnomex/model/InstrumentRun.java
+++ trunk/src/hci/gnomex/model/InstrumentRun.java
@@ -2,6 +2,7 @@ package hci.gnomex.model;
 
 import hci.hibernate3utils.HibernateDetailObject;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -51,6 +52,22 @@ public class InstrumentRun extends HibernateDetailObject {
   {
     this.createDate = createDate;
   }
+  
+  public String getCreateYear() {
+    return InstrumentRun.getCreateYear(this.getCreateDate());
+  }
+  
+  public static String getCreateYear(java.util.Date theCreateDate) {
+    if (theCreateDate == null) {
+      return "";
+    } else {
+      String createDate  = new SimpleDateFormat("MM/dd/yyyy").format(theCreateDate);
+      String tokens[] = createDate.split("/");
+      String createYear  = tokens[2];
+      return createYear;
+    }
+  }
+
 
   public String getCodeInstrumentRunStatus()
   {
