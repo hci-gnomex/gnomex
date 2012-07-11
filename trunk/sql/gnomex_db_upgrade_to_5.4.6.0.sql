2f4eb8f0c0bd2a6bf7d1d06ac170862726f9c375
HCI\rcundick <HCI\rcundick@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-07-11 18:41:19 +0000

    Fixed minor Topics bugs.

--- trunk/sql/gnomex_db_upgrade_to_5.4.6.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.4.6.0.sql
@@ -62,3 +62,14 @@ insert into gnomex.SealType
 values 
 ('SEPTA',  'Septa', 'Y');
 
+-- Add isDefault field to Institution table
+
+alter table gnomex.Institution add column isDefault char(1) null;
+
+
+--- Alter Topic columns to allow nulls
+
+alter table gnomex.Topic alter column idParentTopic int(10) NULL;
+
+alter table gnomex.Topic alter column idLab int(10) NULL;
+
