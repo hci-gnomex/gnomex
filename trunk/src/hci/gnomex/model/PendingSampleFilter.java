ba563dc531f28a3d5092b58967df3160f5ca72f9
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-07-27 16:05:55 +0000

    Rework GetPendingSampleList to optimize query.  Remove week, month, year filters.

--- trunk/src/hci/gnomex/model/PendingSampleFilter.java
+++ trunk/src/hci/gnomex/model/PendingSampleFilter.java
@@ -15,10 +15,6 @@ public class PendingSampleFilter extends DetailObject {
   private String               requestNumber;
   private Integer              idLab;
   private String               codeRequestCategory = RequestCategory.CAPILLARY_SEQUENCING_REQUEST_CATEGORY;
-  private String               lastWeek = "N";
-  private String               lastMonth = "N";
-  private String               lastThreeMonths = "N";
-  private String               lastYear = "N";
   
   
   private StringBuffer         queryBuf;
@@ -37,7 +33,7 @@ public class PendingSampleFilter extends DetailObject {
     addWhere = true;
     queryBuf = new StringBuffer();
     
-    queryBuf.append(" SELECT     req.idRequest, ");
+    queryBuf.append(" SELECT DISTINCT req.idRequest, ");
     queryBuf.append("            req.number, ");
     queryBuf.append("            req.codeRequestStatus, ");
     queryBuf.append("            req.codeRequestCategory, ");
@@ -46,7 +42,7 @@ public class PendingSampleFilter extends DetailObject {
     queryBuf.append("            lab.lastName, ");
     queryBuf.append("            lab.firstName, ");
     queryBuf.append("            appUser, ");
-    queryBuf.append("            sample, ");
+    queryBuf.append("            sample.idSample, ");
     queryBuf.append("            well.row, ");
     queryBuf.append("            well.col, ");
     queryBuf.append("            well.position, ");
@@ -55,14 +51,15 @@ public class PendingSampleFilter extends DetailObject {
     queryBuf.append("            plate.idPlate, ");
     queryBuf.append("            plate.label, ");
     queryBuf.append("            req.name, ");
-    queryBuf.append("            req.submitter  ");
+    queryBuf.append("            req.submitter,  ");
+    queryBuf.append("            sample.name ");
    
     
     queryBuf.append(" FROM       Request as req ");
     queryBuf.append(" JOIN       req.lab as lab ");
     queryBuf.append(" JOIN       req.appUser as appUser ");
     queryBuf.append(" JOIN       req.samples as sample ");
-    queryBuf.append(" JOIN       sample.wells as well ");
+    queryBuf.append(" JOIN  sample.wells as well ");
     queryBuf.append(" LEFT JOIN  well.plate plate ");
     
     queryBuf.append(" WHERE well.redoFlag = 'Y' ");
@@ -84,7 +81,7 @@ public class PendingSampleFilter extends DetailObject {
     queryBuf = new StringBuffer();
     
     // Get all samples that are in tubes (well with no idPlate) or plates
-    queryBuf.append(" SELECT     req.idRequest, ");
+    queryBuf.append(" SELECT DISTINCT req.idRequest, ");
     queryBuf.append("            req.number, ");
     queryBuf.append("            req.codeRequestStatus, ");
     queryBuf.append("            req.codeRequestCategory, ");
@@ -93,33 +90,34 @@ public class PendingSampleFilter extends DetailObject {
     queryBuf.append("            lab.lastName, ");
     queryBuf.append("            lab.firstName, ");
     queryBuf.append("            appUser, ");
-    queryBuf.append("            sample, ");
-    queryBuf.append("            well.row, ");  // well row
-    queryBuf.append("            well.col, ");  // well col
-    queryBuf.append("            well.position, ");  // well position
-    queryBuf.append("            well.idAssay, ");  // well idassay
-    queryBuf.append("            well.idPrimer, ");  // well idprimer
-    queryBuf.append("            plate.idPlate, ");
-    queryBuf.append("            plate.label, ");
+    queryBuf.append("            sample.idSample, ");
+    queryBuf.append("            sourceWell.row, ");  // well row
+    queryBuf.append("            sourceWell.col, ");  // well col
+    queryBuf.append("            sourceWell.position, ");  // well position
+    queryBuf.append("            sourceWell.idAssay, ");  // well idassay
+    queryBuf.append("            sourceWell.idPrimer, ");  // well idprimer
+    queryBuf.append("            sourcePlate.idPlate, ");
+    queryBuf.append("            sourcePlate.label, ");
     queryBuf.append("            req.name,  ");
-    queryBuf.append("            req.submitter  ");
+    queryBuf.append("            req.submitter,  ");
+    queryBuf.append("            sample.name ");
     
     queryBuf.append(" FROM       Request as req ");
     queryBuf.append(" JOIN       req.lab as lab ");
     queryBuf.append(" JOIN       req.appUser as appUser ");
     queryBuf.append(" JOIN       req.samples as sample ");
-    queryBuf.append(" JOIN       sample.wells as well ");
-    queryBuf.append(" LEFT JOIN  well.plate plate ");
+    queryBuf.append(" JOIN       sample.wells as sourceWell ");
+    queryBuf.append(" LEFT JOIN  sourceWell.plate sourcePlate");
     
-    queryBuf.append(" WHERE (well.redoFlag != 'Y' OR well.redoFlag is NULL) ");
-    queryBuf.append(" AND   (req.codeRequestStatus = '" + RequestStatus.PROCESSING + "') ");
-    queryBuf.append(" AND   (well.idPlate is NULL or plate.codePlateType = '" + PlateType.SOURCE_PLATE_TYPE + "') "); 
+    queryBuf.append(" WHERE (req.codeRequestStatus = '" + RequestStatus.PROCESSING + "') ");
+    queryBuf.append(" AND   (sourcePlate.codePlateType is NULL or sourcePlate.codePlateType = '" + PlateType.SOURCE_PLATE_TYPE + "') "); 
+    queryBuf.append(" AND   (sourceWell.redoFlag != 'Y' OR sourceWell.redoFlag is NULL) ");
 
     addWhere = false;
     
     addRequestCriteria();
     
-    queryBuf.append(" ORDER BY req.createDate, req.idRequest, plate.idPlate, well.position ");
+    queryBuf.append(" ORDER BY req.createDate, req.idRequest, sourcePlate.idPlate, sourceWell.position ");
    
 
     return queryBuf;
@@ -132,7 +130,7 @@ public class PendingSampleFilter extends DetailObject {
     queryBuf = new StringBuffer();
     
     // Get all samples that are in tubes (well with no idPlate) or plates
-    queryBuf.append(" SELECT     sample ");
+    queryBuf.append(" SELECT     sample.idSample ");
     
     queryBuf.append(" FROM       Request as req ");
     queryBuf.append(" JOIN       req.lab as lab ");
@@ -178,57 +176,6 @@ public class PendingSampleFilter extends DetailObject {
     }     
     
     
-    // Search for requests submitted in last week
-    if (lastWeek.equals("Y")) {
-
-      Calendar cal = Calendar.getInstance();
-      cal.add(Calendar.DAY_OF_YEAR, -7);
-      java.sql.Date lastWeek = new java.sql.Date(cal.getTimeInMillis());
-      
-      this.addWhereOrAnd();
-      queryBuf.append(" req.createDate >= '");
-      queryBuf.append(this.formatDate(lastWeek, this.DATE_OUTPUT_SQL));
-      queryBuf.append("'");
-    }
-    // Search for requests submitted in last month
-    if (lastMonth.equals("Y")) {
-
-      Calendar cal = Calendar.getInstance();
-      cal.add(Calendar.MONTH, -1);
-      java.sql.Date lastMonth = new java.sql.Date(cal.getTimeInMillis());
-      
-      this.addWhereOrAnd();
-      queryBuf.append(" req.createDate >= '");
-      queryBuf.append(this.formatDate(lastMonth, this.DATE_OUTPUT_SQL));
-      queryBuf.append("'");
-    }
-    // Search for requests submitted in last 3 months
-    if (lastThreeMonths.equals("Y")) {
-
-      Calendar cal = Calendar.getInstance();
-      cal.add(Calendar.MONTH, -3);
-      java.sql.Date last3Month = new java.sql.Date(cal.getTimeInMillis());
-      
-      this.addWhereOrAnd();
-      queryBuf.append(" req.createDate >= '");
-      queryBuf.append(this.formatDate(last3Month, this.DATE_OUTPUT_SQL));
-      queryBuf.append("'");
-    }
-    // Search for requests submitted in last year
-    if (lastYear.equals("Y")) {
-
-      Calendar cal = Calendar.getInstance();
-      cal.add(Calendar.YEAR, -1);
-      java.sql.Date lastYear = new java.sql.Date(cal.getTimeInMillis());
-      
-      this.addWhereOrAnd();
-      queryBuf.append(" req.createDate >= '");
-      queryBuf.append(this.formatDate(lastYear, this.DATE_OUTPUT_SQL));
-      queryBuf.append("'");
-    }    
-
-    
-    
   }
 
   
@@ -260,38 +207,6 @@ public class PendingSampleFilter extends DetailObject {
   
   public void setIdLab(Integer idLab) {
     this.idLab = idLab;
-  }
-
-  public String getLastWeek() {
-    return lastWeek;
-  }
-
-  public void setLastWeek(String lastWeek) {
-    this.lastWeek = lastWeek;
-  }
-
-  public String getLastMonth() {
-    return lastMonth;
-  }
-
-  public void setLastMonth(String lastMonth) {
-    this.lastMonth = lastMonth;
-  }
-
-  public String getLastThreeMonths() {
-    return lastThreeMonths;
-  }
-
-  public void setLastThreeMonths(String lastThreeMonths) {
-    this.lastThreeMonths = lastThreeMonths;
-  }
-
-  public String getLastYear() {
-    return lastYear;
-  }
-
-  public void setLastYear(String lastYear) {
-    this.lastYear = lastYear;
   }
 
   public String getCodeRequestCategory() {
