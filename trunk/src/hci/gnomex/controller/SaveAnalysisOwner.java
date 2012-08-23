5e9d88541030a6ac47bf37e3f8a60f712276ca93
HCI\rcundick <HCI\rcundick@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-08-23 22:08:03 +0000

    Implemented prompt for new owner on change analysis lab.

---
+++ trunk/src/hci/gnomex/controller/SaveAnalysisOwner.java
@@ -0,0 +1,74 @@
+package hci.gnomex.controller;
+
+import hci.framework.control.Command;
+import hci.framework.control.RollBackCommandException;
+import hci.gnomex.model.Analysis;
+import hci.gnomex.utility.HibernateSession;
+
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+
+
+public class SaveAnalysisOwner extends GNomExCommand implements Serializable {
+  
+ 
+  
+  // the static field for logging in Log4J
+  private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SaveLab.class);
+  
+  private Integer   idAnalysis; 
+  private Integer   idOwner; 
+
+  
+  public void validate() {
+  }
+  
+  public void loadCommand(HttpServletRequest request, HttpSession session) {
+    
+    if (request.getParameter("idAnalysis") != null && !request.getParameter("idAnalysis").equals("")) {
+      idAnalysis = Integer.valueOf(request.getParameter("idAnalysis"));
+    } else {
+      this.addInvalidField( "idAnalysis", "idAnalysis is required");
+    }
+    
+    if (request.getParameter("idOwner") != null && !request.getParameter("idOwner").equals("")) {
+      idOwner = Integer.valueOf(request.getParameter("idOwner"));
+    } else {
+      this.addInvalidField( "idOwner", "idOwner is required");
+    }
+  
+  }
+
+  public Command execute() throws RollBackCommandException {
+    
+    try {
+      Session sess = HibernateSession.currentSession(this.getUsername());
+      
+      Analysis analysis = (Analysis)sess.load(Analysis.class, idAnalysis);
+      analysis.setIdAppUser(idOwner);
+      sess.save(analysis);
+      sess.flush();
+      this.xmlResult = "<SUCCESS/>";
+      
+      setResponsePage(this.SUCCESS_JSP);
+      
+    }catch (Exception e){
+      log.error("An exception has occurred in SaveAnalysis ", e);
+      e.printStackTrace();
+      throw new RollBackCommandException(e.getMessage());
+        
+    }finally {
+      try {
+        HibernateSession.closeSession();        
+      } catch(Exception e) {
+        
+      }
+    }
+    
+    return this;
+  }
+}
