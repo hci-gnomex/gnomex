b01ccfe84e9e5355ce0e5cbb3ea4140102d9b537
HCI\u0763721 <HCI\u0763721@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-08-29 15:28:06 +0000

    Users with null permission codes now show blank in users view instead of keeping previously shown values.

--- sql/gnomex_db_upgrade_to_5.4.8.0.sql
+++ sql/gnomex_db_upgrade_to_5.4.8.0.sql
@@ -1,1 +1,5 @@
 alter table FlowCellChannel change read1ClustersPassedFilterM read1ClustersPassedFilterM decimal(6,2);
+
+-- update any old user with USER permission to LAB
+update AppUser set codeUserPermissionKind='LAB' where codeUserPermissionKind='USER';
+update UserPermissionKind set isActive='N' where codeUserPermissionKind='User';
