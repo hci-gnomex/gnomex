30a95f078796b43645b11ecf1ab75f2afaadf98d
HCI\u0104305 <HCI\u0104305@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-08-31 02:01:55 +0000

    Fix sql error (missing ; at end of statement).

--- sql/gnomex_db_upgrade_to_5.4.7.0.sql
+++ sql/gnomex_db_upgrade_to_5.4.7.0.sql
@@ -11,9 +11,9 @@ values('billing_gl_journal_line_ref_core_facility','','Always blank for open sou
 insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly) 
 values('billing_gl_journal_id_core_facility','','Always blank for open source GNomEx', 'Y');
 insert into PropertyDictionary(propertyName, propertyValue, propertyDescription, forServerOnly) 
-	values('generic_no_reply_email', 'DoNotReply@somewhere.edu', 'Generic no reply to use for emails going to multiple core facilities.', 'Y')
+	values('generic_no_reply_email', 'DoNotReply@somewhere.edu', 'Generic no reply to use for emails going to multiple core facilities.', 'Y');
 insert into PropertyDictionary(propertyName, propertyValue, propertyDescription, forServerOnly) 
-	values('lucene_topic_index_directory', '/home/gnomex/luceneIndex/Topic', 'The file directory for storing lucene index files on topic data.', 'Y')
+	values('lucene_topic_index_directory', '/home/gnomex/luceneIndex/Topic', 'The file directory for storing lucene index files on topic data.', 'Y');
 
 -- Add Invoice table
 CREATE TABLE `gnomex`.`Invoice` (
