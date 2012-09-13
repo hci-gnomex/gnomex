d3318eb7e5219e04fa9fd9046cda09b9e990f11b
HCI\u0395021 <HCI\u0395021@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-09-13 22:26:22 +0000

    Fixed renaming bug for individual files.(tcourdy)

--- src/hci/gnomex/controller/OrganizeExperimentUploadFiles.java
+++ src/hci/gnomex/controller/OrganizeExperimentUploadFiles.java
@@ -189,7 +189,10 @@ public class OrganizeExperimentUploadFiles extends GNomExCommand implements Seri
           for(Iterator i = parser.getFilesToRenameMap().keySet().iterator(); i.hasNext();) {
             String file = (String)i.next();
             File f1 = new File(file);
-            if(f1.exists() && f1.list().length == 0 && !f1.delete()){
+            if(f1.exists() && f1.isDirectory() && f1.list().length == 0 && !f1.delete()){
+              throw new Exception("Unable to rename file.  Invalid file name");
+            }
+            else if(f1.renameTo(new File((String)parser.getFilesToRenameMap().get(file)))){
               throw new Exception("Unable to rename file.  Invalid file name");
             }
             
