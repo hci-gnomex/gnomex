b2d97ed5e06145e1ceeade914a737a488de61342
HCI\u0351456 <HCI\u0351456@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-09-13 18:43:32 +0000

    Added contact information for all active core facilities on About page. (mbowler)

--- src/hci/gnomex/controller/GetLaunchProperties.java
+++ src/hci/gnomex/controller/GetLaunchProperties.java
@@ -1,14 +1,18 @@
 package hci.gnomex.controller;
 
+import hci.dictionary.model.DictionaryEntry;
+import hci.dictionary.model.NullDictionaryEntry;
+import hci.dictionary.utility.DictionaryManager;
 import hci.framework.control.Command;
 import hci.framework.control.RollBackCommandException;
+import hci.gnomex.model.CoreFacility;
 import hci.gnomex.model.PropertyDictionary;
-import hci.gnomex.utility.HibernateGuestSession;
 import hci.gnomex.utility.HibernateSession;
 import hci.gnomex.utility.PropertyDictionaryHelper;
 
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.Iterator;
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +20,9 @@ import javax.servlet.http.HttpSession;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
 
 public class GetLaunchProperties extends GNomExCommand implements Serializable {
 
@@ -47,10 +54,22 @@ public class GetLaunchProperties extends GNomExCommand implements Serializable {
 
       String baseURL = scheme + "://" + serverName + ":" + serverPort + contextPath;
        
-      this.xmlResult = "<LaunchProperties>";
-      this.xmlResult += "<Property name='university_user_authentication' value='" + (propUniversityUserAuth.getPropertyValue() != null ? propUniversityUserAuth.getPropertyValue() : "N") + "'/>";
-      this.xmlResult += "<Property name='base_url' value='" + baseURL + "'/>";
-      this.xmlResult += "</LaunchProperties>";
+      Document doc = new Document(new Element("LaunchProperties"));
+      
+      Element node = new Element("Property");
+      node.setAttribute( "name", "university_user_authentication" );
+      node.setAttribute( "value", (propUniversityUserAuth.getPropertyValue() != null ? propUniversityUserAuth.getPropertyValue() : "N") );
+      doc.getRootElement().addContent( node );
+      
+      node = new Element("Property");
+      node.setAttribute( "name", "base_url" );
+      node.setAttribute( "value", baseURL );
+      doc.getRootElement().addContent( node );
+      
+      getCoreFacilities( sess, doc );
+      
+      XMLOutputter out = new org.jdom.output.XMLOutputter();
+      this.xmlResult = out.outputString(doc);
       
       validate();
       
@@ -85,6 +104,37 @@ public class GetLaunchProperties extends GNomExCommand implements Serializable {
     return this;
   }
 
+  private void getCoreFacilities(Session sess, Document doc) {
+    Element facilitiesNode = new Element("CoreFacilities");
+    doc.getRootElement().addContent(facilitiesNode);
+
+    for (Iterator i = CoreFacility.getActiveCoreFacilities(sess).iterator(); i.hasNext();) {
+      DictionaryEntry de = (DictionaryEntry)i.next();
+      
+      if (de instanceof NullDictionaryEntry) {
+        continue;
+      }
+      
+      CoreFacility cf = (CoreFacility)de;
+      
+      if (cf.getIsActive() != null && cf.getIsActive().equals("Y")) {
+        String coreName = cf.getFacilityName();
+        int idCoreFacility = cf.getIdCoreFacility();
+        String contactName = PropertyDictionaryHelper.getInstance(sess).getCoreFacilityProperty(idCoreFacility, PropertyDictionary.CONTACT_NAME_CORE_FACILITY);
+        String contactNumber = PropertyDictionaryHelper.getInstance(sess).getCoreFacilityProperty(idCoreFacility, PropertyDictionary.CONTACT_PHONE_CORE_FACILITY);
+        String contactEmail = PropertyDictionaryHelper.getInstance(sess).getCoreFacilityProperty(idCoreFacility, PropertyDictionary.CONTACT_EMAIL_CORE_FACILITY);
+
+        Element facilityNode = new Element("CoreFacility");
+        facilitiesNode.addContent(facilityNode);
+        facilityNode.setAttribute("idCoreFacility", String.valueOf( idCoreFacility ));
+        facilityNode.setAttribute("coreName", coreName);
+        facilityNode.setAttribute("contactName", contactName);
+        facilityNode.setAttribute("contactNumber", contactNumber);
+        facilityNode.setAttribute("contactEmail", contactEmail);
+      }
+    }
+  }
+  
   public void validate() {
     // See if we have a valid form
     if (isValid()) {
