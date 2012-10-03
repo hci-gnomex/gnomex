2fac98acb1685751bf51c410f2d4ff04c09cd29b
HCI\rcundick <HCI\rcundick@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-10-03 19:03:35 +0000

    Changed delete annotation functionality to offer option to delete associated values rather than limiting to inactivate if there are values.

--- src/hci/gnomex/controller/DeleteProperty.java
+++ src/hci/gnomex/controller/DeleteProperty.java
@@ -1,7 +1,11 @@
 package hci.gnomex.controller;
 
 import hci.gnomex.model.AppUser;
+import hci.gnomex.model.PlateWell;
 import hci.gnomex.model.Property;
+import hci.gnomex.model.PropertyEntry;
+import hci.gnomex.model.PropertyEntryValue;
+import hci.gnomex.model.Request;
 import hci.gnomex.security.SecurityAdvisor;
 import hci.gnomex.utility.DictionaryHelper;
 import hci.gnomex.utility.HibernateSession;
@@ -9,11 +13,14 @@ import hci.framework.control.Command;
 import hci.framework.control.RollBackCommandException;
 
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
 import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.exception.ConstraintViolationException;
 
@@ -29,6 +36,7 @@ public class DeleteProperty extends GNomExCommand implements Serializable {
   
   
   private Integer      idProperty = null;
+  private String       deleteAll = null;
   
  
   
@@ -38,11 +46,16 @@ public class DeleteProperty extends GNomExCommand implements Serializable {
   
   public void loadCommand(HttpServletRequest request, HttpSession session) {
     
-   if (request.getParameter("idProperty") != null && !request.getParameter("idProperty").equals("")) {
-     idProperty = new Integer(request.getParameter("idProperty"));
-   } else {
-     this.addInvalidField("idProperty", "idProperty is required.");
-   }
+    if (request.getParameter("idProperty") != null && !request.getParameter("idProperty").equals("")) {
+      idProperty = new Integer(request.getParameter("idProperty"));
+    } else {
+      this.addInvalidField("idProperty", "idProperty is required.");
+    }
+    if (request.getParameter("deleteAll") != null && !request.getParameter("deleteAll").equals("")) {
+      deleteAll = request.getParameter("deleteAll");
+    } else {
+      this.addInvalidField("deleteAll", "deleteAll is required.");
+    }
 
   }
 
@@ -50,13 +63,75 @@ public class DeleteProperty extends GNomExCommand implements Serializable {
     Session sess = null;
     Property property = null;
     
+    int nonBlankSampleCount = 0;
+    int nonBlankAnalysisCount = 0;
+    int nonBlankDataTrackCount = 0;
+    
     try {
       sess = HibernateSession.currentSession(this.getUsername());
-      property = (Property)sess.load(Property.class, idProperty);
-      
+      property = (Property)sess.load(Property.class, idProperty);       
+                      
       // Check permissions
       if (this.getSecAdvisor().canDelete(property)) {
         
+        StringBuffer propertyEntryQuery = new StringBuffer();
+        propertyEntryQuery.append("SELECT pe from PropertyEntry as pe");
+        propertyEntryQuery.append(" where pe.idProperty = " + idProperty.intValue());
+        propertyEntryQuery.append(" and pe.idSample is not null");
+        List samples = sess.createQuery(propertyEntryQuery.toString()).list();
+        for(Iterator i1 = samples.iterator(); i1.hasNext();) {
+          PropertyEntry pe = (PropertyEntry) i1.next();
+          if(pe.getValue() != null && pe.getValue().length() > 0) {
+            nonBlankSampleCount++;
+          }
+        }        
+        
+        propertyEntryQuery = new StringBuffer();
+        propertyEntryQuery.append("SELECT pe from PropertyEntry as pe");
+        propertyEntryQuery.append(" where pe.idProperty = " + idProperty.intValue());
+        propertyEntryQuery.append(" and pe.idAnalysis is not null");
+        List analyses = sess.createQuery(propertyEntryQuery.toString()).list();
+        for(Iterator i1 = analyses.iterator(); i1.hasNext();) {
+          PropertyEntry pe = (PropertyEntry) i1.next();
+          if(pe.getValue() != null && pe.getValue().length() > 0) {
+            nonBlankAnalysisCount++;
+          }
+        }        
+        
+        
+        propertyEntryQuery = new StringBuffer();
+        propertyEntryQuery.append("SELECT pe from PropertyEntry as pe");
+        propertyEntryQuery.append(" where pe.idProperty = " + idProperty.intValue());
+        propertyEntryQuery.append(" and pe.idDataTrack is not null");
+        List dataTracks = sess.createQuery(propertyEntryQuery.toString()).list();
+        for(Iterator i1 = dataTracks.iterator(); i1.hasNext();) {
+          PropertyEntry pe = (PropertyEntry) i1.next();
+          if(pe.getValue() != null && pe.getValue().length() > 0) {
+            nonBlankDataTrackCount++;
+          }
+        }  
+        
+        if(deleteAll.compareTo("Y")==0 && 
+            (this.getSecAdvisor().hasPermission(SecurityAdvisor.CAN_ADMINISTER_ALL_CORE_FACILITIES))
+              || (nonBlankSampleCount == 0 && nonBlankAnalysisCount == 0 && nonBlankDataTrackCount == 0)) {
+          
+          StringBuffer allPropertyEntries = new StringBuffer();
+          allPropertyEntries.append("SELECT pe from PropertyEntry as pe");
+          allPropertyEntries.append(" where pe.idProperty = " + idProperty.intValue());
+          List entriesToDelete = sess.createQuery(allPropertyEntries.toString()).list();
+          for(Iterator i1 = entriesToDelete.iterator(); i1.hasNext();) {
+            PropertyEntry entry = (PropertyEntry)i1.next();
+            for(Iterator i2 = entry.getValues().iterator(); i2.hasNext();) {
+              PropertyEntryValue v = (PropertyEntryValue)i2.next();
+              sess.delete(v);
+            }
+            sess.flush();
+            entry.setValues(null);
+            sess.delete(entry);                        
+          }  
+          sess.flush();
+        }
+                
         //
         // Clear out property organism list
         //
@@ -89,13 +164,18 @@ public class DeleteProperty extends GNomExCommand implements Serializable {
         setResponsePage(this.ERROR_JSP);
       }
     } catch (ConstraintViolationException ce) {
-      this.addInvalidField("constraint", "Property set to inactive.  Unable to delete because of sample annotations on existing experiments.");
+      //this.addInvalidField("constraint", "Property set to inactive.  Unable to delete because of sample annotations on existing experiments.");
       
       try {
         sess.clear();
         property = (Property)sess.load(Property.class, idProperty);
         property.setIsActive("N");
+        sess.save(property);
         sess.flush();
+                
+        this.xmlResult = "<NONBLANKVALUES sampleCount='"+ nonBlankSampleCount +"' analysisCount='"+ nonBlankAnalysisCount +"' dataTrackCount='"+ nonBlankDataTrackCount +"'/>";
+        setResponsePage(this.SUCCESS_JSP);        
+        
       } catch(Exception e) {
         log.error("An exception has occurred in DeleteProperty when trying to inactivate property ", e);
         e.printStackTrace();
