47fb9bcf873488f07cd4d0d9adb279858e5181db
HCI\rcundick <HCI\rcundick@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-10-29 22:30:24 +0000

    Checked in files from opensource version.

---
+++ fdtHCI/src/lia/util/net/jiperf/control/StreamConsumer.java
@@ -0,0 +1,20 @@
+/*
+ * $Id: StreamConsumer.java,v 1.1 2012-10-29 22:29:36 HCI\rcundick Exp $
+ */
+package lia.util.net.jiperf.control;
+
+/**
+ * 
+ * This will be kept for history :). 
+ * The entire package lia.util.net.jiperf is the very first version of FDT. It
+ * started as an Iperf-like test for Java.
+ * 
+ * @author ramiro
+ */
+public interface StreamConsumer {
+    /**
+     * Called when the StreamPumper pumps a line from the Stream.
+     */
+    public void consumeLine(String line);
+}
+
