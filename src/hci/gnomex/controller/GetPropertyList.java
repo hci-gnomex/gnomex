945b254a5a1184b3d3db0aa7ce4514373ecf46c2
tdisera <tdisera@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-11-12 18:52:56 +0000

    The properties window needs its own XML list of properties since it requires a different XML structure (for options) to support the advanced data grid view.

--- src/hci/gnomex/controller/GetPropertyList.java
+++ src/hci/gnomex/controller/GetPropertyList.java
@@ -5,27 +5,18 @@ import hci.framework.control.Command;
 import hci.framework.control.RollBackCommandException;
 import hci.framework.model.DetailObject;
 import hci.framework.utilities.XMLReflectException;
-import hci.gnomex.model.Price;
-import hci.gnomex.model.PriceCategory;
-import hci.gnomex.model.PriceCriteria;
-import hci.gnomex.model.PriceSheet;
-import hci.gnomex.model.PriceSheetPriceCategory;
 import hci.gnomex.model.Property;
 import hci.gnomex.model.PropertyOption;
-import hci.gnomex.security.SecurityAdvisor;
-import hci.gnomex.utility.DictionaryHelper;
 
 import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -36,7 +27,7 @@ public class GetPropertyList extends GNomExCommand implements Serializable {
   // the static field for logging in Log4J
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetPropertyList.class);
   
-  private String showOptions = "Y";
+  private String showOptions = "N";
 
   
   public void validate() {
