d9df72336aec4821ff5db08491aa5d28315c6a63
tdisera <tdisera@572511d9-adea-4dea-8d56-86744b86a2dd>
2012-12-05 22:06:37 +0000

    Use special bash wildcard so that command line arguments can have embedded spaces.

--- scripts/transfer_logger.sh
+++ scripts/transfer_logger.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -classpath /home/local/HCI/orionsrvs/fdtapplogger/gnomex_client.jar hci.gnomex.httpclient.TransferLoggerMain "$@" -server hci-bio-test
+java -classpath /home/local/HCI/orionsrvs/fdtapplogger/gnomex_client.jar hci.gnomex.httpclient.TransferLoggerMain ${1+"$@"} -serverURL http://hci-bio-test.hci.utah.edu:8008
