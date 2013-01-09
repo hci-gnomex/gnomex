d0b43c26473befe4a8360523385fe6b8f3c85562
bvdveen <bvdveen@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-01-09 12:25:00 +0000

    Preparation for dashboard integration

--- branches/bernd/src/hci/gnomex/controller/GetNotificationOverview.java
+++ branches/bernd/src/hci/gnomex/controller/GetNotificationOverview.java
@@ -1,6 +1,6 @@
 package hci.gnomex.controller;
 
-import hci.gnomex.model.ExperimentOverviewFilter;
+import hci.gnomex.model.NotificationOverviewFilter;
 import hci.gnomex.model.Step;
 import hci.gnomex.security.SecurityAdvisor;
 import hci.gnomex.utility.DictionaryHelper;
@@ -25,12 +25,12 @@ import org.jdom.Element;
 import org.jdom.output.XMLOutputter;
 
 
-public class GetExperimentOverviewList extends GNomExCommand implements Serializable {
+public class GetNotificationOverview extends GNomExCommand implements Serializable {
   
   
 private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetExperimentOverviewList.class);
   
-  private ExperimentOverviewFilter filter;
+  private NotificationOverviewFilter filter;
   
   
   public void validate() {
@@ -38,7 +38,7 @@ private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(G
   
   public void loadCommand(HttpServletRequest request, HttpSession session) {
 
-    filter = new ExperimentOverviewFilter();
+    filter = new NotificationOverviewFilter();
     
     HashMap errors = this.loadDetailObject(request, filter);
     this.addInvalidFields(errors);
@@ -55,7 +55,7 @@ private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(G
 
         /// Process filter calling here 
         TreeMap clusterGenMap = new TreeMap();
-        Document doc = new Document(new Element("ExperimentOverviewList"));
+        Document doc = new Document(new Element("NotificationOverviewList"));
         
         StringBuffer queryBuf = filter.getQuery(this.getSecAdvisor());
         List rows = (List) sess.createQuery(queryBuf.toString()).list();
@@ -63,53 +63,14 @@ private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(G
         for (Iterator<Object[]> i1 = rows.iterator(); i1.hasNext();) {
             Object[] row = (Object[]) i1.next();
             
-            Element n = new Element("ExperimentOverview");
+            Element n = new Element("NotificationOverview");
 
-            n.setAttribute("sampleId",              	 row[0] == null ? "" :  ((Integer)row[0]).toString());
-            n.setAttribute("sampleName",              	 row[1] == null ? "" :  (String)row[1]);
-            n.setAttribute("sampleBarcode",              row[2] == null ? "" :  (String)row[2]);
-            n.setAttribute("samplePrepDate",             row[3] == null ? "" :  this.formatDate((java.util.Date)row[3]));
-            n.setAttribute("expCodeApp",	             row[4] == null ? "" :  (String)row[4]);
-            n.setAttribute("experimentId", 	             row[5] == null ? "" :  (String)row[5]);
-            n.setAttribute("expCreateDate",              row[6] == null ? "" :  this.formatDate((java.util.Date)row[6]));
-            n.setAttribute("expStatus",  	             row[7] == null ? "" :  (String)row[7]);
-            n.setAttribute("userFirstName",          	 row[8] == null ? "" :  (String)row[8]);
-            n.setAttribute("userLastName",               row[9] == null ? "" :  (String)row[9]);
-            n.setAttribute("labId",              	 	 row[10] == null ? "" :  ((Integer)row[10]).toString());
-            n.setAttribute("labFirstName",		         row[11] == null ? "" :  (String)row[11]);
-            n.setAttribute("expReadApp",	             row[12] == null ? "" :  (String)row[12]);
-            n.setAttribute("expSeqRunType", 			 row[13] == null ? "" : (String)row[13]);
-            n.setAttribute("expNumSeqCycles",			 row[14] == null ? "" : ((Integer)row[14]).toString());
-            n.setAttribute("expInstrument", 			 row[15] == null ? "" : (String)row[15]);
-            n.setAttribute("labLastName",		         row[16] == null ? "" :  (String)row[16]);
-            n.setAttribute("sampleCodeStepNext",		 row[17] == null ? "" : (String)row[17]);
-            n.setAttribute("idCoreFacility",	 		 row[18] == null ? "" : ((Integer)row[18]).toString());
+            /*
+             * Add values to Element n
+             * Example:
+             * n.setAttribute("sampleId",              	 row[0] == null ? "" :  ((Integer)row[0]).toString()); 
+             */
             
-            String labFullName = "";
-            if (row[16] != null) {
-              labFullName = (String)row[16];
-            }
-            if (row[11] != null) {
-              if (labFullName.length() > 0) {
-                labFullName += ", ";
-              }
-              labFullName += (String)row[11];
-            }
-            
-            n.setAttribute("labFullName", labFullName); 
-            
-            String userFullName = "";
-            if (row[9] != null) {
-              userFullName = (String)row[9];
-            }
-            if (row[8] != null) {
-              if (userFullName.length() > 0) {
-                userFullName += ", ";
-              }
-              userFullName += (String)row[8];
-            }
-            
-            n.setAttribute("userFullName", userFullName); 
 
             // Add node content to rootElement XML output.
             doc.getRootElement().addContent(n);
