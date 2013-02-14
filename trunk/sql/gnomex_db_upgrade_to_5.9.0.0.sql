2123b7b0541c98cda22b5882e46ad1babc80dc93
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-02-14 18:11:26 +0000

    334 Complete microarray billing items when experiment finishes.

--- trunk/sql/gnomex_db_upgrade_to_5.9.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.9.0.0.sql
@@ -2,13 +2,29 @@ use gnomex;
 
 alter table Step add column sortOrder INT(10) null;
 
-update Step set sortOrder = 1 where codeStepNext like '%QC';
-update Step set sortOrder = 2 where codeStepNext like '%LIBPREP';
-update Step set sortOrder = 3 where codeStepNext like '%SEQASSEM';
-update Step set sortOrder = 4 where codeStepNext like '%SEQPIPE';
+update Step set sortOrder = 1 where codeStep like '%QC';
+update Step set sortOrder = 2 where codeStep like '%LIBPREP';
+update Step set sortOrder = 3 where codeStep like '%SEQASSEM';
+update Step set sortOrder = 4 where codeStep like '%SEQPIPE';
 
-update Step set sortOrder = 2 where codeStepNext = 'LABEL';
-update Step set sortOrder = 3 where codeStepNext = 'HYB';
-update Step set sortOrder = 4 where codeStepNext = 'EXT';
+update Step set sortOrder = 2 where codeStep = 'LABEL';
+update Step set sortOrder = 3 where codeStep = 'HYB';
+update Step set sortOrder = 4 where codeStep = 'EXT';
 
 
+
+-- Create many-many between step and price category table
+CREATE TABLE `gnomex`.`PriceCategoryStep` (
+  idPriceCategory INT(10),
+  codeStep VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`idPriceCategory`, codeStep),
+  CONSTRAINT `FK_PriceCategoryStep_PriceCategory` FOREIGN KEY  (`idPriceCategory`)
+    REFERENCES `gnomex`.`PriceCategory` (`idPriceCategory`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_PriceCategoryStep_Step` FOREIGN KEY  (`codeStep`)
+    REFERENCES `gnomex`.`Step` (`codeStep`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+)
+ENGINE = INNODB;
