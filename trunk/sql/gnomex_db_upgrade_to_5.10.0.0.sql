2c4f6152c60c2944c27d406daefd00b49040cd3d
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-03-12 17:37:41 +0000

    Insert contact_email_core_facility_work_auth_reminder into PropertyDictionary(tcourdy)

--- trunk/sql/gnomex_db_upgrade_to_5.10.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.10.0.0.sql
@@ -6,3 +6,8 @@ alter table RequestCategory add isClinicalResearch CHAR(1) null;
 
 -- Add zipCode to BillingAccount table
 alter table gnomex.BillingAccount add column zipCode varchar(20);
+
+--Add contact_email_core_facility_workauth_reminder
+INSERT INTO PropertyDictionary (propertyName,propertyValue,propertyDescription, forServerOnly) VALUES
+('contact_email_core_facility_work_auth', 'first.last@somwhere.edu', 'Core facility''s email adresses for reminders', 'N');
+
