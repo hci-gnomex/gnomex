40f7ad4724b04036dbeb650742edc6706fc72776
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-03-12 17:38:48 +0000

    Added info for contact_email_core_facility_workauth_reminder to the PropertyDictionary populate script(tcourdy)

--- trunk/sql/gnomex_db_populate.sql
+++ trunk/sql/gnomex_db_populate.sql
@@ -869,7 +869,8 @@ INSERT INTO `gnomex`.`PropertyDictionary` (`idPropertyDictionary`,`propertyName`
  (91,'experiment_view_limit', '1000', 'Maximum number of experiments shown in experiment browse view.', 'N'),
  (92,'configurable_billing_accounts', 'Y', 'Y/N value indicating whether hard-coded or configuration billing account fields should be used.', 'N'),
  (93,'das2_url', 'Y', 'http://localhost:8080/das2gnomex/genome', 'N'),
- (94,'guest_download_terms', '', 'If property is set, then guests are prompted to agree to these terms prior to allowing download of files.  In addition an email is required which is saved in the transfer log.', 'N');
+ (94,'guest_download_terms', '', 'If property is set, then guests are prompted to agree to these terms prior to allowing download of files.  In addition an email is required which is saved in the transfer log.', 'N'),
+ (95,'contact_email_core_facility_workauth_reminder','first.last@somwhere.edu','Core facility''s email adresses for reminders', 'N');
  
  
 
