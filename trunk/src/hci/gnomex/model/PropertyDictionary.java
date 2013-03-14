574924ab6ebc44c36047a660e8c4785a86d0c9be
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-03-14 14:44:24 +0000

    421 Send new order email to core.

--- trunk/src/hci/gnomex/model/PropertyDictionary.java
+++ trunk/src/hci/gnomex/model/PropertyDictionary.java
@@ -88,6 +88,7 @@ public class PropertyDictionary extends DictionaryEntry implements Serializable 
   public static final String        GNOMEX_SUPPORT_EMAIL                = "gnomex_support_email";
   public static final String        GUEST_DOWNLOAD_TERMS                = "guest_download_terms";
   public static final String        CAN_ACCESS_BSTX                     = "can_access_bstx";
+  public static final String        REQUEST_SUBMIT_CONFIRMATION_EMAIL   = "request_submit_confirmation_email";
   
   public static final String        OPTION_USER_USER_VISIBILITY_NONE    = "none";
   public static final String        OPTION_USER_USER_VISIBILITY_MASKED  = "masked";
