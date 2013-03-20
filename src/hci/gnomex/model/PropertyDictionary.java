b6e48b11d1db5a9e59f7daadf752455d89076451
meganb <meganb@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-03-20 22:53:58 +0000

    Added 'GNOMEX_LINKAGE_BST_URL' (URL to link to itBioPath) as a property. (mbowler)

--- src/hci/gnomex/model/PropertyDictionary.java
+++ src/hci/gnomex/model/PropertyDictionary.java
@@ -46,6 +46,7 @@ public class PropertyDictionary extends DictionaryEntry implements Serializable 
   public static final String        TEMP_DIRECTORY                      = "temp_directory";
   public static final String        BST_LINKAGE_SUPPORTED               = "bst_linkage_supported";  
   public static final String        BST_LINKAGE_GNOMEX_URL              = "bst_linkage_gnomex_url";
+  public static final String        GNOMEX_LINKAGE_BST_URL              = "gnomex_linkage_bst_url";  
   public static final String        FDT_SUPPORTED                       = "fdt_supported";
   public static final String        HTTP_PORT                           = "http_port";
   public static final String        FDT_USER                            = "fdt_user";
