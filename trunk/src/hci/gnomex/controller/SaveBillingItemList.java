2da35d334f0da0b5beb49d57b5cbb4fe0ea3536b
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-04-25 20:15:50 +0000

    Don't throw messaging exception if the email is not properly formed.  Just log the error.(tcourdy)

--- trunk/src/hci/gnomex/controller/SaveBillingItemList.java
+++ trunk/src/hci/gnomex/controller/SaveBillingItemList.java
@@ -309,12 +309,12 @@ public class SaveBillingItemList extends GNomExCommand implements Serializable {
     String fromAddress = PropertyDictionaryHelper.getInstance(sess).getCoreFacilityProperty(idCoreFacility, PropertyDictionary.CONTACT_EMAIL_CORE_FACILITY);
     if(emailRecipients.contains(",")){
       for(String e : emailRecipients.split(",")){
-        if(!MailUtil.isValidEmail(e)){
-          throw new Exception("Invalid email address " + e);
+        if(!MailUtil.isValidEmail(e.trim())){
+          log.error("Invalid email address " + e);
         }
       }
     } else if(!MailUtil.isValidEmail(emailRecipients)){
-      throw new Exception("Invalid email address " + emailRecipients);
+      log.error("Invalid email address " + emailRecipients);
     }
     if (dictionaryHelper.isProductionServer(serverName)) {
       send = true;
