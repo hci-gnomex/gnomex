110bc49aabc7e36373ade6c01a50f7ebed1339df
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-05-01 16:10:31 +0000

    606 Annotatinos showing for wrong platform.

--- src/hci/gnomex/controller/GetRequest.java
+++ src/hci/gnomex/controller/GetRequest.java
@@ -331,27 +331,35 @@ public class GetRequest extends GNomExCommand implements Serializable {
             if (entry == null && prop.getIsActive().equals("N")) {
               continue;
             }
+            
             // for sequenom and iscan types we only include properties that explicitly apply to the request category.
             Boolean autoSelect = false;
-            if ( requestCategory != null &&
-                (   requestCategory.getType().equals(RequestCategoryType.TYPE_ISCAN) ||
-                    requestCategory.getType().equals(RequestCategoryType.TYPE_SEQUENOM) ||
-                    requestCategory.getType().equals(RequestCategoryType.TYPE_CLINICAL_SEQUENOM))) {
-              boolean include = false;
-              if (prop.getPlatformApplications() != null) {
-                for(Iterator i1 = prop.getPlatformApplications().iterator(); i1.hasNext();) {
-                  PropertyPlatformApplication pa = (PropertyPlatformApplication) i1.next();
-                  if ( pa.getCodeRequestCategory().equals( request.getCodeRequestCategory() )) {
-                    include = true;
+            boolean include = true;
+            if (requestCategory != null && 
+                  (requestCategory.getType().equals(RequestCategoryType.TYPE_ISCAN) ||
+                   requestCategory.getType().equals(RequestCategoryType.TYPE_SEQUENOM) ||
+                   requestCategory.getType().equals(RequestCategoryType.TYPE_CLINICAL_SEQUENOM))) {
+              include = false;
+            }
+            if (prop.getPlatformApplications() != null && prop.getPlatformApplications().size() > 0 && requestCategory != null) {
+              include = false;
+              for(Iterator i1 = prop.getPlatformApplications().iterator(); i1.hasNext();) {
+                PropertyPlatformApplication pa = (PropertyPlatformApplication) i1.next();
+                if ( pa.getCodeRequestCategory().equals(request.getCodeRequestCategory()) &&
+                        (pa.getApplication() == null || pa.getApplication().getCodeApplication().equals(request.getCodeApplication()))) {
+                  include = true;
+                  if (requestCategory.getType().equals(RequestCategoryType.TYPE_ISCAN) ||
+                      requestCategory.getType().equals(RequestCategoryType.TYPE_SEQUENOM) ||
+                      requestCategory.getType().equals(RequestCategoryType.TYPE_CLINICAL_SEQUENOM)) {
                     autoSelect = true;
-                    break;
-                  }   
-                }
+                  }
+                  break;
+                }   
+              }
 
-              }
-              if ( !include ) {
-                continue;
-              }
+            }
+            if ( !include ) {
+              continue;
             }
             
             peNode.setAttribute("idProperty", prop.getIdProperty().toString());
