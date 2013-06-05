0faf7f0f05ac7d385c2349acdb84a911ca1f6ced
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-06-05 20:20:33 +0000

    666 Minor refinements to experiments change Members to All Lab Members

--- trunk/sql/gnomex_db_upgrade_to_5.13.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.13.0.0.sql
@@ -6,3 +6,5 @@ alter table NumberSequencingCyclesAllowed add isCustom CHAR(1) NOT NULL DEFAULT 
 -- Remove unused columns
 alter table NumberSequencingCyclesAllowed drop column notes;
 
+-- Members is now All Lab Members
+update Visibility set visibility='All Lab Members' where codeVisibility='MEM';
