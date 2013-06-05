fc7b46d42d0449e87b18ef1940ece14c2bc2cf54
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-06-05 19:41:44 +0000

    Remove unreferenced column

--- trunk/sql/gnomex_db_upgrade_to_5.13.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.13.0.0.sql
@@ -2,3 +2,7 @@ use gnomex;
 
 -- Add isCustom
 alter table NumberSequencingCyclesAllowed add isCustom CHAR(1) NOT NULL DEFAULT 'N';
+
+-- Remove unused columns
+alter table NumberSequencingCyclesAllowed drop column notes;
+
