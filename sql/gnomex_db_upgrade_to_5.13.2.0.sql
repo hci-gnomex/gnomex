87d98c2b5f95a8ea4e77afc5be98faae0850087a
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-07-16 18:04:26 +0000

    Added drop statement to remove meanLibSizeActual from request.

--- sql/gnomex_db_upgrade_to_5.13.2.0.sql
+++ sql/gnomex_db_upgrade_to_5.13.2.0.sql
@@ -2,3 +2,6 @@ use gnomex;
 
 -- barcodeSequenceB column on Sample
 alter table Sample add barcodeSequenceB varchar(20) NULL;
+
+--Remove meanLibSizeActual column from Request
+alter table Request drop column meanLibSizeActual;
