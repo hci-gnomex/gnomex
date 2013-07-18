2a3361e2c1a9071b38519b9463a73f2a8c84ef02
tdisera <tdisera@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-07-18 05:29:06 +0000

    Fix syntax error (comment)

--- trunk/sql/gnomex_db_upgrade_to_5.13.2.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.13.2.0.sql
@@ -3,5 +3,5 @@ use gnomex;
 -- barcodeSequenceB column on Sample
 alter table Sample add barcodeSequenceB varchar(20) NULL;
 
---Remove meanLibSizeActual column from Request
+-- Remove meanLibSizeActual column from Request
 alter table Request drop column meanLibSizeActual;
