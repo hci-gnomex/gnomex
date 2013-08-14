e42cf359f37547893873432e3f04fad0f5817b31
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-08-14 17:53:37 +0000

    Silently return from trying to send the email to the bio informatics core since that property is currently not a part of gnomex.  In the event that the property is added back the email code will be fully functional again.

--- src/hci/gnomex/controller/SaveWorkItemSolexaPipeline.java
+++ src/hci/gnomex/controller/SaveWorkItemSolexaPipeline.java
@@ -329,7 +329,13 @@ public class SaveWorkItemSolexaPipeline extends GNomExCommand implements Seriali
     bioIntroNote.append("<br>");
     
     emailRecipients = dictionaryHelper.getPropertyDictionary(PropertyDictionary.CONTACT_EMAIL_BIOINFORMATICS);
-    if(emailRecipients.contains(",")){
+    //As of 08/14/2013 the Bioinformatics contact email property is currently removed from gnomex.  So this email address is currently being set null.
+    //I've decided to just silently return out of this function for now if this email is null.  Otherwise we get an invalid email error in the server log.
+    if(emailRecipients == null){
+      return;
+    }
+    
+    if(emailRecipients != null && emailRecipients.contains(",")){
       for(String e : emailRecipients.split(",")){
         if(!MailUtil.isValidEmail(e.trim())){
           log.error("Invalid email address: " + e);
