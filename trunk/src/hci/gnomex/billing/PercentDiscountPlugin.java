d3a8470c9d46d0bb910b7eb30bf289a80fe3edc8
meganb <meganb@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-09-16 21:39:58 +0000

    Added a check for internal lab. (mbowler)

--- trunk/src/hci/gnomex/billing/PercentDiscountPlugin.java
+++ trunk/src/hci/gnomex/billing/PercentDiscountPlugin.java
@@ -33,6 +33,10 @@ public class PercentDiscountPlugin implements BillingPlugin {
       return billingItems;
     }
     
+    if ( request.getLab() == null || request.getLab().isExternalLab() ) {
+      return billingItems;
+    }
+    
     // Now find the price
     Price price = null;
 
