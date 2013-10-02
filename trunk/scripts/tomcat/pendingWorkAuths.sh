f1c5a059c4a1a86d298834af79b93cd1c3a4dad8
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-10-02 22:08:10 +0000

    891 Batch programs do not work for Tomcat

--- trunk/scripts/tomcat/pendingWorkAuths.sh
+++ trunk/scripts/tomcat/pendingWorkAuths.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 TOMCAT_HOME=../../../
-COMMON_LIB=$TOMCAT_HOME/common/lib
+COMMON_LIB=$TOMCAT_HOME/lib
 OPENEJB_LIB=$TOMCAT_HOME/webapps/openejb/lib
 
 GNOMEX_LIB=../WEB-INF/lib
 GNOMEX_CLASSES=../WEB-INF/classes
 
-CLASSPATH=".:$GNOMEX_CLASSES:$OPENEJB_LIB\commons-logging-1.1.jar:$OPENEJB_LIB\commons-collections-3.2.jar:$OPENEJB_LIB\javaee-api-5.0-3.jar:"
+CLASSPATH=".:$GNOMEX_CLASSES:"
 
 for JAR in $COMMON_LIB/*.jar
 do
 CLASSPATH="$CLASSPATH:$JAR"
+done
 
 for JAR in $GNOMEX_LIB/*.jar
 do
