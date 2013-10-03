4fa50a7a64b8413b648675f605da86dc30069a09
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-10-03 14:45:56 +0000

    891 Batch programs do not work for Tomcat

--- trunk/scripts/tomcat/usageReport.cmd
+++ trunk/scripts/tomcat/usageReport.cmd
@@ -6,7 +6,6 @@ set COMMON_LIB=%TOMCAT_HOME%\lib
 set GNOMEX_LIB=..\WEB-INF\lib
 set GNOMEX_CLASSES=..\WEB-INF\classes
 
-
 set CP=.;%GNOMEX_CLASSES%;
 
 for %%i in (%GNOMEX_LIB%\*.jar) do call append_classpath.cmd %%i
@@ -15,4 +14,4 @@ for %%i in (%COMMON_LIB%\*.jar) do call append_classpath.cmd %%i
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-java -classpath %CP% hci.gnomex.lucene.BuildSearchIndex %*
+java -classpath %CP% hci.gnomex.daemon.UsageReportd %*
