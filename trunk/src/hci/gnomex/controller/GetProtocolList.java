3bd780bae90788eb419ad29374614a6279c4d8d7
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2013-11-06 22:24:15 +0000

    Order all protocols alphabetically.

--- trunk/src/hci/gnomex/controller/GetProtocolList.java
+++ trunk/src/hci/gnomex/controller/GetProtocolList.java
@@ -47,7 +47,7 @@ public class GetProtocolList extends GNomExCommand implements Serializable {
         featureExtractionProtocols.setAttribute("label", "Feature Extraction Protocol");
         featureExtractionProtocols.setAttribute("protocolClassName", FeatureExtractionProtocol.class.getName());
         root.addContent(featureExtractionProtocols);
-        l = sess.createQuery("select fe from FeatureExtractionProtocol fe order by fe.featureExtractionProtocol ").list();
+        l = sess.createQuery("select fe from FeatureExtractionProtocol fe order by fe.featureExtractionProtocol").list();
         if (!l.isEmpty()) {
           Iterator iter = l.iterator();
           while (iter.hasNext()) {
@@ -64,7 +64,7 @@ public class GetProtocolList extends GNomExCommand implements Serializable {
         hybProtocols.setAttribute("label", "Hyb Protocol");
         hybProtocols.setAttribute("protocolClassName", HybProtocol.class.getName());
         root.addContent(hybProtocols);
-        l = sess.createQuery("select h from HybProtocol h order by h.hybProtocol ").list();
+        l = sess.createQuery("select h from HybProtocol h order by h.hybProtocol").list();
         if (!l.isEmpty()) {
           Iterator iter = l.iterator();
           while (iter.hasNext()) {
@@ -96,7 +96,7 @@ public class GetProtocolList extends GNomExCommand implements Serializable {
         scanProtocols.setAttribute("label", "Scan Protocol");
         scanProtocols.setAttribute("protocolClassName", ScanProtocol.class.getName());
         root.addContent(scanProtocols);
-        l = sess.createQuery("from ScanProtocol").list();
+        l = sess.createQuery("select sp from ScanProtocol sp order by sp.scanProtocol").list();
         if (!l.isEmpty()) {
           Iterator iter = l.iterator();
           while (iter.hasNext()) {
@@ -112,7 +112,7 @@ public class GetProtocolList extends GNomExCommand implements Serializable {
         seqLibProtocols.setAttribute("label", "Sequence Lib Protocol");
         seqLibProtocols.setAttribute("protocolClassName", SeqLibProtocol.class.getName());
         root.addContent(seqLibProtocols);
-        l = sess.createQuery("from SeqLibProtocol").list();
+        l = sess.createQuery("select slp from SeqLibProtocol slp order by slp.seqLibProtocol").list();
         if (!l.isEmpty()) {
           Iterator iter = l.iterator();
           while (iter.hasNext()) {
