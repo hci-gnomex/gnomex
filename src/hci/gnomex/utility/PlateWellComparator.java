f6a276dfe8f657150ef47d2fe7faac742a05e5a6
meganb <meganb@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-01-14 16:18:40 +0000

    Added check for null platewells and null ids to comparator. (mbowler)

--- src/hci/gnomex/utility/PlateWellComparator.java
+++ src/hci/gnomex/utility/PlateWellComparator.java
@@ -10,15 +10,24 @@ import java.util.Comparator;
       PlateWell pw1 = (PlateWell)o1;
       PlateWell pw2 = (PlateWell)o2;
       
-      if (pw1.getIdPlateWell() != null && pw2.getIdPlateWell() != null) {
-        return pw1.getIdPlateWell().compareTo(pw2.getIdPlateWell());
-      } else if (pw1.getIdPlateWell() != null) {
-        return 1;
-      } else if (pw2.getIdPlateWell() != null) {
+      if (pw1 != null && pw2 != null) {
+        if (pw1.getIdPlateWell() != null && pw2.getIdPlateWell() != null) {
+          return pw1.getIdPlateWell().compareTo(pw2.getIdPlateWell());
+        } else if (pw1.getIdPlateWell() != null) {
+          return -1;
+        } else if (pw2.getIdPlateWell() != null) {
+          return 1;
+        } else {
+          return 0;
+        }
+      } else if (pw1 != null) {
         return -1;
+      } else if (pw2 != null) {
+        return 1;
       } else {
         return 0;
       }
+      
       
     }
   }
