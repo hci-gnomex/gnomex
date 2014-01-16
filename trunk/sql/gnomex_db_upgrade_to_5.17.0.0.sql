1c26c695a6d91998594071210d3ca508a6e0e3ff
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-01-16 18:36:08 +0000

    Added the new experiment_file_sample_linking_enabled property.  Added Conversion script to convert the old sampleexperimentfile structure to the new one.

--- trunk/sql/gnomex_db_upgrade_to_5.17.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.17.0.0.sql
@@ -36,3 +36,50 @@ alter table gnomex.FlowCell add CONSTRAINT FK_FlowCell_CoreFacility
 -- Add owner only flag to request category
 alter table RequestCategory add isOwnerOnly CHAR(1) null;
 call ExecuteIfTableExists('gnomex','RequestCategory_Audit','alter table gnomex.RequestCategory_Audit add isOwnerOnly CHAR(1) null');
+
+-- New property experiment_file_sample_linking_enabled
+INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory)
+	VALUES('experiment_file_sample_linking_enabled', 'Y', 'Enable this property in order to see the Sample Linking button on the Files tab of the experiment edit view', 'N', null, null);
+
+
+-- *********CONVERSION SCRIPT for sample experiment file table****************
+
+RENAME TABLE `sampleexperimentfile` TO `oldsampleexperimentfile`;
+ALTER TABLE	`oldsampleexperimentfile` Drop Foreign key `FK_SampleExperimentFile_Sample`,
+Add constraint `FK_SampleExperimentFile_Sample1` Foreign key `FK_SampleExperimentFile_Sample1` (`idSample`)
+	References `gnomex`.`Sample` (`idSample`)
+	ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
+
+CREATE TABLE `gnomex`.`sampleexperimentfile` (
+  `idSampleExperimentFile` INT(10) NOT NULL AUTO_INCREMENT,
+  `idSample` INT(10),
+  `idExpFileRead1` INT(10),
+  `idExpFileRead2` INT(10),
+  `seqRunNumber` INT(10),
+  PRIMARY KEY (`idSampleExperimentFile`),
+  UNIQUE KEY `UN_SampleExperimentFile` (idSample, idExpFileRead1, idExpFileRead2),
+  CONSTRAINT `FK_SampleExperimentFile_Sample` FOREIGN KEY `FK_SampleExperimentFile_Sample` (`idSample`)
+    REFERENCES `gnomex`.`Sample` (`idSample`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_SampleExperimentFile_ExperimentFile1` FOREIGN KEY `FK_SampleExperimentFile_ExperimentFile1` (`idExpFileRead1`)
+    REFERENCES `gnomex`.`ExperimentFile` (`idExperimentFile`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_SampleExperimentFile_ExperimentFile2` FOREIGN KEY `FK_SampleExperimentFile_ExperimentFile2` (`idExpFileRead2`)
+    REFERENCES `gnomex`.`ExperimentFile` (`idExperimentFile`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+INSERT INTO `gnomex`.`sampleexperimentfile` (idSample, idExpFileRead1)
+SELECT idSample, idExperimentFile FROM `gnomex`.`oldsampleexperimentfile`
+WHERE `gnomex`.`oldsampleexperimentfile`.codeSampleFileType = 'fastqRead1';
+
+UPDATE `gnomex`.`sampleexperimentfile`
+JOIN `gnomex`.`oldsampleexperimentfile`
+SET `gnomex`.`sampleexperimentfile`.idExpFileRead2 = `gnomex`.`oldsampleexperimentfile`.idExperimentFile
+WHERE `gnomex`.`sampleexperimentfile`.idSample = `gnomex`.`oldsampleexperimentfile`.idSample AND `gnomex`.`oldsampleexperimentfile`.codeSampleFileType = 'fastqRead2';
+
+UPDATE `gnomex`.`sampleexperimentfile` SET seqRunNumber = 1;
