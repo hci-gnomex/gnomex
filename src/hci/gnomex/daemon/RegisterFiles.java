679ea613f2c54b73bb33b0d30fd8bbe309f4b5cb
tdisera <tdisera@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-02-06 21:53:46 +0000

    GNomEx 5.17 (final) tag

--- src/hci/gnomex/daemon/RegisterFiles.java
+++ src/hci/gnomex/daemon/RegisterFiles.java
@@ -19,7 +19,6 @@ import hci.gnomex.utility.FileDescriptor;
 import hci.gnomex.utility.MailUtil;
 import hci.gnomex.utility.PropertyDictionaryHelper;
 import hci.gnomex.utility.UploadDownloadHelper;
-import hci.gnomex.utility.Util;
 
 import java.io.File;
 import java.io.IOException;
@@ -696,7 +695,7 @@ public class RegisterFiles extends TimerTask {
       for (int x = 0; x < fileList.length; x++) {
         String fileName = directoryName + "/" + fileList[x];
         File f1 = new File(fileName);
-        if(f1.isFile() && !Util.isSymlink(f1)) {
+        if(f1.isFile()) {
           FileDescriptor fd = new FileDescriptor(requestNumber, fileList[x], f1, baseRequestNumber + "/" + fileList[x]);
           fileMap.put(fd.getZipEntryName().replace("\\", "/"), fd);
         }
