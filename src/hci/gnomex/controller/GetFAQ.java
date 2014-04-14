bd2b3f7d6bf44b3b41394fa38a1235f32aaf6025
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-04-14 19:26:21 +0000

    Made quick links core facility driven.  Moved links that were currently on main nav menu bar to the quick links dropdown.  Added fix for menu items that would collapse on themselves intermittently.

--- src/hci/gnomex/controller/GetFAQ.java
+++ src/hci/gnomex/controller/GetFAQ.java
@@ -1,13 +1,15 @@
 package hci.gnomex.controller;
 
-import hci.gnomex.security.SecurityAdvisor;
+import hci.gnomex.model.CoreFacility;
 import hci.framework.control.Command;
 import hci.framework.control.RollBackCommandException;
 
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
@@ -20,64 +22,89 @@ import org.jdom.output.XMLOutputter;
 
 
 public class GetFAQ extends GNomExCommand implements Serializable {
-  
-  
-private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetNewsItem.class);
-  
+  private String isManageMode = "";
+  private Set coreFacilities = new HashSet();
+
+
+  private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetNewsItem.class);
+
   public void validate() {
   }
-  
+
   public void loadCommand(HttpServletRequest request, HttpSession session) {
+    if(request.getParameter("isManageMode") != null && !request.getParameter("isManageMode").equals("")) {
+      isManageMode = request.getParameter("isManageMode");
+    }
 
   }
 
   public Command execute() throws RollBackCommandException {
-    
+
     try {
-    	
-//      if (this.getSecurityAdvisor().hasPermission(SecurityAdvisor.CAN_MANAGE_DASHBOARD)) {
 
-        Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
-        Document doc = new Document(new Element("FaqCollection"));
-        StringBuffer buf =  new StringBuffer();
-        
-        buf.append("SELECT f.idFAQ, f.title, f.url, f.idCoreFacility");
-        buf.append(" FROM  FAQ f ");
-  	  	
-  	  	List rows = (List) sess.createQuery(buf.toString()).list();
+      //      if (this.getSecurityAdvisor().hasPermission(SecurityAdvisor.CAN_MANAGE_DASHBOARD)) {
 
-        for (Iterator<Object[]> i1 = rows.iterator(); i1.hasNext();) {
-            Object[] row = (Object[]) i1.next();
-            Element n = new Element("FAQ");
+      Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
 
-            /*
-             * Add values to Element n
-             */
-              n.setAttribute("idFAQ",              			row[0] == null ? "" :  ((Integer)row[0]).toString()); 
-              n.setAttribute("title",              	 		row[1] == null ? "" :  ((String) row[1]));
-              n.setAttribute("url", 						        row[2] == null ? "" :  ((String) row[2]));
-              n.setAttribute("idCoreFacility",	        row[3] == null ? "0" : ((Integer)row[3]).toString());
-              
-              // Add node content to rootElement XML output.
-            doc.getRootElement().addContent(n);
+      Document doc = new Document(new Element("FaqCollection"));
+      StringBuffer buf =  new StringBuffer();
+
+      buf.append("SELECT f.idFAQ, f.title, f.url, f.idCoreFacility");
+      buf.append(" FROM  FAQ f ");
+
+      if(!this.getSecAdvisor().hasPermission(this.getSecAdvisor().CAN_ADMINISTER_ALL_CORE_FACILITIES)) {
+        if(!isManageMode.equals("Y")) {
+          coreFacilities = this.getSecAdvisor().getCoreFacilitiesForMyLab(); 
+        }
+        coreFacilities.addAll(this.getSecAdvisor().getCoreFacilitiesIManage());
+        buf.append(" where f.idCoreFacility is null or f.idCoreFacility in (-1,  ");
+        for(Iterator i = coreFacilities.iterator(); i.hasNext();) {
+          CoreFacility cf = (CoreFacility)i.next();
+          buf.append(cf.getIdCoreFacility());
+          if(i.hasNext()) {
+            buf.append(", ");
+          }
+
         }
 
-        XMLOutputter out = new org.jdom.output.XMLOutputter();
-        this.xmlResult = out.outputString(doc);
-        
-        // Send redirect with response SUCCESS or ERROR page.
-        setResponsePage(this.SUCCESS_JSP);
+        buf.append(")");
+      }
 
-//      } else {
-//        this.addInvalidField("Insufficient permissions", "Insufficient permission to retrieve FAQs.");
-//        setResponsePage(this.ERROR_JSP);
-//      }
+
+      List rows = (List) sess.createQuery(buf.toString()).list();
+
+      for (Iterator<Object[]> i1 = rows.iterator(); i1.hasNext();) {
+        Object[] row = (Object[]) i1.next();
+        Element n = new Element("FAQ");
+
+        /*
+         * Add values to Element n
+         */
+        n.setAttribute("idFAQ",              			row[0] == null ? "" :  ((Integer)row[0]).toString()); 
+        n.setAttribute("title",              	 		row[1] == null ? "" :  ((String) row[1]));
+        n.setAttribute("url", 						        row[2] == null ? "" :  ((String) row[2]));
+        n.setAttribute("idCoreFacility",	        row[3] == null ? "-1" : ((Integer)row[3]).toString());
+
+        // Add node content to rootElement XML output.
+        doc.getRootElement().addContent(n);
+      }
+
+      XMLOutputter out = new org.jdom.output.XMLOutputter();
+      this.xmlResult = out.outputString(doc);
+
+      // Send redirect with response SUCCESS or ERROR page.
+      setResponsePage(this.SUCCESS_JSP);
+
+      //      } else {
+      //        this.addInvalidField("Insufficient permissions", "Insufficient permission to retrieve FAQs.");
+      //        setResponsePage(this.ERROR_JSP);
+      //      }
 
     }catch (NamingException e){
       log.error("An exception has occurred in GetFAQ ", e);
       e.printStackTrace(System.out);
       throw new RollBackCommandException(e.getMessage());
-        
+
     }catch (SQLException e) {
       log.error("An exception has occurred in GetFAQ ", e);
       e.printStackTrace(System.out);
@@ -90,10 +117,10 @@ private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(G
       try {
         this.getSecAdvisor().closeReadOnlyHibernateSession();        
       } catch(Exception e) {
-        
+
       }
     }
-    
+
     return this;
   }
 }
