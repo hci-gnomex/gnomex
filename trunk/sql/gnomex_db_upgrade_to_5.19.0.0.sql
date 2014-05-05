59d78514b84303bff5a8906c7d06206816f135b0
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-05-05 16:42:08 +0000

    Add dashboard related properties and tables.

--- trunk/sql/gnomex_db_upgrade_to_5.19.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.19.0.0.sql
@@ -8,7 +8,23 @@ insert into PropertyDictionary (propertyName, propertyValue, propertyDescription
 	
 insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
 	VALUES ('status_to_start_workflow', 'SUBMITTED', 'What request status is required before work items are shown for that request', 'Y', 1, null);
+
+insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
+	VALUES ('metrix_server_host','localhost','Hostname or IP on which the Illumina statistics Metrix Server is running.','Y', null, null);
 	
+insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
+	VALUES ('metrix_server_port','12345','Port (>1024) on which the Illumina statistics Metrix Server is running.','Y', null, null);
+	
+insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
+	VALUES ('show_activity_dashboard','N','Should the activity feed be shown on the dashboard','N', null, null);
+	
+insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
+	VALUES ('show_metrix_dashboard','N','Should the metrix server feed be shown on the dashboard','N', null, null);
+	
+insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
+	VALUES ('show_news_dashboard','N','Should the news feed be shown on the dashboard','N', null, null);
+
+ 
 alter table Request add includeQubitConcentration CHAR(1) null;  
 call ExecuteIfTableExists('gnomex','Request_Audit','alter table Request_Audit add includeQubitConcentration CHAR(1) null');
  
@@ -59,3 +75,53 @@ call ExecuteIfTableExists('gnomex','RequestCategory_Audit','alter table RequestC
 -- Remove unused properties for sequence alignment
 delete from PropertyDictionary where propertyName='sequence_alignment_supported';
 delete from PropertyDictionary where propertyName='sequence_alignment_server_url';     
+
+-- Dashboard tables
+CREATE TABLE `gnomex`.`NewsItem` (
+	`idNewsItem` INT(10) NOT NULL AUTO_INCREMENT,
+	`idSubmitter` INT(10) NOT NULL,
+	`idCoreSender` INT(10) NOT NULL,
+	`idCoreTarget` INT(10) NULL,
+	`title` VARCHAR(200) NOT NULL,
+	`message` VARCHAR(4000) NOT NULL,
+	`date` DATETIME NULL,
+	CONSTRAINT `FK_NewsItem_Submitter` FOREIGN KEY `FK_NewsItem_Submitter` (`idSubmitter`)
+       REFERENCES `gnomex`.`AppUser` (`idAppUser`)
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+    PRIMARY KEY (`idNewsItem`)
+)
+ENGINE = INNODB;
+
+CREATE TABLE `gnomex`.`FAQ` (
+  `idFAQ` INT(10) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(300) NOT NULL,
+  `url` VARCHAR(500) NOT NULL,
+  `views` INT(10) NULL,
+  PRIMARY KEY (`idFAQ`)
+)
+ENGINE = INNODB;
+
+CREATE TABLE `gnomex`.`Notification` (
+	`idNotification` INT(10) NOT NULL AUTO_INCREMENT,
+	`idUserTarget` INT(10) NOT NULL,
+	`idLabTarget` INT(10) NULL,
+	`sourceType` VARCHAR(20) NOT NULL,
+	`message` VARCHAR(250) NULL,
+	`date` DATETIME NULL,
+	`expID` INT(10) NULL,
+	`type`	VARCHAR(25) NULL,
+	`fullNameUser`	VARCHAR(100) NULL,
+	`imageSource` VARCHAR(50), 
+	PRIMARY KEY (`idNotification`)
+)
+ENGINE = INNODB;
+
+CREATE TABLE `gnomex`.`MetrixObject` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `run_id` VARCHAR(512) NULL,
+  `object_value` VARBINARY(8000),
+  `state` INT(10) NULL,
+  PRIMARY KEY (`id`)
+)
+ENGINE = INNODB;
