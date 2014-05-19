e333df62862446ff55522c66bed867423b81c58e
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-05-19 15:46:42 +0000

    Added note to seq_lane_number_separator and seq_lane_letter properties informing user that if they change these values then they will have to run the update_sequencelane stored procedure otherwise errors will occur.

--- sql/gnomex_db_upgrade_to_5.19.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.19.0.0.sql
@@ -1,10 +1,10 @@
 use gnomex;
 
 insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
-	VALUES ('seq_lane_number_separator', '-', 'The default separator character for sequence lane numbers', 'Y', null, null);
+	VALUES ('seq_lane_number_separator', '-', 'The default separator character for sequence lane numbers.  If you change this value you will have to run the update_sequencelane stored procedure or parsing errors will occur.', 'Y', null, null);
 	
 insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
-	VALUES ('seq_lane_letter', 'F', 'The default letter used in Sequence Lane naming', 'Y', null, null);
+	VALUES ('seq_lane_letter', 'F', 'The default letter used in Sequence Lane naming.  If you change this value you will have to run the update_sequencelane stored procedure or parsing errors will occur.', 'Y', null, null);
 	
 insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory) 
 	VALUES ('status_to_start_workflow', 'SUBMITTED', 'What request status is required before work items are shown for that request', 'Y', 1, null);
