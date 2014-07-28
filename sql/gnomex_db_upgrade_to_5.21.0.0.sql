5cc93c077085dbf13961689b856e5691ecbb1f7a
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-07-28 20:36:10 +0000

    Added new annotationReportField table to the ddl and upgrade scripts.

---
+++ sql/gnomex_db_upgrade_to_5.21.0.0.sql
@@ -0,0 +1,14 @@
+use gnomex;
+
+DROP TABLE IF EXISTS `gnomex`.`AnnotationReportField`;
+CREATE TABLE `gnomex`.`AnnotationReportField` (
+  `idAnnotationReportField` INT(10) NOT NULL AUTO_INCREMENT, 
+  `source` VARCHAR(50) NULL,
+  `fieldName` VARCHAR(50) NULL,
+  `display` VARCHAR(50) NULL,
+  `isCustom` CHAR(1) NULL,
+  `sortOrder` INT(10) NULL,
+  `dictionaryLookUpTable` VARCHAR(100) NULL,
+  PRIMARY KEY (`idAnnotationReportField`)
+)
+ENGINE = INNODB;
