0a1ec8922d0e7a08e3b756f30719ea9ff66b746b
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-08-13 20:17:14 +0000

    Added annotation report field table

--- sql/gnomex_db_upgrade_to_5.21.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.21.0.0.sql
@@ -197,3 +197,17 @@ ALTER TABLE CoreFacility add contactImage VARCHAR(200) NULL;
 call ExecuteIfTableExists('gnomex','CoreFacility_Audit','ALTER TABLE CoreFacility_Audit add contactImage VARCHAR(200) NULL');
 
 
+DROP TABLE IF EXISTS `gnomex`.`AnnotationReportField`;
+CREATE TABLE `gnomex`.`AnnotationReportField` (
+  `idAnnotationReportField` INT(10) NOT NULL AUTO_INCREMENT, 
+  `source` VARCHAR(50) NULL,
+  `fieldName` VARCHAR(50) NULL,
+  `display` VARCHAR(50) NULL,
+  `isCustom` CHAR(1) NULL,
+  `sortOrder` INT(10) NULL,
+  `dictionaryLookUpTable` VARCHAR(100) NULL,
+  PRIMARY KEY (`idAnnotationReportField`)
+)
+ENGINE = INNODB;
+
+
