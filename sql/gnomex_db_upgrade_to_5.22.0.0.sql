ef23638ec3c841d2bf0567c72cfbb3dfce18b4b0
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-09-10 21:54:04 +0000

    Added groupName to sample table for sample grouping on link files tab.

--- sql/gnomex_db_upgrade_to_5.22.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.22.0.0.sql
@@ -3,3 +3,8 @@ use gnomex;
 -- New property to control adapter trimming on illumina experiment creation.
 insert into PropertyDictionary(propertyName, propertyValue, propertyDescription, forServerOnly)
 	values('choose_adapter_trim_default','N','Indicates if adapter trimming should be chosen by default on illumina experiment creation','N');
+	
+	
+	
+ALTER TABLE Sample add groupName VARCHAR(200) NULL;
+call ExecuteIfTableExists('gnomex','Sample_Audit','ALTER TABLE Sample_Audit add groupName VARCHAR(200) NULL');
