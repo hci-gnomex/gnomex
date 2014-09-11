5a4347b6927851aa26d5de565abc30995934c5cf
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-09-11 17:17:13 +0000

    SampleTypeApplication is to be dropped -- don't create audit tables for it anymore.

--- scripts/genMysqlGNomExAuditTrigDropCr8.pl
+++ scripts/genMysqlGNomExAuditTrigDropCr8.pl
@@ -43,7 +43,7 @@ print MYOUTFILE "delimiter \$\$\n\n";
 $tQry = "SELECT t.table_name ".
           "FROM information_schema.tables t ".
          "WHERE t.table_schema = '$tgtDb' ".
-           "AND t.table_name NOT IN ('dtproperties','TransferLog') ".
+           "AND t.table_name NOT IN ('dtproperties','TransferLog','VisitLog','SampleTypeApplication') ".
            "AND t.table_name NOT LIKE '%\_Audit' ESCAPE '\' ".
          "ORDER BY t.table_name";
 
