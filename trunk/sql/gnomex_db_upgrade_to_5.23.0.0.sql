c8f51eb14c765e6615f398876f2da15f959f3e18
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-09-17 21:54:09 +0000

    New upgrade script for 5.23 release

---
+++ trunk/sql/gnomex_db_upgrade_to_5.23.0.0.sql
@@ -0,0 +1,4 @@
+use gnomex;
+
+insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory)
+	VALUES('show_funding_agency', 'N', 'Show the funding agency field for billing accounts', 'N', null, null);
