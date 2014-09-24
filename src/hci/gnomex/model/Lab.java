f00f7a72a0d8e71d5464589823bf2893aecd8506
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-09-24 22:46:23 +0000

    New billing contact phone field for Billing Admin tab.

--- src/hci/gnomex/model/Lab.java
+++ src/hci/gnomex/model/Lab.java
@@ -28,6 +28,7 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
   private String  isCcsgMember;
   private String  isActive;
   private String  excludeUsage;
+  private String  billingContactPhone;
   private String  billingContactEmail;  private Long    version;
   private Set     billingAccounts;
   private Set     members;
@@ -42,114 +43,114 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
   private boolean canManage = false;
   private boolean isMyLab = false;
   private boolean canGuestSubmit = false;
-  
-  
+
+
   public String getContactAddress() {
     return contactAddress;
   }
-  
+
   public void setContactAddress(String contactAddress) {
     this.contactAddress = contactAddress;
   }
-  
+
   public String getContactCity() {
     return contactCity;
   }
-  
+
   public void setContactCity(String contactCity) {
     this.contactCity = contactCity;
   }
-  
+
   public String getContactCodeState() {
     return contactCodeState;
   }
-  
+
   public void setContactCodeState(String contactCodeState) {
     this.contactCodeState = contactCodeState;
   }
-  
+
   public String getContactEmail() {
     return contactEmail;
   }
-  
+
   public void setContactEmail(String contactEmail) {
     this.contactEmail = contactEmail;
   }
-  
+
   public String getContactName() {
     return contactName;
   }
-  
+
   public void setContactName(String contactName) {
     this.contactName = contactName;
   }
-  
+
   public String getContactAddress2() {
     return contactAddress2;
   }
-  
+
   public void setContactAddress2(String contactAddress2) {
     this.contactAddress2 = contactAddress2;
   }
-  
+
   public String getContactPhone() {
     return contactPhone;
   }
-  
+
   public void setContactPhone(String contactPhone) {
     this.contactPhone = contactPhone;
   }
-  
+
   public String getContactZip() {
     return contactZip;
   }
-  
+
   public void setContactZip(String contactZip) {
     this.contactZip = contactZip;
   }
-  
+
   public String getContactCountry() {
-	    return contactCountry;
-	  }
-	  
-	  public void setContactCountry(String contactCountry) {
-	    this.contactCountry = contactCountry;
-	  }
-  
+    return contactCountry;
+  }
+
+  public void setContactCountry(String contactCountry) {
+    this.contactCountry = contactCountry;
+  }
+
   public String getDepartment() {
     return department;
   }
-  
+
   public void setDepartment(String department) {
     this.department = department;
   }
-  
+
   public Integer getIdLab() {
     return idLab;
   }
-  
+
   public void setIdLab(Integer idLab) {
     this.idLab = idLab;
   }
-  
-  
+
+
   public String getBillingContactEmail() {
     return billingContactEmail;
   }
-  
+
   public void setBillingContactEmail(String billingContactEmail) {
     this.billingContactEmail = billingContactEmail;
   }
-  
+
 
   public Long getVersion() {
     return version;
   }
-  
+
   public void setVersion(Long version) {
     this.version = version;
   }
-  
+
   public String getName() {
     return getName(true,true,true);
   }
@@ -157,8 +158,8 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
   public String getName( Boolean lastNameFirst, Boolean includeLab ) {
     return getName(lastNameFirst, includeLab, false);
   }
-  
-  
+
+
   public String getName( Boolean lastNameFirst, Boolean includeLab, Boolean includeActiveFlag) {
 
     String name = "";
@@ -191,55 +192,55 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
         name += " (inactive)";
       }
     }
-    
+
     return name;
   }
 
- 
+
   public void registerMethodsToExcludeFromXML() {
     this.excludeMethodFromXML("getAppUsers");
   }
 
-  
+
   public Set getBillingAccounts() {
     return billingAccounts;
   }
 
-  
+
   public void setBillingAccounts(Set billingAccounts) {
     this.billingAccounts = billingAccounts;
   }
 
-  
+
   public Set getMembers() {
     return members;
   }
 
-  
+
   public void setMembers(Set members) {
     this.members = members;
   }
 
-  
+
   public Set getCollaborators() {
     return collaborators;
   }
 
-  
+
   public void setCollaborators(Set collaborators) {
     this.collaborators = collaborators;
   }
 
-  
+
   public Set getManagers() {
     return managers;
   }
 
-  
+
   public void setManagers(Set managers) {
     this.managers = managers;
   }
-  
+
   public String getCanSubmitRequests() {
     if (this.canSubmitRequests) {
       return "Y";
@@ -247,7 +248,7 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
       return "N";
     }
   }
-  
+
   public String getCanGuestSubmit() {
     if (this.canGuestSubmit) {
       return "Y";
@@ -255,15 +256,15 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
       return "N";
     }
   }
-  
+
   public void canSubmitRequests(boolean canDo) {
     canSubmitRequests = canDo;
   }
-  
+
   public void canGuestSubmit(boolean canDo) {
     canGuestSubmit = canDo;
   }
-  
+
   public String getCanManage() {
     if (this.canManage) {
       return "Y";
@@ -275,7 +276,7 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
     canManage = canDo;
   }
 
-  
+
   public String getIsMyLab() {
     if (isMyLab) {
       return "Y";
@@ -284,38 +285,38 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
     }
   }
 
-  
+
   public void isMyLab(boolean isMyLab) {
     this.isMyLab = isMyLab;
   }
 
- 
-  
+
+
   public String getIsCcsgMember() {
     return isCcsgMember;
   }
 
-  
+
   public void setIsCcsgMember(String isCcsgMember) {
     this.isCcsgMember = isCcsgMember;
   }
 
-  
+
   public String getFirstName() {
     return firstName;
   }
 
-  
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
 
-  
+
   public String getLastName() {
     return lastName;
   }
 
-  
+
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
@@ -325,7 +326,7 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
   }
 
   public static String formatLabName(String lastName, String firstName) {
-    
+
     String labName = "";
     if (lastName != null && !lastName.equals("")) {
       labName = lastName;
@@ -342,46 +343,46 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
     return labName;
   }
 
-  
+
   public Set getProjects() {
     return projects;
   }
 
-  
+
   public void setProjects(Set projects) {
     this.projects = projects;
   }
 
-  
+
   public String getIsActive() {
     return isActive;
   }
 
-  
+
   public void setIsActive(String isActive) {
     this.isActive = isActive;
   }
 
-  
+
   public String getIsExternalPricing() {
     return isExternalPricing;
   }
 
-  
+
   public void setIsExternalPricing(String isExternalPricing) {
     this.isExternalPricing = isExternalPricing;
   }
-  
-  
+
+
   public String getIsExternalPricingCommercial() {
     return isExternalPricingCommercial;
   }
 
-  
+
   public void setIsExternalPricingCommercial(String isExternalPricingCommercial) {
     this.isExternalPricingCommercial = isExternalPricingCommercial;
   }
-  
+
   public Set getInstitutions() {
     return institutions;
   }
@@ -389,7 +390,7 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
   public void setInstitutions(Set institutions) {
     this.institutions = institutions;
   }  
-  
+
   public Set getCoreFacilities() {
     return coreFacilities;
   }
@@ -397,12 +398,12 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
   public void setCoreFacilities(Set coreFacilities) {
     this.coreFacilities = coreFacilities;
   }  
-  
+
   public Boolean isExternalLab() {
     return (getIsExternalPricing() != null && getIsExternalPricing().equals("Y"))
     || (getIsExternalPricingCommercial() != null && getIsExternalPricingCommercial().equals("Y"));
   }
-  
+
   public List getApprovedBillingAccounts() {
     ArrayList approvedBillingAccounts = new ArrayList();
     for(Iterator i = getBillingAccounts().iterator(); i.hasNext();) {
@@ -424,7 +425,7 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
     }
     return internalBillingAccounts;
   }
-  
+
   public List getPOBillingAccounts() {
     ArrayList poBillingAccounts = new ArrayList();
     for(Iterator i = getBillingAccounts().iterator(); i.hasNext();) {
@@ -459,12 +460,20 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
   public String getBillingNotificationEmail() {
     return formatBillingNotificationEmail(getContactEmail(), getBillingContactEmail());
   }
-  
+
+  public String getBillingContactPhone() {
+    return billingContactPhone;
+  }
+
+  public void setBillingContactPhone(String billingContactPhone) {
+    this.billingContactPhone = billingContactPhone;
+  }
+
   // Always inclused PI
   public String getWorkAuthSubmitEmail() {
     return formatBillingNotificationEmail(getContactEmail(), getBillingContactEmail());
   }
-  
+
   public static String formatBillingNotificationEmail(String contactEmail, String billingContactEmail) {
     String email = "";
     if (billingContactEmail != null) {
@@ -480,10 +489,10 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
       }
       email += piEmail;
     }
-    
+
     return email;
   }
-  
+
   public Boolean validateVisibilityInLab(VisibilityInterface object) {
     Boolean valid = true;
     if (object != null && object.getCodeVisibility().equals(Visibility.VISIBLE_TO_INSTITUTION_MEMBERS)) {
@@ -499,11 +508,11 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
 
     return valid;
   }
-  
+
   public Integer getDefaultIdInstitutionForLab() {
     return getDefaultIdInstitutionForLab((Set<Institution>)getInstitutions());
   }
-  
+
   public Integer getDefaultIdInstitutionForLab(Collection<Institution> institutions) {
     Integer defaultInst = null;
     Integer onlyInst = null;
@@ -522,11 +531,11 @@ public class Lab extends HibernateDetailObject implements java.lang.Comparable {
         }
       }
     }
-    
+
     if (defaultInst == null && onlyInst != null) {
       defaultInst = onlyInst;
     }
-    
+
     return defaultInst;
   }
 
