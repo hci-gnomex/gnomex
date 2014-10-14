d8a391d5c66a118430def65d629d7fa04b543a5e
bspiglejr <bspiglejr@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-10-14 18:11:37 +0000

    Fix errors in the sql update script

--- trunk/sql/gnomex_db_upgrade_to_5.23.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.23.0.0.sql
@@ -1,35 +1,28 @@
 use gnomex;
 
---Hide/Show property for funding agency combo box on new billing account window
+-- Hide/Show property for funding agency combo box on new billing account window
 insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory)
 	VALUES('show_funding_agency', 'N', 'Show the funding agency field for billing accounts', 'N', null, null);
 	
 
---Add billing account to ProductOrder	
+-- Add billing account to ProductOrder	
 alter table ProductOrder add idBillingAccount int null;
 call ExecuteIfTableExists('gnomex','ProductOrder_Audit','ALTER TABLE ProductOrder_Audit add idBillingAccount int null');
 
 
---Property to specify where purchase order forms are stored.
+-- Property to specify where purchase order forms are stored.
 insert into PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory)
 	VALUES('product_order_directory', '/home/gnomex/PurchseOrder_HSCGenomics', 'Directory to store purchase order forms', 'Y', null, null);
 
 	
-	--Add columns to CoreFacility  
-ALTER TABLE CoreFacility add labPhone VARCHAR(200) NULL;
-call ExecuteIfTableExists('gnomex','CoreFacility_Audit','ALTER TABLE CoreFacility_Audit add labPhone VARCHAR(200) NULL');
-ALTER TABLE CoreFacility add contactRoom VARCHAR(200) NULL;
-call ExecuteIfTableExists('gnomex','CoreFacility_Audit','ALTER TABLE CoreFacility_Audit add contactRoom VARCHAR(200) NULL');
-ALTER TABLE CoreFacility add labRoom VARCHAR(200) NULL;
-call ExecuteIfTableExists('gnomex','CoreFacility_Audit','ALTER TABLE CoreFacility_Audit add labRoom VARCHAR(200) NULL');
 
 
---Delete trim adapter question property.  No longer needed.	
+-- Delete trim adapter question property.  No longer needed.	
 delete from PropertyDictionary where propertyName = 'choose_adapter_trim_default';
 
---new isolation prep type custom annotations
-insert into AnnotationReportField values('REQUEST', 'codeDNAPrepType', 'DNA prep type', 'N', null, 'hci.gnomex.model.DNAPrepType');
-insert into AnnotationReportField values('REQUEST', 'codeRNAPrepType', 'RNA prep type', 'N', null, 'hci.gnomex.model.RNAPrepType');
+-- new isolation prep type custom annotations
+insert into AnnotationReportField(source, fieldName, display, isCustom, sortOrder, dictionaryLookupTable) values('REQUEST', 'codeDNAPrepType', 'DNA prep type', 'N', null, 'hci.gnomex.model.DNAPrepType');
+insert into AnnotationReportField(source, fieldName, display, isCustom, sortOrder, dictionaryLookupTable) values('REQUEST', 'codeRNAPrepType', 'RNA prep type', 'N', null, 'hci.gnomex.model.RNAPrepType');
 
 -- new align to genome build.
 ALTER TABLE Request add alignToGenomeBuild CHAR(1) NULL;
@@ -41,8 +34,10 @@ ALTER TABLE Request add adminNotes VARCHAR(5000) NULL;
 call ExecuteIfTableExists('gnomex','Request_Audit','ALTER TABLE Request_Audit add adminNotes VARCHAR(5000) NULL');
 
 -- new properties for order review screen and experiment edit screen.
-insert into PropertyDictionary values ('is_plate_based_core', 'N', 'Does the core facility deal with plates for experiment submissions?', 'N', null, null);
-insert into PropertyDictionary values ('show_admin_notes_on_request', 'N', 'Show admin notes box on description tab when editing an experiment', 'N', null, null);
+insert into PropertyDictionary(propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory)
+	values ('is_plate_based_core', 'N', 'Does the core facility deal with plates for experiment submissions?', 'N', null, null);
+insert into PropertyDictionary(propertyName, propertyValue, propertyDescription, forServerOnly, idCoreFacility, codeRequestCategory)
+	values ('show_admin_notes_on_request', 'N', 'Show admin notes box on description tab when editing an experiment', 'N', null, null);
 
 -- add idCoreFacility to Application
 Alter Table Application add idCoreFacility INT(10) NULL;
