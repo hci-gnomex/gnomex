1e239ce62c96af59bb3d82ea4e48b2721f687527
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-12-03 18:29:20 +0000

    Show product billing items on billing tab.  Added new product order view that shows details of product orders.  Similar to experiment browse view.  This is a first draft check in.

--- trunk/src/hci/gnomex/model/BillingItem.java
+++ trunk/src/hci/gnomex/model/BillingItem.java
@@ -9,7 +9,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 
 public class BillingItem extends HibernateDetailObject {
-  
+
   private Integer        idBillingItem;
   private String         category;
   private String         description;
@@ -39,51 +39,53 @@ public class BillingItem extends HibernateDetailObject {
   private Invoice        invoice;
   private Integer        idDiskUsageByMonth;
   private Integer        idProductOrder;
-  
+  private Integer        idProductLineItem;
+
+
   public Integer getIdBillingItem() {
     return idBillingItem;
   }
-  
+
   public void setIdBillingItem(Integer idBillingItem) {
     this.idBillingItem = idBillingItem;
   }
-  
+
   public String getCategory() {
     return category;
   }
-  
+
   public void setCategory(String category) {
     this.category = category;
   }
-  
+
   public String getDescription() {
     return description;
   }
-  
+
   public void setDescription(String description) {
     this.description = description;
   }
-  
+
   public Integer getQty() {
     return qty;
   }
-  
+
   public void setQty(Integer qty) {
     this.qty = qty;
   }
-  
+
   public BigDecimal getUnitPrice() {
     return unitPrice;
   }
-  
+
   public void setUnitPrice(BigDecimal unitPrice) {
     this.unitPrice = unitPrice;
   }
-  
+
   public String getTotalPriceDisplay() {
     return "$" + totalPrice;
   }
-  
+
   public BigDecimal getTotalPrice() {
     return totalPrice;
   }
@@ -91,7 +93,7 @@ public class BillingItem extends HibernateDetailObject {
   public void setTotalPrice(BigDecimal totalPrice) {
     this.totalPrice = totalPrice;
   }
-  
+
   public String getInvoicePriceDisplay() {
     return "$" + invoicePrice;
   }
@@ -99,27 +101,27 @@ public class BillingItem extends HibernateDetailObject {
   public BigDecimal getInvoicePrice() {
     return invoicePrice;
   }
-  
+
   public void setInvoicePrice(BigDecimal invoicePrice) {
     this.invoicePrice = invoicePrice;
   }
-  
+
   public String getCodeBillingChargeKind() {
     return codeBillingChargeKind;
   }
-  
+
   public void setCodeBillingChargeKind(String codeBillingChargeKind) {
     this.codeBillingChargeKind = codeBillingChargeKind;
   }
-  
+
   public String getCodeBillingStatus() {
     return codeBillingStatus;
   }
-  
+
   public void setCodeBillingStatus(String codeBillingStatus) {
     this.codeBillingStatus = codeBillingStatus;
   }
-  
+
   public String getCurrentCodeBillingStatus() {
     return currentCodeBillingStatus;
   }
@@ -131,49 +133,49 @@ public class BillingItem extends HibernateDetailObject {
   public Integer getIdBillingPeriod() {
     return idBillingPeriod;
   }
-  
+
   public void setIdBillingPeriod(Integer idBillingPeriod) {
     this.idBillingPeriod = idBillingPeriod;
   }
-  
+
   public Integer getIdRequest() {
     return idRequest;
   }
-  
+
   public void setIdRequest(Integer idRequest) {
     this.idRequest = idRequest;
   }
 
-  
+
   public Integer getIdBillingAccount() {
     return idBillingAccount;
   }
 
-  
+
   public void setIdBillingAccount(Integer idBillingAccount) {
     this.idBillingAccount = idBillingAccount;
   }
 
-  
+
   public BigDecimal getPercentagePrice() {
     return percentagePrice;
   }
 
-  
+
   public void setPercentagePrice(BigDecimal percentagePrice) {
     this.percentagePrice = percentagePrice;
   }
 
-  
+
   public BillingAccount getBillingAccount() {
     return billingAccount;
   }
 
-  
+
   public void setBillingAccount(BillingAccount billingAccount) {
     this.billingAccount = billingAccount;
   }
-  
+
   public Date getCompleteDate()
   {
     return completeDate;
@@ -183,11 +185,11 @@ public class BillingItem extends HibernateDetailObject {
   {
     this.completeDate = completeDate;
   }
-  
+
   public String getCompleteDateOther() {
     return this.formatDate(this.completeDate, this.DATE_OUTPUT_SLASH); 
   }
-  
+
   public String getSplitType() {
     return splitType;
   }
@@ -204,36 +206,36 @@ public class BillingItem extends HibernateDetailObject {
     this.excludeMethodFromXML("getInvoice");
   }
 
-  
+
   public String getNotes() {
     return notes;
   }
 
-  
+
   public void setNotes(String notes) {
     this.notes = notes;
   }
 
-  
+
   public Integer getIdLab() {
     return idLab;
   }
 
-  
+
   public void setIdLab(Integer idLab) {
     this.idLab = idLab;
   }
 
-  
+
   public Lab getLab() {
     return lab;
   }
 
-  
+
   public void setLab(Lab lab) {
     this.lab = lab;
   }
-  
+
   public String getAccountName() {
     if (billingAccount != null) {
       return billingAccount.getAccountName();
@@ -241,6 +243,15 @@ public class BillingItem extends HibernateDetailObject {
       return "";
     }
   }
+
+  public String getAccountNameDisplay() {
+    if(billingAccount != null) {
+      return billingAccount.getAccountNameDisplay();
+    } else {
+      return "";
+    }
+  }
+
   public String getAccountNameAndNumber() {
     if (billingAccount != null) {
       return billingAccount.getAccountNameAndNumber();
@@ -262,7 +273,7 @@ public class BillingItem extends HibernateDetailObject {
       return "";
     }
   }
-  
+
   public String getPercentageDisplay() {
     if (percentagePrice != null) {
       return new Integer(percentagePrice.multiply(new BigDecimal(100)).intValue()).toString() + "%"; 
@@ -271,42 +282,42 @@ public class BillingItem extends HibernateDetailObject {
     }
   }
 
-  
+
   public BillingPeriod getBillingPeriod() {
     return billingPeriod;
   }
 
-  
+
   public void setBillingPeriod(BillingPeriod billingPeriod) {
     this.billingPeriod = billingPeriod;
   }
 
-  
+
   public Integer getIdPriceCategory() {
     return idPriceCategory;
   }
 
-  
+
   public void setIdPriceCategory(Integer idPriceCategory) {
     this.idPriceCategory = idPriceCategory;
   }
 
-  
+
   public Integer getIdPrice() {
     return idPrice;
   }
 
-  
+
   public void setIdPrice(Integer idPrice) {
     this.idPrice = idPrice;
   }
 
-  
+
   public PriceCategory getPriceCategory() {
     return priceCategory;
   }
 
-  
+
   public void setPriceCategory(PriceCategory priceCategory) {
     this.priceCategory = priceCategory;
   }
@@ -320,36 +331,44 @@ public class BillingItem extends HibernateDetailObject {
   {
     this.idCoreFacility = idCoreFacility;
   }
-  
+
   public Integer getIdInvoice() {
     return idInvoice;
   }
   public void setIdInvoice(Integer id) {
     idInvoice = id;
   }
-  
+
   public Invoice getInvoice() {
     return invoice;
   }
   public void setInvoice(Invoice invoice) {
     this.invoice = invoice;
   }
-  
+
   public Integer getIdDiskUsageByMonth() {
     return idDiskUsageByMonth;
   }
   public void setIdDiskUsageByMonth(Integer id) {
     idDiskUsageByMonth = id;
   }
-  
-  
+
+
   public Integer getIdProductOrder() {
     return idProductOrder;
   }
 
-  
+
   public void setIdProductOrder( Integer idProductOrder ) {
     this.idProductOrder = idProductOrder;
+  }
+
+  public Integer getIdProductLineItem() {
+    return idProductLineItem;
+  }
+
+  public void setIdProductLineItem(Integer idProductLineItem) {
+    this.idProductLineItem = idProductLineItem;
   }
 
   private Invoice getInvoiceForBillingItem(Session sess) {
@@ -369,7 +388,7 @@ public class BillingItem extends HibernateDetailObject {
     }
     return inv;
   }
-  
+
   public Boolean resetInvoiceForBillingItem(Session sess) {
     Invoice invFromIds = getInvoiceForBillingItem(sess);
     if (getIdInvoice() == null || invFromIds.getIdInvoice() == null || !invFromIds.getIdInvoice().equals(getIdInvoice())) {
