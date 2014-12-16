b8e6a5237e7471bd385b1ac1558cff58bc62c1f2
meganb <meganb@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-12-16 19:38:04 +0000

    Updates to product order views and controllers. (mbowler)

--- trunk/src/hci/gnomex/controller/GetProductOrderLineItemList.java
+++ trunk/src/hci/gnomex/controller/GetProductOrderLineItemList.java
@@ -2,10 +2,13 @@ package hci.gnomex.controller;
 
 import hci.framework.control.Command;
 import hci.framework.control.RollBackCommandException;
-import hci.gnomex.model.ProductLineItem;
+import hci.gnomex.model.Product;
+import hci.gnomex.model.ProductOrder;
+import hci.gnomex.model.ProductOrderFilter;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -21,14 +24,21 @@ public class GetProductOrderLineItemList extends GNomExCommand implements Serial
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetProductOrderLineItemList.class);
 
   private Integer idProductOrder;
+  private ProductOrderFilter productOrderFilter;
 
+  
   @Override
   public void loadCommand(HttpServletRequest request, HttpSession sess) {
-    if(request.getParameter("idProductOrder") != null && !request.getParameter("idProductOrder").equals("")) {
+    productOrderFilter = new ProductOrderFilter(this.getSecAdvisor());
+    HashMap errors = this.loadDetailObject(request, productOrderFilter);
+    this.addInvalidFields(errors);
+
+    
+   /* if(request.getParameter("idProductOrder") != null && !request.getParameter("idProductOrder").equals("")) {
       idProductOrder = Integer.parseInt(request.getParameter("idProductOrder"));
     } else {
       this.addInvalidField("Missing idProductOrder", "idProductOrder required");
-    }
+    }*/
 
   }
 
@@ -39,23 +49,34 @@ public class GetProductOrderLineItemList extends GNomExCommand implements Serial
       if(this.isValid()) {
         Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
 
-        List lineItemRows = sess.createQuery("SELECT pli from ProductLineItem as pli where pli.idProductOrder = " + idProductOrder).list();
-
         Document doc = new Document(new Element("LineItems"));
+        
+        StringBuffer buf = productOrderFilter.getLineItemQuery();
+        log.info("Query for GetProductOrderList: " + buf.toString());
+
+        List lineItemRows = sess.createQuery(buf.toString()).list();
 
         for(Iterator i = lineItemRows.iterator(); i.hasNext();) {
-          ProductLineItem pli = (ProductLineItem)i.next();
+          Object row[] = (Object[])i.next();
+          ProductOrder po = (ProductOrder) row[0];
+          Integer qty = (Integer) row[1];
+          BigDecimal unitPrice = (BigDecimal) row[2];
+          Product product = (Product) row[3];
+          Integer idProductLineItem = (Integer) row[4];
+          
           Element e = new Element("LineItem");
-
-          Integer qty = pli.getQty();
-          BigDecimal unitPrice = pli.getUnitPrice();
-          e.setAttribute("name", pli.getProduct().getName());
-          e.setAttribute("qty", qty.toString());
-          e.setAttribute("unitPrice", unitPrice.toString());
-          e.setAttribute("totalPrice", unitPrice.multiply(new BigDecimal(qty)).toString());
-
+          e.setAttribute("idProductOrder", getNonNullString( po.getIdProductOrder() ));
+          e.setAttribute("idProductLineItem", getNonNullString( idProductLineItem ));
+          e.setAttribute("name", getNonNullString(product.getName()));
+          e.setAttribute("qty", getNonNullString( qty ));
+          e.setAttribute("unitPrice", getNonNullString( unitPrice ));
+          e.setAttribute("totalPrice", getNonNullString( unitPrice.multiply(new BigDecimal(qty)) ));
+          e.setAttribute( "labName", po.getLab().getFormattedLabName( true ) );
+          e.setAttribute( "submitter", po.getSubmitter().getDisplayName() );
+          
           doc.getRootElement().addContent(e);
         }
+        
 
         XMLOutputter out = new org.jdom.output.XMLOutputter();
         this.xmlResult = out.outputString(doc);
