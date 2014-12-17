418ee2695178f13cc7c11a6170d7712997b9be1b
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2014-12-17 18:08:16 +0000

    Updated productLedger with lab and product

--- trunk/src/hci/gnomex/model/ProductLedger.java
+++ trunk/src/hci/gnomex/model/ProductLedger.java
@@ -8,9 +8,11 @@ import java.sql.Date;
 
 
 public class ProductLedger extends DictionaryEntry implements Serializable {
-  
+
   private Integer     idProductLedger;
   private Integer     idLab;
+  private Lab         lab;
+  private Product     product;
   private Integer     idProduct;
   private Integer     qty;
   private String      comment;
@@ -18,7 +20,7 @@ public class ProductLedger extends DictionaryEntry implements Serializable {
   private Integer     idProductOrder; // To record which product order added to the ledger
   private String      requestNumber;  // To record which request used some of the product
 
-  
+
   public String getDisplay() {
     String display = this.getNonNullString(getIdProductLedger());
     return display;
@@ -28,85 +30,101 @@ public class ProductLedger extends DictionaryEntry implements Serializable {
     return this.getNonNullString(getIdProductLedger());
   }
 
-  
+
   public Integer getIdProductLedger() {
     return idProductLedger;
   }
 
-  
+
   public void setIdProductLedger( Integer idProductLedger ) {
     this.idProductLedger = idProductLedger;
   }
 
-  
+
   public Integer getIdLab() {
     return idLab;
   }
 
-  
+
   public void setIdLab( Integer idLab ) {
     this.idLab = idLab;
   }
 
-  
+
   public Integer getIdProduct() {
     return idProduct;
   }
 
-  
+
   public void setIdProduct( Integer idProduct ) {
     this.idProduct = idProduct;
   }
 
-  
+
   public Integer getQty() {
     return qty;
   }
 
-  
+
   public void setQty( Integer qty ) {
     this.qty = qty;
   }
 
-  
+
   public String getComment() {
     return comment;
   }
 
-  
+
   public void setComment( String comment ) {
     this.comment = comment;
   }
 
-  
+
   public Date getTimeStamp() {
     return timeStamp;
   }
 
-  
+
   public void setTimeStamp( Date timeStamp ) {
     this.timeStamp = timeStamp;
   }
 
-  
+
   public Integer getIdProductOrder() {
     return idProductOrder;
   }
 
-  
+
   public void setIdProductOrder( Integer idProductOrder ) {
     this.idProductOrder = idProductOrder;
   }
 
-  
+
   public String getRequestNumber() {
     return requestNumber;
   }
 
-  
+
   public void setRequestNumber( String requestNumber ) {
     this.requestNumber = requestNumber;
   }
 
- 
+  public Lab getLab() {
+    return lab;
+  }
+
+  public void setLab(Lab lab) {
+    this.lab = lab;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+
 }
