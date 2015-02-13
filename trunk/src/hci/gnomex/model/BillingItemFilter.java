db4f6bc7342aa3abb0df81d3231fef44267d1032
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2015-02-13 18:07:37 +0000

    Altered query to return requests with no billing items that were completed in the given billing period. GNOM-1525

--- trunk/src/hci/gnomex/model/BillingItemFilter.java
+++ trunk/src/hci/gnomex/model/BillingItemFilter.java
@@ -72,7 +72,10 @@ public class BillingItemFilter extends DetailObject {
 
     if (billingPeriod != null) {
       queryBuf.append(" AND        req.createDate >= '" + this.formatDateTime(billingPeriod.getStartDate(),  this.dateOutputStyle) + "'");
-      queryBuf.append(" AND        req.createDate <= '" + this.formatDate(billingPeriod.getEndDate(), this.DATE_OUTPUT_SQL) + " 23:59:59'");      
+      queryBuf.append(" AND        req.createDate <= '" + this.formatDate(billingPeriod.getEndDate(), this.DATE_OUTPUT_SQL) + " 23:59:59'");
+      queryBuf.append(" OR ");
+      queryBuf.append(" (        req.completedDate >= '" + this.formatDate(billingPeriod.getStartDate(), this.DATE_OUTPUT_SQL) + " 23:59:59'");
+      queryBuf.append(" AND        req.completedDate <= '" + this.formatDate(billingPeriod.getEndDate(), this.DATE_OUTPUT_SQL) + " 23:59:59' )");
     }
 
 
@@ -474,7 +477,7 @@ public class BillingItemFilter extends DetailObject {
       queryBuf.append(" bi.idCoreFacility =");
       queryBuf.append(idCoreFacility);
     }
-    
+
     // Search by billing account 
     if (idBillingAccount != null){
       this.addWhereOrAnd();
