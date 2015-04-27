4859d4469a21ff67d90507447f1433c1a2ef76c0
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2015-04-27 17:22:18 +0000

    Fixed bug that was causing xml reflection error because we were sending back blank flow cell id's when the flow cell grid wasn't populated yet.

--- src/hci/gnomex/controller/GetFlowCell.java
+++ src/hci/gnomex/controller/GetFlowCell.java
@@ -30,7 +30,7 @@ public class GetFlowCell extends GNomExCommand implements Serializable {
   private Integer idFlowCell;
 
   public void loadCommand(HttpServletRequest request, HttpSession session) {
-    if (request.getParameter("id") != null) {
+    if (request.getParameter("id") != null && !request.getParameter("id").equals("")) {
       idFlowCell = new Integer(request.getParameter("id"));
     } else {
       this.addInvalidField("idFlowCell", "idFlowCell is required");
