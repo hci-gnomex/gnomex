ad7511a9b52564aeabc0f8f28eb35588a9a27fd0
meganb <meganb@572511d9-adea-4dea-8d56-86744b86a2dd>
2015-08-12 21:10:33 +0000

    More robust delete product functionality. (mbowler)

--- src/hci/gnomex/controller/DeleteProduct.java
+++ src/hci/gnomex/controller/DeleteProduct.java
@@ -10,6 +10,7 @@ import hci.gnomex.utility.DictionaryHelper;
 import hci.gnomex.utility.HibernateSession;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -20,78 +21,121 @@ import org.hibernate.Session;
 
 
 public class DeleteProduct extends GNomExCommand implements Serializable {
-  
+
   // the static field for logging in Log4J
   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(DeleteProduct.class);
-  
-  
+
+
   private Integer      idProduct = null; 
-  
-  
+  private String       resultMessage = "";
+
   public void validate() {
   }
-  
+
   public void loadCommand(HttpServletRequest request, HttpSession session) {
-    
-   if (request.getParameter("idProduct") != null && !request.getParameter("idProduct").equals("")) {
-     idProduct = new Integer(request.getParameter("idProduct"));
-   }
+
+    if (request.getParameter("idProduct") != null && !request.getParameter("idProduct").equals("")) {
+      idProduct = new Integer(request.getParameter("idProduct"));
+    }
 
   }
 
   public Command execute() throws RollBackCommandException {
-    
+
     try {
       Session sess = HibernateSession.currentSession(this.getUsername());
       DictionaryHelper dictionaryHelper = DictionaryHelper.getInstance(sess);
-      
+
       Product product = (Product)sess.load(Product.class, idProduct);
       ProductType pt = dictionaryHelper.getProductTypeObject(product.getCodeProductType());
-           
+
       if (pt != null && ((pt.getIdCoreFacility() != null && this.getSecAdvisor().isCoreFacilityIManage(pt.getIdCoreFacility())) || this.getSecAdvisor().hasPermission(SecurityAdvisor.CAN_ADMINISTER_ALL_CORE_FACILITIES))) {
-          
-        // Get and delete the corresponding price        
+
+        // Get and delete the corresponding price 
         Price price = GetProductList.getProductPrice( sess, product, pt );
         if ( price != null ) {
           product.setIdPrice( null );
+          sess.save( product );
           sess.flush();
           
-          Hibernate.initialize(price.getPriceCriterias());
-          sess.delete(price);
+          if ( getPriceBillingItems(price, sess) == null || getPriceBillingItems( price, sess ).size() == 0 ) {
+            deletePrice( price, sess ); 
+          } else {
+            inactivatePrice( price, sess );
+          }
         }
-        
+
         //
         // Delete product
         //
-        sess.delete(product);
+        if ( getProductLedgerEntries( product, sess ) == null || getProductLedgerEntries( product, sess ).size() == 0 ) {
+          sess.delete(product);
+          resultMessage += product.getDisplay() + " deleted.";
+        } else {
+          inactivateProduct( product, sess );
+          resultMessage += "There are ledger entries for this product.  " + product.getDisplay() + " marked as inactive instead of deleted.";
+        }
         
         sess.flush();
-        
-        this.xmlResult = "<SUCCESS/>";
-      
+
+        this.xmlResult = "<SUCCESS message=\"" + resultMessage + "\"/>";
+
         setResponsePage(this.SUCCESS_JSP);
-        
+
       } else {
         this.addInvalidField("Insufficient permissions", "Insufficient permissions to delete product.");
         setResponsePage(this.ERROR_JSP);
       }
-      
+
     }catch (Exception e){
       log.error("An exception has occurred in DeleteProduct ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-        
+
     }finally {
       try {
         HibernateSession.closeSession();        
       } catch(Exception e) {
-        
+
       }
     }
-    
+
     return this;
   }
-  
- 
 
+  private void deletePrice(Price price, Session sess) {
+      Hibernate.initialize(price.getPriceCriterias());
+      sess.delete(price);
+      sess.flush();
+  }
+
+  private List getPriceBillingItems(Price price, Session sess) {  
+    if ( price == null ) {
+      return null;
+    }
+    String billingItemQuery = "SELECT bi from BillingItem as bi where bi.idPrice=" + price.getIdPrice();
+    List bi = sess.createQuery( billingItemQuery ).list();
+    return bi;
+   }
+  
+  private void inactivatePrice(Price price, Session sess) {   
+    price.setIsActive( "N" );
+    sess.save( price );
+    sess.flush();
+  }
+
+  private List getProductLedgerEntries(Product product, Session sess) {  
+    if ( product == null ) {
+      return null;
+    }
+    String ledgerEntryQuery = "SELECT le from ProductLedger as le where le.idProduct=" + product.getIdProduct();
+    List le = sess.createQuery( ledgerEntryQuery ).list();
+    return le;
+   }
+  
+  private void inactivateProduct(Product product, Session sess) {   
+    product.setIsActive( "N" );
+    sess.save( product );
+    sess.flush();
+  }
 }
