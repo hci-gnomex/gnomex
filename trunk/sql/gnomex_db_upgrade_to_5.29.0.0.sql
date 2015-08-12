43b1e40ad179ee6df1267f702b803ba05079a503
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2015-08-12 21:58:26 +0000

    DB changes for combining RNA isolation and DNA isolation into one experiment type.

--- trunk/sql/gnomex_db_upgrade_to_5.29.0.0.sql
+++ trunk/sql/gnomex_db_upgrade_to_5.29.0.0.sql
@@ -42,3 +42,87 @@ ADD CONSTRAINT `FK_RequestCategory_ProductType` FOREIGN KEY `FK_RequestCategory_
 REFERENCES `gnomex`.`ProductType` (`codeProductType`)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
+
+
+
+
+-- SQL upgrade script for DNA/RNA isolation combining
+
+
+-- Add table Isolation Prep Type  FOREIGN KEY on codeRequestCategory???
+DROP TABLE IF EXISTS `gnomex`.`IsolationPrepType`;
+CREATE TABLE `gnomex`.`IsolationPrepType` ( 
+     `codeIsolationPrepType`	varchar(15) NOT NULL,
+     `isolationPrepType`  		varchar(100) NULL,
+	 `type`			varchar(10) NULL,
+	 `isActive`		char(1) NULL,
+	 `codeRequestCategory` varchar(50) NULL,
+    PRIMARY KEY (`codeIsolationPrepType`),
+	CONSTRAINT `FK_IsolationPrepType_RequestCategory` FOREIGN KEY (`codeRequestCategory`) 
+	REFERENCES `gnomex`.`RequestCategory`(`codeRequestCategory`)
+	ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = INNODB;
+
+
+-- The following are for production
+-- Add all the values from DNAPrepType into isolation prep type
+INSERT INTO IsolationPrepType (codeIsolationPrepType, isolationPrepType, type, isActive, codeRequestCategory)
+SELECT codeDNAPrepType, DNAPrepType, 'DNA', isActive, 'ISOL' from DNAPrepType;
+
+-- Add all values from RNAPrepType into isolation prep type (This will fail on duplicate codePrepType values so update duplicates with prefix _RNA before running)
+INSERT INTO IsolationPrepType (codeIsolationPrepType, isolationPrepType, type, isActive, codeRequestCategory)
+SELECT codeRNAPrepType + '_RNA', RNAPrepType, 'RNA', isActive, 'ISOL' from RNAPrepType;
+
+-- Add codeIsolationPrepType Column
+ALTER TABLE Request ADD COLUMN codeIsolationPrepType varchar(15) null;
+call ExecuteIfTableExists('gnomex', 'Request_Audit', ' ALTER TABLE Request_Audit ADD COLUMN codeIsolationPrepType varchar(15) null');
+
+-- Add the new foreign key to Request for Isolation Prep Type
+alter table Request add 
+  CONSTRAINT `FK_Request_IsolationPrepType` FOREIGN KEY (`codeIsolationPrepType`)
+    REFERENCES `gnomex`.`IsolationPrepType` (`codeIsolationPrepType`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
+	
+	
+-- Remove both foreign keys that reference DNAPrepType and RNAPrepType tables from the Request Table.
+alter table Request drop
+	CONSTRAINT FK_Request_DNAPrepType;
+	
+alter table Request drop
+	CONSTRAINT FK_Request_RNAPrepType;	
+
+
+
+-- Copy codePrepTypes from DNA and RNA prep type columns in Request to isolationPrepType column in request	(Talk to Tim and Megan about these)
+-- For DNA
+UPDATE Request
+SET codeIsolationPrepType = codeDNAPrepType
+FROM Request
+WHERE codeDNAPrepType is not NULL;
+
+-- For RNA
+UPDATE Request
+SET codeIsolationPrepType = codeRNAPrepType
+FROM Request
+WHERE codeIsolationPrepType is null and codeRNAPrepType is not null;
+
+
+
+-- Remove dna and rna code prep type columns from Request
+ALTER TABLE Request DROP COLUMN codeDNAPrepType;
+ALTER TABLE Request DROP COLUMN codeRNAPrepType;
+
+
+-- New request category for nucleic Acid Isolation
+insert into RequestCategory (codeRequestCategory, requestCategory, idVendor, isActive, numberOfChannels, notes, icon, type, sortOrder, idOrganism, 
+isInternal, isExternal, idCoreFacility, refrainFromAutoDelete, isClinicalResearch, isOwnerOnly, sampleBatchSize, codeProductType, associatedWithAnalysis)
+VALUES('ISOL', 'Nucleic Acid Isolation', null, 'Y', 1, null, 'assets/DNA_test_tube.png', 'ISOLATION', NULL, NULL, 'Y', 'N', 3, NULL, 'N',
+NULL, NULL, NULL, NULL)
+
+
+-- New price category for nucleic acid isolation
+INSERT into PriceCategory (name, description, codeBillingChargeKind, pluginClassName, dictionaryClassNameFilter1, dictionaryClassNameFilter2, isActive)
+VALUES ('Nucleic Acid Isolation', null, 'SERVICE', 'hci.gnomex.billing.SequenomIsolationExtractPlugin', 'hci.gnomex.model.IsolationPrepType', null, 'Y');
+
