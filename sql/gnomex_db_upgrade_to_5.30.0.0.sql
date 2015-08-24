d6fcb4ff7acc8609075ace57864eec61d520ab44
tc1686 <tc1686@572511d9-adea-4dea-8d56-86744b86a2dd>
2015-08-24 15:51:23 +0000

    Drop dnaPrepType foreign key on Request. Drop dna and rna prep type columns on Request.

---
+++ sql/gnomex_db_upgrade_to_5.30.0.0.sql
@@ -0,0 +1,10 @@
+use gnomex;
+
+-- Remove both foreign keys that reference DNAPrepType and RNAPrepType tables from the Request Table.
+alter table Request drop
+	FOREIGN KEY FK_Request_DNAPrepType;
+	
+-- Remove dna and rna code prep type columns from Request
+ALTER TABLE Request DROP COLUMN codeDNAPrepType;
+ALTER TABLE Request DROP COLUMN codeRNAPrepType;
+
