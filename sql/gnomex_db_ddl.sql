90501863d0cd58422699aa028aaec3b67b429204
michaelmorrismm <michaelmorrismm@572511d9-adea-4dea-8d56-86744b86a2dd>
2015-09-04 14:18:37 +0000

    Added y/n field to Product for batching samples by product use quantity, configurable in the Products window. This field can be set and property-driven warnings appear correctly, however, the actual batching of samples does not work yet. (mmorris)

--- sql/gnomex_db_ddl.sql
+++ sql/gnomex_db_ddl.sql
@@ -705,6 +705,7 @@ CREATE TABLE `gnomex`.`Product` (
   `useQty` INT(10) NULL,
   `catalogNumber` VARCHAR(100) NULL,
   `isActive` CHAR(1) NULL,
+  `batchSamplesByUseQuantity` CHAR(1) NULL,
   PRIMARY KEY (`idProduct`),
   CONSTRAINT `FK_Product_Price` FOREIGN KEY `FK_Product_Price` (`idPrice`)
     REFERENCES `gnomex`.`Price` (`idPrice`)
