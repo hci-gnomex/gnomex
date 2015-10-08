550573fba9b4e9bb2d5291f3ec9bdca18e73de04
michaelmorrismm <michaelmorrismm@572511d9-adea-4dea-8d56-86744b86a2dd>
2015-10-08 14:50:17 +0000

    Added scripts to remove old view_limit properties and add the new ones. (mmorris)

--- sql/gnomex_db_upgrade_to_5.31.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.31.0.0.sql
@@ -10,3 +10,30 @@ alter table Request drop
 -- Remove dna and rna code prep type columns from Request
 ALTER TABLE Request DROP COLUMN codeDNAPrepType;
 ALTER TABLE Request DROP COLUMN codeRNAPrepType;
+
+-- Add new view_limit properties
+INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly)
+VALUES ('view_limit_analyses', '1000', 'The maximum number of analyses returned from the back-end.', 'N');
+ 
+INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly)
+VALUES ('view_limit_datatracks', '200', 'The maximum number of data tracks returned from the back-end.', 'N');
+
+INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly)
+VALUES ('view_limit_chromatograms', '1000', 'The maximum number of chromatograms returned from the back-end.', 'N');
+ 
+INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly)
+VALUES ('view_limit_experiments', '100', 'The maximum number of experiments returned from the back-end.', 'N');
+ 
+INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly)
+VALUES ('view_limit_plates_and_runs', '200', 'The maximum number of plates and runs returned from the back-end.', 'N');
+
+-- Remove out-dated view_limit properties
+DELETE FROM PropertyDictionary WHERE propertyName = 'analysis_view_limit';
+
+DELETE FROM PropertyDictionary WHERE propertyName = 'datatrack_view_limit';
+
+DELETE FROM PropertyDictionary WHERE propertyName = 'chromatogram_view_limit';
+
+DELETE FROM PropertyDictionary WHERE propertyName = 'experiment_view_limit';
+
+DELETE FROM PropertyDictionary WHERE propertyName = 'plate_and_run_view_limit';
