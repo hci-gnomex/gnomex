4b0f677912243ce5a1a36e753afe83d34e32e393
Megan Bowler <megan.bowler@hci.utah.edu>
2015-10-30 14:45:38 -0600

    Upgrade scripts to change idProductLineItem to idProductOrder for BillingItem (mbowler)

--- sql/gnomex_db_upgrade_to_5.31.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.31.0.0.sql
@@ -3,9 +3,9 @@ use gnomex;
 -- Remove both foreign keys that reference DNAPrepType and RNAPrepType tables from the Request Table.
 alter table Request drop
        FOREIGN KEY FK_Request_DNAPrepType;
-	   
+     
 alter table Request drop
-		FOREIGN KEY FK_Request_RNAPrepType;
+    FOREIGN KEY FK_Request_RNAPrepType;
        
 -- Remove dna and rna code prep type columns from Request
 ALTER TABLE Request DROP COLUMN codeDNAPrepType;
@@ -41,3 +41,24 @@ DELETE FROM PropertyDictionary WHERE propertyName = 'plate_and_run_view_limit';
 -- Add product_sheet_name property
 INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly)
 VALUES ('product_sheet_name', 'Product Purchasing', 'The name of the price sheet for products.', 'N');
+
+-- Move BillingItem from ProductLineItem to ProductOrder --
+-- Drop foreign key idProductLineItem from BillingItem
+ALTER TABLE BillingItem DROP CONSTRAINT FK_BillingItem_ProductLineItem;
+
+-- Change idProductLineItem to idProduct Order on BillingItem
+ALTER TABLE BillingItem change column idProductLineItem idProductOrder INT(10) NULL;
+
+-- Script to update idProductLineItem to idProduct Order on existing BillingItems
+update bi
+set bi.idProductOrder = pli.idProductOrder
+from BillingItem bi 
+join ProductLineItem pli on bi.idProductOrder = pli.idProductLineItem
+where bi.idProductOrder is not null
+  and bi.idProductOrder = pli.idProductLineItem;
+
+-- Add foreign key idProductOrder to BillingItem
+ALTER TABLE BillingItem ADD CONSTRAINT `FK_BillingItem_ProductOrder` FOREIGN KEY `FK_BillingItem_ProductOrder` (`idProductOrder`)
+    REFERENCES `gnomex`.`ProductOrder` (`idProductOrder`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
