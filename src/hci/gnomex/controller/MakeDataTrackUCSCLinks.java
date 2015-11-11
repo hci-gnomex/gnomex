48b4a2a9770ab1756ebf8d723beba212619a1f81
tmaness <Tim.Maness@hci.utah.edu>
2015-11-10 17:51:44 -0700

    Allow UCSC data track viewer to be invoked with a filename rather than data track.

--- src/hci/gnomex/controller/MakeDataTrackUCSCLinks.java
+++ src/hci/gnomex/controller/MakeDataTrackUCSCLinks.java
@@ -3,6 +3,7 @@ package hci.gnomex.controller;
 import hci.framework.control.Command;
 import hci.framework.control.RollBackCommandException;
 import hci.gnomex.constants.Constants;
+import hci.gnomex.model.Analysis;
 import hci.gnomex.model.AnalysisFile;
 import hci.gnomex.model.DataTrack;
 import hci.gnomex.model.DataTrackFile;
@@ -44,6 +45,8 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 	private String dataTrackFileServerURL;
 	private String dataTrackFileServerWebContext;
 	private Integer idAnalysisFile;
+	private Integer idAnalysis;
+	private String pathName;
 
 	public static final Pattern TO_STRIP = Pattern.compile("\\n");
 
@@ -55,17 +58,29 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 	}
 
 	public void loadCommand(HttpServletRequest request, HttpSession session) {
+		idDataTrack = null;
 		if (request.getParameter("idDataTrack") != null && !request.getParameter("idDataTrack").equals("")) {
 			idDataTrack = new Integer(request.getParameter("idDataTrack"));   
-		} else {
-			this.addInvalidField("idDataTrack", "idDataTrack is required");
-		}
+		} 
 
 	    // if idAnalysisFile is a parameter we will need to figure out idDataTrack
 	    idAnalysisFile = null;
 	    if (request.getParameter("idAnalysisFile") != null && !request.getParameter("idAnalysisFile").equals("")) {
 	    	idAnalysisFile = new Integer(request.getParameter("idAnalysisFile"));   
-	      }    
+	      }
+	    
+	    // are we doing this based on files or datatracks?
+	    pathName = null;
+	    if (request.getParameter("pathName") != null && !request.getParameter("pathName").equals("")) {
+	    	pathName = request.getParameter("pathName");   
+	      }
+
+	    // we need to have idAnalysis if dealing with a file
+	    idAnalysis = null;
+	    if (request.getParameter("idAnalysis") != null && !request.getParameter("idAnalysis").equals("")) {
+	    	idAnalysis = new Integer(request.getParameter("idAnalysis"));   
+	      }
+	    
 		
 		serverName = request.getServerName();
 	}
@@ -89,9 +104,7 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 		      idDataTrack = getidDataTrack (idAnalysisFile,sess);
 		    }
 			
-			DataTrack dataTrack = DataTrack.class.cast(sess.load(DataTrack.class, idDataTrack));
-
-			if (this.getSecAdvisor().canRead(dataTrack)) {
+			if (pathName != null || this.getSecAdvisor().canRead(DataTrack.class.cast(sess.load(DataTrack.class, idDataTrack)))) {
 
 
 				//make links fetching url(s)
@@ -135,32 +148,96 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 	private ArrayList<String>  makeUCSCLink(Session sess) throws Exception {
 
 		ArrayList<String> urlsToLoad = new ArrayList<String>();
+		
 		// What is the users preferred ucsc url?
 		String ucscUrl = this.getSecAdvisor().getUserUcscUrl();
 
-		//load dataTrack
-		DataTrack dataTrack = DataTrack.class.cast(sess.load(DataTrack.class, idDataTrack));      
+	    File[] filesToLink = null;
+	    String ucscGenomeBuildName = null;
+	    String summary = "";
+	    UCSCLinkFiles link = null;
+	    String DTName = "";
+	    String DTNumber = "";
+	    String datasetName = "";
+		if (pathName == null) {
+			//load dataTrack
+			DataTrack dataTrack = DataTrack.class.cast(sess.load(DataTrack.class, idDataTrack));      
 
-		//check genome has UCSC name
-		GenomeBuild gv = GenomeBuild.class.cast(sess.load(GenomeBuild.class, dataTrack.getIdGenomeBuild()));
-		String ucscGenomeBuildName = gv.getUcscName();
-		if (ucscGenomeBuildName == null || ucscGenomeBuildName.length() ==0) throw new Exception ("Missing UCSC Genome Version name, update, and resubmit.");
+			//check genome has UCSC name
+			GenomeBuild gv = GenomeBuild.class.cast(sess.load(GenomeBuild.class, dataTrack.getIdGenomeBuild()));
+			ucscGenomeBuildName = gv.getUcscName();
+			if (ucscGenomeBuildName == null || ucscGenomeBuildName.length() ==0) throw new Exception ("Missing UCSC Genome Version name, update, and resubmit.");
 
-		List<File> dataTrackFiles = dataTrack.getFiles(baseDir, analysisBaseDir);
+			List<File> dataTrackFiles = dataTrack.getFiles(baseDir, analysisBaseDir);
 
-		//check if dataTrack has exportable file type (xxx.bam, xxx.bai, xxx.bw, xxx.bb, xxx.vcf.gz, xxx.vcf.gz.tbi, xxx.useq (will be converted if autoConvert is true))
-		UCSCLinkFiles link = DataTrackUtil.fetchUCSCLinkFiles(dataTrackFiles, GNomExFrontController.getWebContextPath());
-		if (link == null) {
-		  throw new Exception ("No files to link?!");
-		}
-		File[] filesToLink = link.getFilesToLink();
-		if (filesToLink == null) {
-		  throw new Exception ("No files to link?!");
-		}
+			//check if dataTrack has exportable file type (xxx.bam, xxx.bai, xxx.bw, xxx.bb, xxx.vcf.gz, xxx.vcf.gz.tbi, xxx.useq (will be converted if autoConvert is true))
+			link = DataTrackUtil.fetchUCSCLinkFiles(dataTrackFiles, GNomExFrontController.getWebContextPath());
+			if (link == null) {
+				throw new Exception ("No files to link?!");
+			}
+			filesToLink = link.getFilesToLink();
+			if (filesToLink == null) {
+				throw new Exception ("No files to link?!");
+			}
 
-		// When new .bw/.bb files are created, add analysis files and then link via data
-		// track file to the data track.
-		registerDataTrackFiles(sess, analysisBaseDir, dataTrack, filesToLink);
+			// When new .bw/.bb files are created, add analysis files and then link via data
+			// track file to the data track.
+			registerDataTrackFiles(sess, analysisBaseDir, dataTrack, filesToLink);
+			
+			//is there a summary?
+			summary = dataTrack.getSummary();
+			if (summary !=null && summary.trim().length() !=0) {
+				summary = Constants.HTML_BRACKETS.matcher(summary).replaceAll("");
+				summary = "description=\""+summary+"\"";
+			}
+			else summary = "";
+			
+			DTName = dataTrack.getName();
+			DTNumber = dataTrack.getFileName();
+			
+	    } else {
+	    	
+			//check genome has UCSC name
+	    	Analysis analysis = (Analysis)sess.load(Analysis.class, idAnalysis);
+	    	Set <GenomeBuild> gvs = analysis.getGenomeBuilds();
+	    	if (gvs != null) {
+	    		GenomeBuild gv = gvs.iterator().next();
+	    		ucscGenomeBuildName = gv.getUcscName();
+	    	}
+			if (ucscGenomeBuildName == null || ucscGenomeBuildName.length() ==0) throw new Exception ("Missing UCSC Genome Version name, update, and resubmit.");
+
+	    	// the file we want to link to
+	    	filesToLink = new File[2];
+	    	filesToLink[0] = new File(pathName);
+	    	
+	    	// get the data track name out of the pathName
+	    	String thename = pathName.toLowerCase().replace("\\", "/");
+	    	if (thename.lastIndexOf("/") != -1) {
+	    		thename = thename.substring(thename.lastIndexOf("/")+1);
+	    	}
+	    	
+	    	int ipos = thename.lastIndexOf(".");
+	    	int jpos = thename.lastIndexOf(".vcf.gz");
+	    	if (jpos != -1) ipos = jpos;
+	    	
+	    	DTName = thename.substring(0,ipos);
+	    	DTNumber = "DTNone";
+	    	
+	    	// add the correct index file
+	    	if (pathName.endsWith(".vcf.gz")) {
+	    		filesToLink[1] = new File(pathName + ".tbi");
+	    	} else {
+	    		// figure out whether the .bam.bai or the .bai file exists
+	    		File bambai = new File (pathName + ".bai");
+	    		if (bambai.exists()) {
+	    			filesToLink[1] = new File(pathName + ".bai");
+	    		} else {
+	    			// we will assume the index file ends in .bai (without the .bam)
+	    			// we don't check or complain if it doesn't because the user can't do anything anyway
+	    			filesToLink[1] = new File (pathName.substring(0, pathName.length() - 4) + ".bai");
+	    		}
+	    	}
+	    }
 
 		//look and or make directory to hold softlinks to data, also removes old softlinks
 		File urlLinkDir = DataTrackUtil.checkUCSCLinkDirectory(baseURL, dataTrackFileServerWebContext);
@@ -171,9 +248,6 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 			linkPath = UUID.randomUUID().toString() + username;
 		}
 
-		//if (randomWord.length() > 6) randomWord = randomWord.substring(0, 6) +"_"+gv.getDas2Name();
-		//if (ucscGenomeBuildName != null && ucscGenomeBuildName.length() !=0) randomWord = randomWord+"_"+ ucscGenomeBuildName;
-
 		//Create the users' data directory
 		File dir = new File(urlLinkDir.getAbsoluteFile(),linkPath);
 		if (!dir.exists())
@@ -181,17 +255,6 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 
 		//what data type (bam, bigBed, bigWig, vcfTabix)
 		String type = "type=" + DataTrackUtil.fetchUCSCDataType (filesToLink);
-
-		//is there a summary?
-		String summary = dataTrack.getSummary();
-		if (summary !=null && summary.trim().length() !=0) {
-			summary = Constants.HTML_BRACKETS.matcher(summary).replaceAll("");
-			summary = "description=\""+summary+"\"";
-		}
-		else summary = "";
-
-		//TODO: color indicated? look for property named color, convert to RGB, comma delimited and set 'color='
-
 		
 		//for each file, there might be two for xxx.bam and xxx.bai files, xxx.vcf.gz and xxx.vcf.gz.tbi, possibly two for converted useq files, plus/minus strands, otherwise just one.
 		String customHttpLink = null;
@@ -208,13 +271,13 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 
 			//stranded?
 			String strand = "";
-			if (link.isStranded()){
+			if (link != null && link.isStranded()){
 				if (annoString.endsWith("_Plus.bw")) strand = " + ";
 				else if (annoString.endsWith("_Minus.bw")) strand = " - ";
 				else throw new Exception ("\nCan't determine strand of bw file? "+annoString);
 			}
 
-			String datasetName = "name=\""+dataTrack.getName()+ strand +" "+dataTrack.getFileName()+"\"";
+			datasetName = "name=\""+DTName+ strand +" "+DTNumber+"\"";
 
 			//make bigData URL e.g. bigDataUrl=http://genome.ucsc.edu/goldenPath/help/examples/bigBedExample.bb
 			int index = annoString.indexOf(Constants.URL_LINK_DIR_NAME);
@@ -225,8 +288,6 @@ public class MakeDataTrackUCSCLinks extends GNomExCommand implements Serializabl
 			customHttpLink = ucscUrl + "/cgi-bin/hgTracks?db=" + ucscGenomeBuildName + "&hgct_customText=track+visibility=full+";
 			toEncode = type +" "+ datasetName +" "+ summary +" "+ bigDataUrl;
 
-			//System.out.println("LinkForLoading "+customHttpLink + toEncode);
-			//System.out.println(customHttpLink+ GeneralUtils.URLEncode(toEncode)+"\n");
 
 			//remove any returns
 			toEncode = TO_STRIP.matcher(toEncode).replaceAll(" ");
