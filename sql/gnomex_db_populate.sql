1dae09248a5747ac45b69f792864743dd397ce88
Tarik <tarik.courdy@hci.utah.edu>
2015-12-21 10:59:54 -0700

    Removed duplicate property directory_product_order from the Property Dictionary table

--- sql/gnomex_db_populate.sql
+++ sql/gnomex_db_populate.sql
@@ -937,7 +937,6 @@ Test City, UT 12345',
  ('view_limit_experiments', '100', 'The maximum number of experiments returned from the back-end.', 'N'),
  ('view_limit_plates_and_runs', '200', 'The maximum number of plates and runs returned from the back-end.', 'N'),
  ('product_sheet_name', 'Product Purchasing', 'The name of the price sheet for products.', 'N'),
- ('directory_product_order', '/home/gnomex/productOrder/', 'Default directory for product order files', 'N'),
  ('new_request_save_before_submit', 'N', 'Request category property that allows new requests to be saved before the final submission so that they can be edited by the user in the future', 'N'),
  ('request_props_on_confirm_tab', 'Y', 'Show request properties on confirm tab of new experiment setup', 'N');
 
