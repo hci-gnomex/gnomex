b35601da29a820ddeb98de98a1d59a63a75fb5e9
Tarik <tarik.courdy@hci.utah.edu>
2016-01-08 15:40:48 -0700

    gnomexLite visual stylings.  Show busy cursor in experiment tree when clicking on an experiment.  Change the tree and the detail view to smaller scrollable boxes for better user experience. (tcourdy)

--- defaultroot/app/common/NavbarController.js
+++ defaultroot/app/common/NavbarController.js
@@ -3,253 +3,270 @@
  *
  */
 
-var navbar = angular.module('navbar', ['ui.bootstrap','treeControl','treeGrid', 'filters', 'services', 'directives','chosen','ngProgress','dialogs.main','error']);
+var navbar = angular.module('navbar', ['ui.bootstrap', 'treeControl', 'treeGrid', 'filters', 'services', 'directives', 'chosen', 'ngProgress', 'dialogs.main', 'error']);
 
 angular.module('navbar')
-  .factory('SecurityAdvisorService', function($http) {
-    return {
-      create: function() {
-         return $http.get('/gnomexlite/CreateSecurityAdvisor.gx');
-      }
-    }
-  })
-    .factory('PropertyListService', function($http) {
-      return {
-        getPL: function() {
-           return $http.get('/gnomexlite/GetPropertyList.gx');
-        }
-      }
-    })
-  .factory('ManageDictionariesService', function($http) {
-    return {
-      initDict: function() {
-         return $http.get('/gnomexlite/ManageDictionaries.gx?action=load');
-      }
-    }
-  })
-  .factory('LabList', function($http) {
-    return {
-      getLabs: function() {
-         return $http.get('/gnomexlite/GetLabList.gx');
-      }
-    }
-  })
+        .factory('SecurityAdvisorService', function ($http) {
+            return {
+                create: function () {
+                    return $http.get('/gnomexlite/CreateSecurityAdvisor.gx');
+                }
+            }
+        })
+        .factory('PropertyListService', function ($http) {
+            return {
+                getPL: function () {
+                    return $http.get('/gnomexlite/GetPropertyList.gx');
+                }
+            }
+        })
+        .factory('ManageDictionariesService', function ($http) {
+            return {
+                initDict: function () {
+                    return $http.get('/gnomexlite/ManageDictionaries.gx?action=load');
+                }
+            }
+        })
+        .factory('LabList', function ($http) {
+            return {
+                getLabs: function () {
+                    return $http.get('/gnomexlite/GetLabList.gx');
+                }
+            }
+        })
 
 
-.controller("NavbarController",['$modal','$scope','$http','$rootScope','$location','$interval','$route','DynamicDictionary','ngProgress','dialogs','SecurityAdvisorService','PropertyListService','ManageDictionariesService','LabList',
-	function($modal,$scope,$http,$rootScope,$location,$interval,$route,DynamicDictionary,ngProgress,dialogs,SecurityAdvisorService,PropertyListService,ManageDictionariesService,LabList) {
+        .controller("NavbarController", ['$modal', '$scope', '$http', '$rootScope', '$location', '$interval', '$route', 'DynamicDictionary', 'ngProgress', 'dialogs', 'SecurityAdvisorService', 'PropertyListService', 'ManageDictionariesService', 'LabList',
+            function ($modal, $scope, $http, $rootScope, $location, $interval, $route, DynamicDictionary, ngProgress, dialogs, SecurityAdvisorService, PropertyListService, ManageDictionariesService, LabList) {
 
-$rootScope.securityAdvisor = "";
-$rootScope.propertyList = "";
-$rootScope.manageDictionaries = "";
-$rootScope.propertyOptions = [];
-$rootScope.labs = [];
-$rootScope.okToPickLab = true;
-$rootScope.okToFind = true;
+                $rootScope.securityAdvisor = "";
+                $rootScope.propertyList = "";
+                $rootScope.manageDictionaries = "";
+                $rootScope.propertyOptions = [];
+                $rootScope.labs = [];
+                $rootScope.okToPickLab = true;
+                $rootScope.okToFind = true;
+
+                $scope.reportIssueModal = null;
 
 
-	    $rootScope.loggedUser = null;
-	    $rootScope.lastLocation = "dashboard";
-		$rootScope.admin = false;
-		$rootScope.helpMessage = "";
+                $rootScope.loggedUser = null;
+                $rootScope.lastLocation = "dashboard";
+                $rootScope.admin = false;
+                $rootScope.helpMessage = "";
 
-		$rootScope.whatToLookup = "";
-		$rootScope.whatAnalysisToLookup = "";
-		$rootScope.whatDataTrackToLookup = "";
-		$rootScope.whatTopicToLookup = "";
+                $rootScope.whatToLookup = "";
+                $rootScope.whatAnalysisToLookup = "";
+                $rootScope.whatDataTrackToLookup = "";
+                $rootScope.whatTopicToLookup = "";
 
-		$scope.whatToLookup = "";
+                $scope.whatToLookup = "";
 
 // start the world
-activate();
+                activate();
 
-	function activate() {
-		createSecurityAdvisor();
+                function activate() {
+                    createSecurityAdvisor();
 
-	};
+                }
+                ;
 
-    function getPropertyList() {
-       var promise = PropertyListService.getPL();
+                function getPropertyList() {
+                    var promise = PropertyListService.getPL();
 
-       promise.then(
-          function(payload) {
-              $rootScope.propertyList = payload.data;
-              buildPropertyOptions();
-          },
-          function(errorPayload) {
-              console.log('[navbarcontroller] failure getting Property List' + errorPayload);
-          });
-     };
+                    promise.then(
+                            function (payload) {
+                                $rootScope.propertyList = payload.data;
+                                buildPropertyOptions();
+                            },
+                            function (errorPayload) {
+                                console.log('[navbarcontroller] failure getting Property List' + errorPayload);
+                            });
+                }
+                ;
 
-     function getDictionaries() {
-       var promise = ManageDictionariesService.initDict();
+                $scope.showReportModal = function (e) {
+                    $scope.reportIssueModal = $modal.open({
+                        templateUrl: 'app/common/reportIssue.html',
+                        controller: 'ReportIssueController'
+                    });
 
-       promise.then(
-          function(payload) {
-              $rootScope.manageDictionaries = payload.data;
-              getPropertyList();
-          },
-          function(errorPayload) {
-              console.log('[navbarcontroller] failure manage Dictionaries' + errorPayload);
-          });
-     };
+                };
 
-     function getLabs() {
-       var promise = LabList.getLabs();
+                function getDictionaries() {
+                    var promise = ManageDictionariesService.initDict();
 
-       promise.then(
-          function(payload) {
-              $rootScope.labs = payload.data;
-              $rootScope.okToPickLab = false;
-              $scope.$broadcast('okToPickLab', { message: false });
-              $rootScope.$broadcast('okToPickLab', { message: false });              
-              console.log ("[navbarcontroller] after oktopicklab broadcast");
-          },
-          function(errorPayload) {
-              console.log('[navbarcontroller] failure manage Dictionaries' + errorPayload);
-          });
-     };
+                    promise.then(
+                            function (payload) {
+                                $rootScope.manageDictionaries = payload.data;
+                                getPropertyList();
+                            },
+                            function (errorPayload) {
+                                console.log('[navbarcontroller] failure manage Dictionaries' + errorPayload);
+                            });
+                }
+                ;
 
-     function createSecurityAdvisor() {
-       var promise = SecurityAdvisorService.create();
+                function getLabs() {
+                    var promise = LabList.getLabs();
 
-       promise.then(
-          function(payload) {
-              $rootScope.securityAdvisor = payload.data;
-              getLabs();
-              getDictionaries();
-          },
-          function(errorPayload) {
-              console.log('failure creating security advisor' + errorPayload);
-          });
-     };
+                    promise.then(
+                            function (payload) {
+                                $rootScope.labs = payload.data;
+                                $rootScope.okToPickLab = false;
+                                $scope.$broadcast('okToPickLab', {message: false});
+                                $rootScope.$broadcast('okToPickLab', {message: false});
+                                console.log("[navbarcontroller] after oktopicklab broadcast");
+                            },
+                            function (errorPayload) {
+                                console.log('[navbarcontroller] failure manage Dictionaries' + errorPayload);
+                            });
+                }
+                ;
 
-	function buildPropertyOptions() {
+                function createSecurityAdvisor() {
+                    var promise = SecurityAdvisorService.create();
 
-		$rootScope.propertyOptions = new Object();
+                    promise.then(
+                            function (payload) {
+                                $rootScope.securityAdvisor = payload.data;
+                                getLabs();
+                                getDictionaries();
+                            },
+                            function (errorPayload) {
+                                console.log('failure creating security advisor' + errorPayload);
+                            });
+                }
+                ;
 
-		for (var i=0; i<$rootScope.propertyList.length; i++) {
-			if ($rootScope.propertyList[i].options.length == 0) {
-				continue;
-			}
+                function buildPropertyOptions() {
 
-			var propertyMap = new Object();
+                    $rootScope.propertyOptions = new Object();
 
-			for (var j=0; j<$rootScope.propertyList[i].options.length; j++) {
-				propertyMap[$rootScope.propertyList[i].options[j].value] = $rootScope.propertyList[i].options[j].option;
-			}; // end of for
+                    for (var i = 0; i < $rootScope.propertyList.length; i++) {
+                        if ($rootScope.propertyList[i].options.length == 0) {
+                            continue;
+                        }
 
-			$rootScope.propertyOptions[$rootScope.propertyList[i].value] = angular.copy(propertyMap);
-		}; // end of for
+                        var propertyMap = new Object();
 
-	    //console.log("[buildPropertyOptions] $rootScope.propertyOptions.length: " + $rootScope.propertyOptions.length);
-	    //for (var propertyName in $rootScope.propertyOptions) {
-		//	console.log("[buildPropertyOptions] $rootScope.propertyOptions propertyName: " + propertyName);
-		//};
+                        for (var j = 0; j < $rootScope.propertyList[i].options.length; j++) {
+                            propertyMap[$rootScope.propertyList[i].options[j].value] = $rootScope.propertyList[i].options[j].option;
+                        }
+                        ; // end of for
 
-		$rootScope.okToFind = false;
-		$scope.$broadcast('okToFind', { message: false });
-		$rootScope.$broadcast('okToFind', { message: false });
-		console.log ("[navbarcontroller] after okToFind broadcast");
-		//console.log("[navbarcontroller] ** leaving buildPropertyOptions **");
+                        $rootScope.propertyOptions[$rootScope.propertyList[i].value] = angular.copy(propertyMap);
+                    }
+                    ; // end of for
 
-	}; // end of buildPropertyOptions
+                    //console.log("[buildPropertyOptions] $rootScope.propertyOptions.length: " + $rootScope.propertyOptions.length);
+                    //for (var propertyName in $rootScope.propertyOptions) {
+                    //	console.log("[buildPropertyOptions] $rootScope.propertyOptions propertyName: " + propertyName);
+                    //};
 
-	function lookupType (whatToLookup) {
-		var type = "U";
-		if (whatToLookup == null || whatToLookup == "") {
-			return type;
-		}
+                    $rootScope.okToFind = false;
+                    $scope.$broadcast('okToFind', {message: false});
+                    $rootScope.$broadcast('okToFind', {message: false});
+                    console.log("[navbarcontroller] after okToFind broadcast");
+                    //console.log("[navbarcontroller] ** leaving buildPropertyOptions **");
 
-		// assume it's an experiment
-		type = "E";
+                }
+                ; // end of buildPropertyOptions
 
-		// does it start with an "A"
-		var firstChar = whatToLookup.charAt(0);
-		if (firstChar == "A") {
-			type = "A";
-		}
-		else if (firstChar == "D") {
-			type = "D";
-		}
-		else if (firstChar == "T") {
-			type = "T";
-		}
+                function lookupType(whatToLookup) {
+                    var type = "U";
+                    if (whatToLookup == null || whatToLookup == "") {
+                        return type;
+                    }
 
-		return type;
-	}; // end of lookupType
+                    // assume it's an experiment
+                    type = "E";
 
+                    // does it start with an "A"
+                    var firstChar = whatToLookup.charAt(0);
+                    if (firstChar == "A") {
+                        type = "A";
+                    } else if (firstChar == "D") {
+                        type = "D";
+                    } else if (firstChar == "T") {
+                        type = "T";
+                    }
 
-		$scope.lookup = function(msg) {
-			var msgCleaned = msg.trim().toUpperCase();
-			console.log("[lookup] $scope.whatToLookup: " + $scope.whatToLookup + "msg: " + msg + " msgCleaned: " + msgCleaned);
-
-			var whatType = lookupType (msgCleaned);
-			console.log("[lookup] whatType: " + whatType);
-
-			if (whatType == "E") {
-				// broadcast what we're looking up
-				$scope.$broadcast('lookupExperiment', { message: msgCleaned });
-				$rootScope.$broadcast('lookupExperiment', { message: msgCleaned });
-				$scope.whatToLookup = msgCleaned;
-				$rootScope.whatToLookup = msgCleaned;
-
-				// go to experiment
-				$location.path("/experiment");
-			} else if (whatType == "A") {
-				// broadcast what we're looking up
-				$scope.$broadcast('lookupAnalysis', { message: msgCleaned });
-				$rootScope.$broadcast('lookupAnalysis', { message: msgCleaned });
-				$rootScope.whatAnalysisToLookup = msgCleaned;
-
-				// go to analysis
-				$location.path("/analysis");
-			} else if (whatType == "D") {
-				// broadcast what we're looking up
-				$scope.$broadcast('lookupDataTrack', { message: msgCleaned });
-				$rootScope.$broadcast('lookupDataTrack', { message: msgCleaned });
-				$rootScope.whatDataTrackToLookup = msgCleaned;
-
-				// go to datatrack
-				$location.path("/datatrack");
-			} else if (whatType == "T") {
-				// broadcast what we're looking up
-				$scope.$broadcast('lookupTopic', { message: msgCleaned });
-				$rootScope.$broadcast('lookupTopic', { message: msgCleaned });
-				$rootScope.whatTopicToLookup = msgCleaned;
-
-				// go to topic
-				$location.path("/topic");
-			}
-
-		}; // end of lookup
+                    return type;
+                }
+                ; // end of lookupType
 
 
-		$rootScope.$on('$routeChangeStart',function(event, next, prev) {
-			var url = "/dashboard";
-			if (prev != undefined) {
-				url = prev.originalPath;
-			}
+                $scope.lookup = function (msg) {
+                    var msgCleaned = msg.trim().toUpperCase();
+                    console.log("[lookup] $scope.whatToLookup: " + $scope.whatToLookup + "msg: " + msg + " msgCleaned: " + msgCleaned);
 
-		});
+                    var whatType = lookupType(msgCleaned);
+                    console.log("[lookup] whatType: " + whatType);
 
-		$scope.displayHelp = function() {
-			$modal.open({
-	    		templateUrl: 'app/common/userError.html',
-	    		controller: 'userErrorController',
-	    		resolve: {
-	    			title: function() {
-	    				return "Help";
-	    			},
-	    			message: function() {
-	    				return $rootScope.helpMessage;
-	    			}
-	    		}
-	    	});
-		};
+                    if (whatType == "E") {
+                        // broadcast what we're looking up
+                        $scope.$broadcast('lookupExperiment', {message: msgCleaned});
+                        $rootScope.$broadcast('lookupExperiment', {message: msgCleaned});
+                        $scope.whatToLookup = msgCleaned;
+                        $rootScope.whatToLookup = msgCleaned;
+
+                        // go to experiment
+                        $location.path("/experiment");
+                    } else if (whatType == "A") {
+                        // broadcast what we're looking up
+                        $scope.$broadcast('lookupAnalysis', {message: msgCleaned});
+                        $rootScope.$broadcast('lookupAnalysis', {message: msgCleaned});
+                        $rootScope.whatAnalysisToLookup = msgCleaned;
+
+                        // go to analysis
+                        $location.path("/analysis");
+                    } else if (whatType == "D") {
+                        // broadcast what we're looking up
+                        $scope.$broadcast('lookupDataTrack', {message: msgCleaned});
+                        $rootScope.$broadcast('lookupDataTrack', {message: msgCleaned});
+                        $rootScope.whatDataTrackToLookup = msgCleaned;
+
+                        // go to datatrack
+                        $location.path("/datatrack");
+                    } else if (whatType == "T") {
+                        // broadcast what we're looking up
+                        $scope.$broadcast('lookupTopic', {message: msgCleaned});
+                        $rootScope.$broadcast('lookupTopic', {message: msgCleaned});
+                        $rootScope.whatTopicToLookup = msgCleaned;
+
+                        // go to topic
+                        $location.path("/topic");
+                    }
+
+                }; // end of lookup
+
+
+                $rootScope.$on('$routeChangeStart', function (event, next, prev) {
+                    var url = "/dashboard";
+                    if (prev != undefined) {
+                        url = prev.originalPath;
+                    }
+
+                });
+
+                $scope.displayHelp = function () {
+                    $modal.open({
+                        templateUrl: 'app/common/userError.html',
+                        controller: 'userErrorController',
+                        resolve: {
+                            title: function () {
+                                return "Help";
+                            },
+                            message: function () {
+                                return $rootScope.helpMessage;
+                            }
+                        }
+                    });
+                };
 
 
 
 
 
-}]);
+            }]);
