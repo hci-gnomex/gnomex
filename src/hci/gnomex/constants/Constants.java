83168962f16732f632dbeffa0fbb3e07816ab8df
Megan Bowler <megan.bowler@hci.utah.edu>
2016-02-01 19:09:06 -0700

    Bug fix for product error message on experiments with no products (mbowler)

--- src/hci/gnomex/constants/Constants.java
+++ src/hci/gnomex/constants/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
   public static final String   DOWNLOAD_ANALYSIS_SINGLE_FILE_SERVLET          = "DownloadAnalysisSingleFileServlet.gx";
   public static final String   DOWNLOAD_CHROMATOGRAM_FILE_SERVLET             = "DownloadChromatogramFileServlet.gx";
 
-  public static final boolean  REQUIRE_SECURE_REMOTE                          = true;
+  public static final boolean  REQUIRE_SECURE_REMOTE           = true;
 
   public static final String   SQL_SERVER                                     = "SQL SERVER";
 
