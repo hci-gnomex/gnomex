c648493bd839653acbd2910db47d967a43280645
Megan Bowler <megan.bowler@hci.utah.edu>
2016-02-03 15:28:09 -0700

    Move Billing Template changes from 5.32 to 5.33 upgrade script. (mbowler)

--- sql/gnomex_db_upgrade_to_5.33.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.33.0.0.sql
@@ -9,3 +9,99 @@ ALTER TABLE ProductType ADD CONSTRAINT UNQ_ProductType_idCoreFacility_descriptio
 -- Note that this might fail if there are duplicates in the DB already
 -- We will have to null out values where email is not unique.
 ALTER TABLE AppUser ADD CONSTRAINT UNQ_AppUser_email UNIQUE (email);
+
+
+-----------------------------------------------------------------
+-- Billing Template
+-----------------------------------------------------------------
+-- Add BillingTemplate Table
+DROP TABLE IF EXISTS `gnomex`.`BillingTemplate`;
+CREATE TABLE `gnomex`.`BillingTemplate` (
+	`idBillingTemplate` INT(10) NOT NULL AUTO_INCREMENT,
+	`targetClassIdentifier` INT(10) NOT NULL,
+	`targetClassName` VARCHAR(100) NOT NULL,
+	PRIMARY KEY (`idBillingTemplate`)
+)
+ENGINE = INNODB;
+
+-- Add BillingTemplateItem Table
+DROP TABLE IF EXISTS `gnomex`.`BillingTemplateItem`;
+CREATE TABLE `gnomex`.`BillingTemplateItem` (
+	`idBillingTemplateItem` INT(10) NOT NULL AUTO_INCREMENT,
+	`idBillingTemplate` INT(10) NOT NULL,
+	`idBillingAccount` INT(10) NOT NULL,
+	`percentSplit` DECIMAL(4, 3) NULL,
+	`dollarAmount` DECIMAL(7, 2) NULL,
+	`dollarAmountBalance` DECIMAL(7, 2) NULL,
+	PRIMARY KEY (`idBillingTemplateItem`),
+	CONSTRAINT `FK_BillingTemplateItem_BillingAccount` FOREIGN KEY `FK_BillingTemplateItem_BillingAccount` (`idBillingAccount`)
+		REFERENCES `gnomex`.`BillingAccount` (`idBillingAccount`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT `FK_BillingTemplateItem_BillingTemplate` FOREIGN KEY `FK_BillingTemplateItem_BillingTemplate` (`idBillingTemplate`)
+		REFERENCES `gnomex`.`BillingTemplate` (`idBillingTemplate`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+ENGINE = INNODB;
+
+-- Add MasterBillingItem Table
+DROP TABLE IF EXISTS gnomex.MasterBillingItem;
+CREATE TABLE gnomex.MasterBillingItem (
+	idMasterBillingItem INT(10) NOT NULL AUTO_INCREMENT,
+	idBillingTemplate INT(10) NOT NULL,
+	codeBillingChargeKind VARCHAR(10) NULL,
+	category VARCHAR(200) NULL,
+	description VARCHAR(500) NULL,
+	qty INT(10) NULL,
+	unitPrice DECIMAL(7, 2) NULL,
+	totalPrice DECIMAL(9, 2) NULL,
+	idBillingPeriod INT(10) NULL,
+	idPrice INT(10) NULL,
+	idPriceCategory INT(10) NULL,
+	idCoreFacility INT(10) NULL,
+	PRIMARY KEY (idMasterBillingItem),
+	CONSTRAINT FK_MasterBillingItem_BillingChargeKind FOREIGN KEY (codeBillingChargeKind)
+		REFERENCES gnomex.BillingChargeKind (codeBillingChargeKind)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT FK_MasterBillingItem_BillingTemplate FOREIGN KEY (idBillingTemplate)
+		REFERENCES gnomex.BillingTemplate (idBillingTemplate)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT FK_MasterBillingItem_BillingPeriod FOREIGN KEY (idBillingPeriod)
+		REFERENCES gnomex.BillingPeriod (idBillingPeriod)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT FK_MasterBillingItem_Price FOREIGN KEY (idPrice)
+		REFERENCES gnomex.Price (idPrice)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT FK_MasterBillingItem_PriceCategory FOREIGN KEY (idPriceCategory)
+		REFERENCES gnomex.PriceCategory (idPriceCategory)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT FK_MasterBillingItem_CoreFacility FOREIGN KEY (idCoreFacility)
+		REFERENCES gnomex.CoreFacility (idCoreFacility)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+ENGINE = INNODB;
+
+-- Add idMasterBillingItem to BillingItem
+ALTER TABLE BillingItem
+ADD COLUMN idMasterBillingItem INT(10) NULL;
+
+ALTER TABLE BillingItem ADD CONSTRAINT FK_BillingItem_MasterBillingItem FOREIGN KEY FK_BillingItem_MasterBillingItem (idMasterBillingItem)
+    REFERENCES gnomex.MasterBillingItem (idMasterBillingItem)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
+
+CALL ExecuteIfTableExists('gnomex','BillingItem_Audit','ALTER TABLE BillingItem_Audit ADD COLUMN idMasterBillingItem INT(10) NULL');
+
+-- Add sort order to BillingTemplateItem
+ALTER TABLE BillingTemplateItem
+ADD COLUMN sortOrder INT(10) NULL;
+CALL ExecuteIfTableExists('gnomex','BillingTemplateItem_Audit','ALTER TABLE BillingTemplateItem_Audit ADD COLUMN sortOrder INT(10) NULL');
+
+-----------------------------------------------------------------
