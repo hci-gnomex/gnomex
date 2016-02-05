71dff95c192a6ba2251e3d6529b2ebccd83c10d5
Tarik <tarik.courdy@hci.utah.edu>
2016-02-05 14:13:58 -0700

    (GNOM-1925)Allow regular lab members to filter their labs experiments by other members in the lab.  Also removed salt and guid from part of the returned AppUser xml (tcourdy)

--- src/hci/gnomex/controller/GetAppUserList.java
+++ src/hci/gnomex/controller/GetAppUserList.java
@@ -23,99 +23,93 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.output.XMLOutputter;
 
-
 public class GetAppUserList extends GNomExCommand implements Serializable {
-  
-  private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetAppUserList.class);
-  
-  private AppUserFilter filter;
-  private String        listKind = "AppUserList";
 
-  
+  private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetAppUserList.class);
+
+  private AppUserFilter filter;
+  private String listKind = "AppUserList";
+
   public void validate() {
   }
-  
+
   public void loadCommand(HttpServletRequest request, HttpSession session) {
     filter = new AppUserFilter();
     HashMap errors = this.loadDetailObject(request, filter);
     this.addInvalidFields(errors);
-    
+
     if (request.getParameter("listKind") != null && !request.getParameter("listKind").equals("")) {
       listKind = request.getParameter("listKind");
     }
   }
 
   public Command execute() throws RollBackCommandException {
-    
+
     try {
-      
-   
-    Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
-    
-    
-    Document doc = new Document(new Element(listKind));
 
-    StringBuffer buf = filter.getQuery(this.getSecAdvisor());
-    log.debug("App user query: " + buf.toString());
-    List users = (List)sess.createQuery(buf.toString()).list();
+      Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
 
+      Document doc = new Document(new Element(listKind));
 
-    for(Iterator i = users.iterator(); i.hasNext();) {
-      AppUser user = (AppUser)i.next();
+      StringBuffer buf = filter.getQuery(this.getSecAdvisor());
+      log.debug("App user query: " + buf.toString());
+      List users = sess.createQuery(buf.toString()).list();
 
-      // Exclude extra user information
-      user.excludeMethodFromXML("getCodeUserPermissionKind");
-      user.excludeMethodFromXML("getuNID");
-      //user.excludeMethodFromXML("getEmail");
-      user.excludeMethodFromXML("getDepartment");
-      user.excludeMethodFromXML("getInstitute");
-      user.excludeMethodFromXML("getJobTitle");
-      user.excludeMethodFromXML("getCodeUserPermissionKind");
-      user.excludeMethodFromXML("getUserNameExternal");
-      user.excludeMethodFromXML("getPasswordExternal");
-      user.excludeMethodFromXML("getPhone");
-      user.excludeMethodFromXML("getLabs");
-      user.excludeMethodFromXML("getCollaboratingLabs");
-      user.excludeMethodFromXML("getManagingLabs");  
-      user.excludeMethodFromXML("getPasswordExternalEntered");
-      user.excludeMethodFromXML("getIsExternalUser");
-      user.excludeMethodFromXML("getPasswordExternal");
+      for (Iterator i = users.iterator(); i.hasNext();) {
+        AppUser user = (AppUser) i.next();
 
-      
-      doc.getRootElement().addContent(user.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL, null, Annotations.IGNORE).getRootElement());
+        // Exclude extra user information
+        user.excludeMethodFromXML("getCodeUserPermissionKind");
+        user.excludeMethodFromXML("getuNID");
+        // user.excludeMethodFromXML("getEmail");
+        user.excludeMethodFromXML("getDepartment");
+        user.excludeMethodFromXML("getInstitute");
+        user.excludeMethodFromXML("getJobTitle");
+        user.excludeMethodFromXML("getCodeUserPermissionKind");
+        user.excludeMethodFromXML("getUserNameExternal");
+        user.excludeMethodFromXML("getPasswordExternal");
+        user.excludeMethodFromXML("getPhone");
+        user.excludeMethodFromXML("getLabs");
+        user.excludeMethodFromXML("getCollaboratingLabs");
+        user.excludeMethodFromXML("getManagingLabs");
+        user.excludeMethodFromXML("getPasswordExternalEntered");
+        user.excludeMethodFromXML("getIsExternalUser");
+        user.excludeMethodFromXML("getPasswordExternal");
+        user.excludeMethodFromXML("getSalt");
+        user.excludeMethodFromXML("getGuid");
 
-    }
+        doc.getRootElement().addContent(user.toXMLDocument(null, DetailObject.DATE_OUTPUT_SQL, null, Annotations.IGNORE).getRootElement());
 
+      }
 
-    
-    XMLOutputter out = new org.jdom.output.XMLOutputter();
-    this.xmlResult = out.outputString(doc);
-    setResponsePage(this.SUCCESS_JSP);
-    }catch (NamingException e){
+      XMLOutputter out = new org.jdom.output.XMLOutputter();
+      this.xmlResult = out.outputString(doc);
+      setResponsePage(this.SUCCESS_JSP);
+    } catch (NamingException e) {
       log.error("An exception has occurred in GetAppUserList ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-        
-    }catch (SQLException e) {
+
+    } catch (SQLException e) {
       log.error("An exception has occurred in GetAppUserList ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-    } catch (XMLReflectException e){
+    } catch (XMLReflectException e) {
       log.error("An exception has occurred in GetAppUserList ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
-    } catch (Exception e){
+    } catch (Exception e) {
       log.error("An exception has occurred in GetAppUserList ", e);
       e.printStackTrace();
       throw new RollBackCommandException(e.getMessage());
     } finally {
       try {
-        this.getSecAdvisor().closeReadOnlyHibernateSession();        
-      } catch(Exception e) {
-        
+        this.getSecAdvisor().closeReadOnlyHibernateSession();
+      } catch (Exception e) {
+
       }
     }
-    
+
     return this;
   }
 
--- src/hci/gnomex/model/AppUserFilter.java
+++ src/hci/gnomex/model/AppUserFilter.java
@@ -1,78 +1,72 @@
 package hci.gnomex.model;
 
-
 import hci.framework.model.DetailObject;
 import hci.gnomex.security.SecurityAdvisor;
 
 import java.util.Iterator;
 
 public class AppUserFilter extends DetailObject {
-  
-  
-  // Criteria
-  private String                lab;
-  private String                userLastName;
-  private String                userFirstName;
-  private Integer               idLab;
-  private Integer               idAppUser;
 
-  
-  private SecurityAdvisor       secAdvisor;
-  
-  
-  private StringBuffer          queryBuf;
-  private boolean              addWhere = true;
-  
-  
+  // Criteria
+  private String lab;
+  private String userLastName;
+  private String userFirstName;
+  private Integer idLab;
+  private Integer idAppUser;
+  private Boolean membersOnly;
+
+  private SecurityAdvisor secAdvisor;
+
+  private StringBuffer queryBuf;
+  private boolean addWhere = true;
+
   public StringBuffer getQuery(SecurityAdvisor secAdvisor) {
     addWhere = true;
     this.secAdvisor = secAdvisor;
     queryBuf = new StringBuffer();
-    
+
     queryBuf.append(" SELECT distinct user");
-    
+
     getQueryBody(queryBuf);
-    
+
     return queryBuf;
-    
+
   }
-  
+
   public void getQueryBody(StringBuffer queryBuf) {
-    
+
     // We need to join to labs and the core facilities if the user is an admin (not a super admin).
     boolean joinToCoreFacility = false;
-    if (!secAdvisor.hasPermission(SecurityAdvisor.CAN_ADMINISTER_ALL_CORE_FACILITIES) &&
-        secAdvisor.hasPermission(SecurityAdvisor.CAN_ACCESS_ANY_OBJECT)) {
+    if (!secAdvisor.hasPermission(SecurityAdvisor.CAN_ADMINISTER_ALL_CORE_FACILITIES) && secAdvisor.hasPermission(SecurityAdvisor.CAN_ACCESS_ANY_OBJECT)) {
       joinToCoreFacility = true;
     }
 
-    
-    
     queryBuf.append(" FROM           AppUser as user ");
     if (hasLabCriteria() || joinToCoreFacility) {
-      queryBuf.append(" LEFT JOIN           user.labs as lab ");      
-      queryBuf.append(" LEFT JOIN           user.collaboratingLabs as collabLab ");      
-      queryBuf.append(" LEFT JOIN           user.managingLabs as managerLab ");      
+      queryBuf.append(" LEFT JOIN           user.labs as lab ");
+
+      if (membersOnly == false) {
+        queryBuf.append(" LEFT JOIN           user.collaboratingLabs as collabLab ");
+        queryBuf.append(" LEFT JOIN           user.managingLabs as managerLab ");
+      }
     }
-    
+
     addUserCriteria();
     addLabCriteria();
-    
-    addSecurityCriteria();      
-    
+
+    addSecurityCriteria();
+
     queryBuf.append(" order by user.lastName, user.firstName ");
   }
-  
-  
+
   private boolean hasLabCriteria() {
     // If we will add security criteria by authorized lab, join to lab
     if (!secAdvisor.hasPermission(secAdvisor.CAN_ACCESS_ANY_OBJECT)) {
       return true;
     }
-    
+
     // If we have any lab selection criteria, join to lab
-    if ( (lab != null && !lab.equals("")) ||
-         (idLab != null)) {
+    if ((lab != null && !lab.equals("")) || (idLab != null)) {
       return true;
     } else {
       return false;
@@ -80,27 +74,27 @@ public class AppUserFilter extends DetailObject {
   }
 
   private void addUserCriteria() {
-    // Search by user name 
-    if (userLastName != null && !userLastName.equals("")){
+    // Search by user name
+    if (userLastName != null && !userLastName.equals("")) {
       this.addWhereOrAnd();
       queryBuf.append(" user.lastName like '%");
       queryBuf.append(userLastName);
       queryBuf.append("%'");
-    } 
-    if (userFirstName != null && !userFirstName.equals("")){
+    }
+    if (userFirstName != null && !userFirstName.equals("")) {
       this.addWhereOrAnd();
       queryBuf.append(" user.FirstName like '%");
       queryBuf.append(userFirstName);
       queryBuf.append("%'");
-    } 
+    }
     // Search by user id
-    if (idAppUser != null){
+    if (idAppUser != null) {
       this.addWhereOrAnd();
       queryBuf.append(" user.idAppUser = ");
       queryBuf.append(idAppUser);
-    }     
+    }
   }
-  
+
   private void addLabCriteria() {
     // Search by lab name
     if (lab != null && !lab.equals("")) {
@@ -110,108 +104,107 @@ public class AppUserFilter extends DetailObject {
       queryBuf.append("%'");
     }
     // Search by lab id
-    if (idLab != null){
+    if (idLab != null) {
       this.addWhereOrAnd();
       queryBuf.append(" lab.idLab = ");
       queryBuf.append(idLab);
-    }     
+    }
   }
-    
+
   private void addSecurityCriteria() {
-    if (secAdvisor.hasPermission(secAdvisor.CAN_ADMINISTER_ALL_CORE_FACILITIES) ) {
+    if (secAdvisor.hasPermission(secAdvisor.CAN_ADMINISTER_ALL_CORE_FACILITIES)) {
       // No criteria needed for super users
-    } 
-    else if (secAdvisor.hasPermission(secAdvisor.CAN_ACCESS_ANY_OBJECT) ) {
+    } else if (secAdvisor.hasPermission(secAdvisor.CAN_ACCESS_ANY_OBJECT)) {
       // Admins can see users belong to labs that are associated with the core facilties
       // this admin manages.
-      
+
       if (secAdvisor.getCoreFacilitiesIManage().isEmpty()) {
         throw new RuntimeException("Admin is not assigned to any core facilities.  Cannot apply appropriate filter to user query.");
       }
 
     } else if (secAdvisor.getGroupsIManage().size() > 0) {
-      
+
       // Lab managers must be able to add any user to his/her lab,
       // so only criteria applied is to get active gnomex accounts
       this.addWhereOrAnd();
       queryBuf.append(" user.isActive = 'Y' ");
-      
-      
+
     } else if (secAdvisor.hasPermission(secAdvisor.CAN_PARTICIPATE_IN_GROUPS)) {
       this.addWhereOrAnd();
       queryBuf.append(" user.isActive = 'Y' ");
-      
+
       if (this.secAdvisor.getAllMyGroups().size() > 0) {
         // Limit to users who are members of the authorized labs
         this.addWhereOrAnd();
-        
+
         queryBuf.append(" ( ");
-        
-        
+
         queryBuf.append(" lab.idLab in ( ");
-        for(Iterator i =  this.secAdvisor.getAllMyGroups().iterator(); i.hasNext();) {
-          Lab theLab = (Lab)i.next();
+        for (Iterator i = this.secAdvisor.getAllMyGroups().iterator(); i.hasNext();) {
+          Lab theLab = (Lab) i.next();
           queryBuf.append(theLab.getIdLab());
           if (i.hasNext()) {
             queryBuf.append(", ");
           }
-        }      
-        queryBuf.append(" )");  
-        
-        queryBuf.append(" OR ");
-        queryBuf.append(" collabLab.idLab in ( ");
-        for(Iterator i = this.secAdvisor.getAllMyGroups().iterator(); i.hasNext();) {
-          Lab theLab = (Lab)i.next();
-          queryBuf.append(theLab.getIdLab());
-          if (i.hasNext()) {
-            queryBuf.append(", ");
-          }
-        }      
+        }
         queryBuf.append(" )");
 
-        queryBuf.append(" OR ");
-        queryBuf.append(" managerLab.idLab in ( ");            
-        for(Iterator i =  this.secAdvisor.getAllMyGroups().iterator(); i.hasNext();) {
-          Lab theLab = (Lab)i.next();
-          queryBuf.append(theLab.getIdLab());
-          if (i.hasNext()) {
-            queryBuf.append(", ");
+        if (membersOnly == false) {
+          queryBuf.append(" OR ");
+          queryBuf.append(" collabLab.idLab in ( ");
+          for (Iterator i = this.secAdvisor.getAllMyGroups().iterator(); i.hasNext();) {
+            Lab theLab = (Lab) i.next();
+            queryBuf.append(theLab.getIdLab());
+            if (i.hasNext()) {
+              queryBuf.append(", ");
+            }
           }
-        }      
-        queryBuf.append(" )");          
+          queryBuf.append(" )");
+
+          queryBuf.append(" OR ");
+          queryBuf.append(" managerLab.idLab in ( ");
+          for (Iterator i = this.secAdvisor.getAllMyGroups().iterator(); i.hasNext();) {
+            Lab theLab = (Lab) i.next();
+            queryBuf.append(theLab.getIdLab());
+            if (i.hasNext()) {
+              queryBuf.append(", ");
+            }
+          }
+          queryBuf.append(" )");
+        }
 
         queryBuf.append(")");
 
       } else {
-        // We have a user that is not set up in any labs.  Just 
+        // We have a user that is not set up in any labs. Just
         // Limit to this user only.
-        
+
         this.addWhereOrAnd();
         queryBuf.append(" user.idAppUser = ");
         queryBuf.append(secAdvisor.getIdAppUser());
       }
-      
+
     } else {
       // Limit to this user only
       this.addWhereOrAnd();
       queryBuf.append(" user.idAppUser = ");
       queryBuf.append(secAdvisor.getIdAppUser());
     }
-    
+
   }
-  
+
   private void appendCoreFacilityInClause() {
-    for(Iterator i = secAdvisor.getCoreFacilitiesIManage().iterator(); i.hasNext();) {
-      CoreFacility cf = (CoreFacility)i.next();
+    for (Iterator i = secAdvisor.getCoreFacilitiesIManage().iterator(); i.hasNext();) {
+      CoreFacility cf = (CoreFacility) i.next();
       queryBuf.append(cf.getIdCoreFacility());
       if (i.hasNext()) {
         queryBuf.append(", ");
       }
-    }      
+    }
     queryBuf.append(" )");
-    
+
   }
-  
+
   protected boolean addWhereOrAnd() {
     if (addWhere) {
       queryBuf.append(" WHERE ");
@@ -222,69 +215,64 @@ public class AppUserFilter extends DetailObject {
     return addWhere;
   }
 
-  
   public String getLab() {
     return lab;
   }
 
-  
   public void setLab(String lab) {
     this.lab = lab;
   }
 
+  public Boolean getMembersOnly() {
+    return membersOnly;
+  }
 
-  
+  public void setMembersOnly(String membersOnly) {
+    if (membersOnly == null || membersOnly.equals("") || membersOnly.equals("N")) {
+      this.membersOnly = false;
+    } else {
+      this.membersOnly = true;
+    }
+  }
+
   public Integer getIdLab() {
     return idLab;
   }
 
-  
   public void setIdLab(Integer idLab) {
     this.idLab = idLab;
   }
 
-  
   public boolean isAddWhere() {
     return addWhere;
   }
 
-  
   public void setAddWhere(boolean addWhere) {
     this.addWhere = addWhere;
   }
 
-  
   public Integer getIdAppUser() {
     return idAppUser;
   }
 
-  
   public void setIdAppUser(Integer idAppUser) {
     this.idAppUser = idAppUser;
   }
 
-  
   public String getUserFirstName() {
     return userFirstName;
   }
 
-  
   public void setUserFirstName(String userFirstName) {
     this.userFirstName = userFirstName;
   }
 
-  
   public String getUserLastName() {
     return userLastName;
   }
 
-  
   public void setUserLastName(String userLastName) {
     this.userLastName = userLastName;
   }
 
-
-  
- 
-  
 }
