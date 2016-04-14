50abadc0661d164d5e8920e8554cf4aac6698e19
mbowler <megan.bowler@hci.utah.edu>
2016-04-14 16:08:27 -0600

    GNOM-1974 Restore option to show welcome screen with site title and welcome message.  (mbowler)

--- sql/gnomex_db_upgrade_to_5.34.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.34.0.0.sql
@@ -3,3 +3,7 @@ use gnomex;
 alter table Request add column archived char(1) NULL;
 
 CALL ExecuteIfTableExists('gnomex','Request_Audit','ALTER TABLE Request_Audit ADD COLUMN archived CHAR(1) NULL');
+
+--  Add properties for site title
+INSERT INTO PropertyDictionary (propertyName, propertyValue, propertyDescription, forServerOnly )
+VALUES ('show_title_box', 'N', 'Show site title and welcome message on home page', 'N' );
