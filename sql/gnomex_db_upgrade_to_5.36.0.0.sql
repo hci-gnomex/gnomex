cdf595378e7d512db7b16d40dafd643f54fa9e45
Tarik <tarik.courdy@hci.utah.edu>
2016-09-14 14:15:13 -0600

    fixed typo in upgrade script

--- sql/gnomex_db_upgrade_to_5.36.0.0.sql
+++ sql/gnomex_db_upgrade_to_5.36.0.0.sql
@@ -13,8 +13,8 @@ alter table Request add column usedRnase char(1) null;
 alter table Request add column keepSamples char(1) null;
 
 
-CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request add column reagent varchar(50) null');
-CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request add column elutionBuffer varchar(50) null');
-CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request add column usedDnase char(1) null');
-CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request add column usedRnase char(1) null');
-CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request add column keepSamples char(1) null');
+CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request_Audit add column reagent varchar(50) null');
+CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request_Audit add column elutionBuffer varchar(50) null');
+CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request_Audit add column usedDnase char(1) null');
+CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request_Audit add column usedRnase char(1) null');
+CALL ExecuteIfTableExists('gnomex','Request_Audit','alter table Request_Audit add column keepSamples char(1) null');
