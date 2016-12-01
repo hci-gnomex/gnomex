786cc4c07db0bf831686dbc1e4d1d0e25066dc1c
Cody Haroldsen <sharoldsen@hci.utah.edu>
2016-12-01 10:10:03 -0700

    messing about with wildfly

---
+++ src/main/java/hci/gnomex/api/GNomExApplication.java
@@ -0,0 +1,14 @@
+package hci.gnomex.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Cody Haroldsen <cody.haroldsen@hci.utah.edu>
+ * @since 11/30/2016
+ */
+@ApplicationPath("api")
+public class GNomExApplication extends Application {
+  
+}
