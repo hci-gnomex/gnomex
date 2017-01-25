9240bf08e737676798a2168919e6fb0eda479452
matthewbjorge <matthew.bjorge@hci.utah.edu>
2017-01-25 12:10:00 -0700

    GNOM-2217: Email Invoice - remove misleading checkbox and unused functionality

--- src/main/java/hci/gnomex/controller/SendBillingInvoiceEmail.java
+++ src/main/java/hci/gnomex/controller/SendBillingInvoiceEmail.java
@@ -43,14 +43,8 @@ private Integer idCoreFacility;
 
 private SecurityAdvisor secAdvisor;
 private DictionaryHelper dh;
-private BillingPeriod billingPeriod;
-private CoreFacility coreFacility;
-private Lab lab;
-private BillingAccount billingAccount;
 
 private String emailAddress;
-private boolean includeBillingAccountContact;
-private boolean respondInHTML;
 private String serverName;
 
 public void validate() {
@@ -84,17 +78,6 @@ public void loadCommand(HttpServletRequest request, HttpSession session) {
 	if (request.getParameter("emailAddress") != null && !request.getParameter("emailAddress").equals("")) {
 		emailAddress = request.getParameter("emailAddress");
 	}
-	if (request.getParameter("includeBillingAccountContact") != null
-			&& request.getParameter("includeBillingAccountContact").equalsIgnoreCase("Y")) {
-		includeBillingAccountContact = true;
-	} else {
-		includeBillingAccountContact = false;
-	}
-	if (request.getParameter("respondInHTML") != null && request.getParameter("respondInHTML").equalsIgnoreCase("Y")) {
-		respondInHTML = true;
-	} else {
-		respondInHTML = false;
-	}
 
 	secAdvisor = (SecurityAdvisor) session.getAttribute(SecurityAdvisor.SECURITY_ADVISOR_SESSION_KEY);
 	serverName = request.getServerName();
@@ -109,10 +92,10 @@ public Command execute() throws RollBackCommandException {
 
 				Session sess = secAdvisor.getHibernateSession(this.getUsername());
 				dh = DictionaryHelper.getInstance(sess);
-				billingPeriod = dh.getBillingPeriod(idBillingPeriod);
-				coreFacility = (CoreFacility) sess.get(CoreFacility.class, idCoreFacility);
-				lab = (Lab) sess.get(Lab.class, new Integer(idLab));
-				billingAccount = (BillingAccount) sess.get(BillingAccount.class, idBillingAccount);
+				BillingPeriod billingPeriod = dh.getBillingPeriod(idBillingPeriod);
+				CoreFacility coreFacility = (CoreFacility) sess.get(CoreFacility.class, idCoreFacility);
+				Lab lab = (Lab) sess.get(Lab.class, idLab);
+				BillingAccount billingAccount = (BillingAccount) sess.get(BillingAccount.class, idBillingAccount);
 				TreeMap requestMap = new TreeMap();
 				TreeMap billingItemMap = new TreeMap();
 				TreeMap relatedBillingItemMap = new TreeMap();
@@ -143,7 +126,7 @@ public Command execute() throws RollBackCommandException {
 	return this;
 }
 
-private void sendInvoiceEmail(Session sess, String contactEmail, CoreFacility coreFacility,
+private void sendInvoiceEmail(Session sess, String emailRecipients, CoreFacility coreFacility,
 		BillingPeriod billingPeriod, Lab lab, BillingAccount billingAccount, Map billingItemMap,
 		Map relatedBillingItemMap, Map requestMap) throws Exception {
 
@@ -160,33 +143,18 @@ private void sendInvoiceEmail(Session sess, String contactEmail, CoreFacility co
 
 	String note = "";
 	boolean send = false;
-	String emailRecipients = contactEmail;
-	if (includeBillingAccountContact && billingAccount.getLab() != null
-			&& !lab.getIdLab().equals(billingAccount.getLab().getIdLab())) {
-		emailRecipients = this.appendBillingAccountLabEmail(emailRecipients, billingAccount);
-	}
 	String ccList = emailFormatter.getCCList(sess);
 	String fromAddress = coreFacility.getContactEmail();
-	if (emailRecipients.contains(",")) {
-		for (String e : emailRecipients.split(",")) {
-			if (!MailUtil.isValidEmail(e.trim())) {
-				LOG.error("Invalid email address " + e);
-			}
+	for (String email : emailRecipients.split(",")) {
+		if (!MailUtil.isValidEmail(email.trim())) {
+			LOG.error("Invalid email address " + email);
 		}
-	} else if (!MailUtil.isValidEmail(emailRecipients)) {
-		LOG.error("Invalid email address " + emailRecipients);
-	}
-	if (emailRecipients != null && !emailRecipients.equals("")) {
-		send = true;
-	} else {
-		note = "Unable to email billing invoice. Billing contact email is blank for " + lab.getName(false, true);
 	}
 
-	Map[] billingItemMaps = { billingItemMap };
-	Map[] relatedBillingItemMaps = { relatedBillingItemMap };
-	Map[] requestMaps = { requestMap };
-
-	if (send) {
+	if (!emailRecipients.equals("")) {
+		Map[] billingItemMaps = { billingItemMap };
+		Map[] relatedBillingItemMaps = { relatedBillingItemMap };
+		Map[] requestMaps = { requestMap };
 		if (!MailUtil.isValidEmail(fromAddress)) {
 			fromAddress = dh.getPropertyDictionary(PropertyDictionary.GENERIC_NO_REPLY_EMAIL);
 		}
@@ -225,76 +193,18 @@ private void sendInvoiceEmail(Session sess, String contactEmail, CoreFacility co
 			LOG.error("Unable to send invoice email to " + emailRecipients, e);
 			note = "Unable to email invoice to " + emailRecipients + " due to the following error: " + e.toString();
 		}
+	} else {
+		note = "Unable to email billing invoice. Billing contact email is blank for " + lab.getName(false, true);
 	}
 	org.jdom.Document doc = null;
-	if (respondInHTML) {
-		if (note.length() == 0) {
-			note = "&nbsp";
-		}
-		org.jdom.Element root = new org.jdom.Element("HTML");
-		doc = new org.jdom.Document(root);
-
-		org.jdom.Element head = new org.jdom.Element("HEAD");
-		root.addContent(head);
-
-		org.jdom.Element link = new org.jdom.Element("link");
-		link.setAttribute("rel", "stylesheet");
-		link.setAttribute("type", "text/css");
-		link.setAttribute("href", "invoiceForm.css");
-		head.addContent(link);
-
-		org.jdom.Element title = new org.jdom.Element("TITLE");
-		title.addContent("Email Billing Invoice - " + lab.getName(false, true) + " " + billingAccount.getAccountName());
-		head.addContent(title);
-
-		org.jdom.Element responseBody = new org.jdom.Element("BODY");
-		root.addContent(responseBody);
-
-		org.jdom.Element h = new org.jdom.Element("H3");
-		h.addContent(note);
-		responseBody.addContent(h);
-	} else {
-		org.jdom.Element root = new org.jdom.Element("BillingInvoiceEmail");
-		doc = new org.jdom.Document(root);
-		root.setAttribute("note", note);
-		root.setAttribute("title",
-				"Email Billing Invoice - " + lab.getName(false, true) + " " + billingAccount.getAccountName());
-	}
-
+	org.jdom.Element root = new org.jdom.Element("BillingInvoiceEmail");
+	doc = new org.jdom.Document(root);
+	root.setAttribute("note", note);
+	root.setAttribute("title",
+			"Email Billing Invoice - " + lab.getName(false, true) + " " + billingAccount.getAccountName());
 	XMLOutputter out = new org.jdom.output.XMLOutputter();
 	out.setOmitEncoding(true);
 	this.xmlResult = out.outputString(doc);
-}
-
-private String appendBillingAccountLabEmail(String recipients, BillingAccount billingAccount) {
-	StringBuffer allRecipients = new StringBuffer(recipients);
-
-	if (allRecipients.length() > 0) {
-		allRecipients.append(",");
-	}
-
-	Lab lab = billingAccount.getLab();
-	if (lab.getBillingContactEmail() != null && !lab.getBillingContactEmail().equals("")
-			&& allRecipients.indexOf(lab.getBillingContactEmail()) == -1) {
-		allRecipients.append(lab.getBillingContactEmail());
-	} else if (lab.getContactEmail() != null && !lab.getContactEmail().equals("")
-			&& allRecipients.indexOf(lab.getContactEmail()) == -1) {
-		allRecipients.append(lab.getContactEmail());
-	}
-
-	return allRecipients.toString();
-}
-
-/**
- * The callback method called after the loadCommand, and execute methods, this method allows you to manipulate the HttpServletResponse object prior to
- * forwarding to the result JSP (add a cookie, etc.)
- *
- * @param request
- *            The HttpServletResponse for the command
- * @return The processed response
- */
-public HttpServletResponse setResponseState(HttpServletResponse response) {
-	return response;
 }
 
 }
