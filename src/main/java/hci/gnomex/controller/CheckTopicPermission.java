709baf6cab9e93f45e607c32c9192c20334455db
Erik Rasmussen <u0566434@hci.utah.edu>
2017-01-31 10:07:14 -0700

    GNOM-1250 In Guest mode, Analysis, Data Track, and Topic now notify user if items cannot be retrieved in search and the reason for it.

--- src/main/java/hci/gnomex/controller/CheckTopicPermission.java
+++ src/main/java/hci/gnomex/controller/CheckTopicPermission.java
@@ -6,6 +6,7 @@ import hci.gnomex.model.Topic;
 
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
 import org.apache.log4j.Logger;
-
+import org.hibernate.query.Query;
 
 
 public class CheckTopicPermission extends GNomExCommand implements Serializable {
@@ -44,14 +45,26 @@ public class CheckTopicPermission extends GNomExCommand implements Serializable 
 
       Session sess = this.getSecAdvisor().getReadOnlyHibernateSession(this.getUsername());
 
+
+      String queryStr = "SELECT t FROM Topic as t WHERE t.idTopic = :idTopic";
+      Query query = sess.createQuery(queryStr)
+              .setParameter("idTopic",idTopic);
+      List topics  =  query.list();
+
       if(idTopic != null) {
-        Topic dataTrack = (Topic) (sess.load(Topic.class, idTopic));
-        if (!this.getSecAdvisor().canRead(dataTrack)) {
-          this.addInvalidField("perm", "Insufficient permission to access this topic");
+
+        if (topics.size() > 0) {
+          Topic dataTrack =  (Topic) topics.get(0);
+          if (!this.getSecAdvisor().canRead(dataTrack)) {
+            this.addInvalidField("perm", "Insufficient permission to access this topic");
+          }
+        } else{
+          this.addInvalidField("topicNumber", "Topic "+ idTopic + " does not exist.");
         }
       } else {
         this.addInvalidField("topicNumber", "topicNumber is either invalid or not provided");
       }
+
       if (isValid()) {
         this.xmlResult = "<SUCCESS/>";
         setResponsePage(this.SUCCESS_JSP);
