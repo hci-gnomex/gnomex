020fce42ae55b7f5683ecc0e3a70f9e1212afe2d
u6008750 <john.dewell@hci.utah.edu>
2017-07-26 09:42:45 -0600

    Added left hand tree

--- gnomex_ng/src/app/experiments/browse-experiments.component.ts
+++ gnomex_ng/src/app/experiments/browse-experiments.component.ts
@@ -1,35 +1,66 @@
 /*
  * Copyright (c) 2016 Huntsman Cancer Institute at the University of Utah, Confidential and Proprietary
  */
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild, ViewEncapsulation} from "@angular/core";
 
 import {ExperimentsService} from "./experiments.service";
+import { jqxTreeComponent } from 'jqwidgets-framework';
 
 @Component({
     selector: "experiments",
-    template: `
-    <div>
-        <hci-grid class="w-100"
-                  [title]="'Browse Experiements'"
-                  [inputData]="experiments"
-                  [pageSize]="10">
-            <column-def [name]="'Code'" [field]="'expCodeApp'"></column-def>
-            <column-def [name]="'Create Date'" [field]="'expCreateDate'"></column-def>
-            <column-def [name]="'Status'" [field]="'expStatus'"></column-def>
-            <column-def [name]="'Run Type'" [field]="'expSeqRunType'"></column-def>
-            <column-def [name]="'Lab Name'" [field]="'labFullName'"></column-def>
-        </hci-grid>
-    </div>
-    `
+    templateUrl:'./experiments.component.html',
+    styles: [`
+        angularTree > div:first-child
+        {
+            height: 100%;
+            border: none;
+        }
+    `],
+    encapsulation: ViewEncapsulation.None
+
 })
 export class BrowseExperimentsComponent {
+    @ViewChild('treeReference') tree: jqxTreeComponent;
 
-    private experiments: Array<Object> = null;
-
+    labs: any[];
     constructor(private experimentsService: ExperimentsService) {
-        this.experimentsService.getExperiments().subscribe((response: Array<Object>) => {
-            this.experiments = response;
-        });
+        this.experimentsService.getExperiments().subscribe(response => {
+            this.buildTree(response);
+        })
+        this.labs = [];
+
     }
 
+    buildTree(response: any[]) {
+        this.labs = [response];
+        for( var l of this.labs) {
+            l.icon = "assets/group.png"
+            l.items = l.Project;
+            for( var p of l.items) {
+                p.icon = "assets/folder.png"
+                p.items = [];
+                if (!this.isArray(p.RequestCategory)) {
+                    p.items = p.items.concat([p.RequestCategory.Request]);
+                } else {
+                    for (var r of p.RequestCategory) {
+                        if( !this.isArray(r.Request)) {
+                            p.items = p.items.concat([r.Request]);
+                        } else {
+                            p.items = p.items.concat(r.Request);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    isArray(what) {
+        return Object.prototype.toString.call(what) === '[object Array]';
+    }
+
+    detailFn(): (keywords: string) => void {
+        return (keywords) => {
+            window.location.href = "http://localhost/gnomex/experiments/"+keywords;
+        };
+    }
 }
--- gnomex_ng/src/app/experiments/experiments.service.ts
+++ gnomex_ng/src/app/experiments/experiments.service.ts
@@ -1,6 +1,9 @@
 import {Inject, Injectable, OpaqueToken} from "@angular/core";
 import {Http, Response} from "@angular/http";
 import {Observable} from "rxjs/Observable";
+import {Lab} from "./Lab";
+import {Project} from "./Project";
+import {Request} from "./Request";
 
 export let BROWSE_EXPERIMENTS_ENDPOINT: OpaqueToken = new OpaqueToken("browse_experiments_url");
 export let VIEW_EXPERIMENT_ENDPOINT: OpaqueToken = new OpaqueToken("view_experiment_url");
@@ -9,11 +12,10 @@ export let VIEW_EXPERIMENT_ENDPOINT: OpaqueToken = new OpaqueToken("view_experim
 export class ExperimentsService {
 
     constructor(private _http: Http, @Inject(BROWSE_EXPERIMENTS_ENDPOINT) private _browseExperimentsUrl: string) {}
-
-    getExperiments(): Observable<Object> {
-        return this._http.get("/gnomex/GetExperimentOverviewList.gx", {withCredentials: true}).map((response: Response) => {
+    getExperiments() {
+        return this._http.get("/gnomex/GetProjectRequestList.gx?idLab=1500", {withCredentials: true}).map((response: Response) => {
             if (response.status === 200) {
-                return response.json();
+                return response.json().Lab;
             } else {
                 throw new Error("Error");
             }
